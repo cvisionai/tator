c6a4fc48b3b80eccf6037878f2523ba8ec101004
Brian Tate <brian.tate@cvisionai.com>
2019-09-20 05:03:14 -0400

    Add alert notifications for lockouts

--- main/auth.py
+++ main/auth.py
@@ -2,9 +2,15 @@ from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
 from .notify import Notify
-import datetime
+from datetime import datetime,timezone,timedelta
+
+import logging
+logger = logging.getLogger(__name__)
 
 UserModel = get_user_model()
+
+LOCKOUT_LIMIT = 3
+LOCKOUT_TIME = timedelta(minutes=10)
 
 class TatorAuth(ModelBackend):
     """
@@ -22,17 +28,41 @@ class TatorAuth(ModelBackend):
             # Run the default password hasher once to reduce the timing
             # difference between an existing and a nonexistent user (#20760).
             UserModel().set_password(password)
-        else:
-            if user.check_password(password) and self.user_can_authenticate(user):
-                user.last_login = datetime.datetime.now()
-                user.failed_login_count = 0
-                user.save()
-                return user
-            else:
-                user.last_failed_login = datetime.datetime.now()
+            return
+
+        if user.failed_login_count >= LOCKOUT_LIMIT:
+            now=datetime.now(timezone.utc)
+            since_last_fail=user.last_failed_login - now
+            if since_last_fail <= LOCKOUT_TIME:
+                user.last_failed_login = now
                 user.failed_login_count += 1
                 user.save()
-                if user.failed_login_count >= 3:
-                    msg=f"Bad Login Attempt for {user}/{user.id}"
-                    msg+=f" Attempt count = {user.failed_login_count}"
-                    Notify.notify_admin_msg(msg)
+                lockout_lifted=user.last_failed_login+LOCKOUT_TIME
+                time_left=lockout_lifted-now
+                msg = f" *SECURITY ALERT:* Attempt to login during lockout"
+                msg += f" User={user}/{user.id}"
+                msg += f" Attempt count = {user.failed_login_count}"
+                msg += f" Lockout will be lifted in '{time_left}' at '{lockout_lifted}'"
+                Notify.notify_admin_msg(msg)
+                # Run the default password hasher once to reduce the timing
+                # difference (#20760).
+                UserModel().set_password(password)
+                return
+
+        if user.check_password(password) and self.user_can_authenticate(user):
+            user.last_login = datetime.now()
+            if user.failed_login_count >= LOCKOUT_LIMIT:
+                msg = "Login proceeded after lock expiry"
+                msg += f" User={user}/{user.id}"
+                Notify.notify_admin_msg(msg)
+            user.failed_login_count = 0
+            user.save()
+            return user
+        else:
+            user.last_failed_login = datetime.now()
+            user.failed_login_count += 1
+            user.save()
+            if user.failed_login_count >= LOCKOUT_LIMIT:
+                msg = f"*SECURITY ALERT:* Bad Login Attempt for {user}/{user.id}"
+                msg += f" Attempt count = {user.failed_login_count}"
+                Notify.notify_admin_msg(msg)
