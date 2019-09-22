ed6943b011e57c2c86ad58f2a7f285d028b607a4
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2019-09-22 21:11:48 +0000

    Connect directly to postgis when in debug mode (bypass pgbouncer)

--- tator_online/settings.py
+++ tator_online/settings.py
@@ -133,16 +133,28 @@ WSGI_APPLICATION = 'tator_online.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'tator_online',
-        'USER': os.getenv('TATOR_SECRET_POSTGRES_USER'),
-        'PASSWORD': os.getenv('TATOR_SECRET_POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT', 5432),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.contrib.gis.db.backends.postgis',
+            'NAME': 'tator_online',
+            'USER': os.getenv('TATOR_SECRET_POSTGRES_USER'),
+            'PASSWORD': os.getenv('TATOR_SECRET_POSTGRES_PASSWORD'),
+            'HOST': 'postgis-svc',
+            'PORT': os.getenv('POSTGRES_PORT', 5432),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.contrib.gis.db.backends.postgis',
+            'NAME': 'tator_online',
+            'USER': os.getenv('TATOR_SECRET_POSTGRES_USER'),
+            'PASSWORD': os.getenv('TATOR_SECRET_POSTGRES_PASSWORD'),
+            'HOST': os.getenv('POSTGRES_HOST'),
+            'PORT': os.getenv('POSTGRES_PORT', 5432),
+        }
+    }
 
 
 # Password validation
