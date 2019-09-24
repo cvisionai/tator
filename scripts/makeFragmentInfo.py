371e11678f03cd3c89fd59d4949d294306545b13
Brian Tate <brian.tate@cvisionai.com>
2019-09-23 20:13:05 -0400

    Have fragment info script get video start time from ffprobe

--- scripts/makeFragmentInfo.py
+++ scripts/makeFragmentInfo.py
@@ -14,12 +14,27 @@ if __name__=="__main__":
     cmd=f"mp4dump --format json {args.input}"
     proc=subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
     mp4_data,error=proc.communicate()
+
+    cmd=f"ffprobe -v error -show_entries stream -print_format json {args.input}"
+    proc=subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+    ffprobe_output,error=proc.communicate()
+
+    ffprobe_data=json.loads(ffprobe_output)
+    start_time = 0
+    try:
+        for stream in ffprobe_data["streams"]:
+            if stream["codec_type"] == "video":
+                start_time=float(ffprobe_data["streams"][0]["start_time"])
+                break
+    except Exception as e:
+        print(e)
+
     outputFile=sys.stdout
     if args.output != None:
         outputFile=open(args.output, "w")
 
     currentOffset=0
-    info={"segments" : []}
+    info={"file": {"start": start_time}, "segments" : []}
     with open(args.input) as fp:
         obj=json.loads(mp4_data)
         for block in obj:
