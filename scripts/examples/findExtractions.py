139bd1d819516738e0b2e38c4751678161b33f32
Brian Tate <brian.tate@cvisionai.com>
2019-10-24 15:15:51 -0400

    Support running algorithm automatically on media missing extractions

--- scripts/examples/findExtractions.py
+++ scripts/examples/findExtractions.py
@@ -10,14 +10,17 @@ def verify_extractions(tator,
                        metadata_endpoint,
                        media,
                        metadata,
-                       all_medias):
+                       all_medias,
+                       algorithm_name):
     metadata_for_media = project_metadata.loc[project_metadata['media'] == media['id']]
+    media_reruns=set()
     for idx,metadata in metadata_for_media.iterrows():
         frame = metadata.frame
         extracted_name = f"{media['id']}_{media['name']}_{frame}.png"
         extracted_medias = all_medias.loc[all_medias.name==extracted_name]
         if len(extracted_medias) == 0:
             print(f"{media['name']} missing extraction for frame {frame}")
+            media_reruns.add(media)
         else:
             if len(extracted_medias) != 1:
                 print(f"{media['name']} has duplicate extractions!")
@@ -27,6 +30,12 @@ def verify_extractions(tator,
             metadata_for_extraction = project_metadata.loc[project_metadata.media == extracted_media.id]
             if len(metadata_for_extraction) == 0:
                 print(f"{media['name']} extraction is present, missing data")
+                media_reruns.add(media)
+
+    if algorithm_name:
+        for media in media_reruns:
+            print(f"Launching {algorithm_name} on {media['name']}")
+            tator.Algorithm.launch_on_media(algorithm_name, media['id'])
 
 if __name__=="__main__":
     # Create a standard arg parse and add pytator args
@@ -34,7 +43,8 @@ if __name__=="__main__":
     parser = pytator.tator.cli_parser(parser)
     parser.add_argument("--section", required=True, help="Section Name")
     parser.add_argument("--metadataType", type=str, choices=["state", "localization"], required=True)
-    parser.add_argument("--metadataTypeId", required=True) 
+    parser.add_argument("--metadataTypeId", required=True)
+    parser.add_argument("--algorithm", required=False, help="Algorithm name to launch if data is missing")
     args = parser.parse_args()
     tator = pytator.Tator(args.url, args.token, args.project)
     medias = tator.Media.filter({"attribute": f"tator_user_sections::{args.section}"})
@@ -51,7 +61,7 @@ if __name__=="__main__":
 
     type_id = args.metadataTypeId
     
-    bar = progressbar.ProgressBar(wrap_stdout=True)
+    bar = progressbar.ProgressBar(redirect_stdout=True)
     project_metadata = metadata_endpoint.dataframe({'type': type_id})
     for media in bar(medias):
         verify_extractions(tator,
@@ -59,4 +69,5 @@ if __name__=="__main__":
                            metadata_endpoint,
                            media,
                            project_metadata,
-                           all_medias)
+                           all_medias,
+                           args.algorithm)
