d437d0825b7764117a881b569effe4b7e3e91898
Brian Tate <brian.tate@cvisionai.com>
2020-01-10 12:12:13 -0500

    Make dev push work with collect static too.

--- scripts/dev-push.sh
+++ scripts/dev-push.sh
@@ -6,4 +6,8 @@ for pod in ${pods}; do
     name=`echo $pod | sed 's/pod\///'`
     echo "Updating ${name}"
     kubectl cp main ${name}:/tator_online
+    kubectl cp tator_online ${name}:/tator_online
 done
+
+# Run collect static on one of them
+kubectl exec -it $(kubectl get pod -l "app=gunicorn" -o name | head -n 1 |sed 's/pod\///') -- python3 manage.py collectstatic --noinput
