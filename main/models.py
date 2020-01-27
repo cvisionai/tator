fc865b65032389c3e7c1d0aba3c843adeb4ae1af
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-01-27 18:22:18 +0000

    Add modified field to EntityState

--- main/models.py
+++ main/models.py
@@ -545,6 +545,12 @@ class EntityState(EntityBase):
     """
     association = ForeignKey(AssociationType, on_delete=CASCADE)
     version = ForeignKey(Version, on_delete=SET_NULL, null=True, blank=True)
+    modified = BooleanField(null=True, blank=True)
+    """ Indicates whether an annotation is original or modified.
+        null: Original upload, no modifications.
+        false: Original upload, but was modified or deleted.
+        true: Modified since upload or created via web interface.
+    """
 
     def selectOnMedia(media_id):
         return AssociationType.states(media_id)
