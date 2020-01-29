87594e3576eb03af0429151fd9ffbd7d326d794b
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-01-29 20:13:36 +0000

    Add function to retrieve annotation queryset using elasticsearch

--- main/rest.py
+++ main/rest.py
@@ -1526,6 +1526,39 @@ class EntityMediaListAPI(ListAPIView, AttributeFilterMixin):
         finally:
             return response;
 
+def get_annotation_queryset(project, query_params, attr_filter):
+    """Converts annotation query string into a list of IDs and a count.
+    """
+    mediaId = query_params.get('media_id', None)
+    filterType = query_params.get('type', None)
+    start = query_params.get('start', None)
+    stop = query_params.get('stop', None)
+
+    query = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(dict))))
+    query['sort']['_exact_name'] = 'asc'
+    media_bools = []
+    annotation_bools = []
+
+    if mediaId != None:
+        media_bools.append({'ids': {'values': mediaId.split(',')}})
+
+    if filterType != None:
+        annotation_bools.append({'match': {'_meta': {'query': int(filterType)}}})
+
+    if start != None:
+        query['from'] = int(start)
+
+    if start == None and stop != None:
+        query['size'] = int(stop)
+
+    if start != None and stop != None:
+        query['size'] = int(stop) - int(start)
+    query = get_attribute_query(query_params, query, media_bools, project, False, annotation_bools)
+
+    annotation_ids, annotation_count = TatorSearch().search(project, query)
+
+    return annotation_ids, annotation_count, query
+
 class EntityStateCreateListSchema(AutoSchema, AttributeFilterSchemaMixin):
     def get_manual_fields(self, path, method):
         manual_fields = super().get_manual_fields(path,method)
