8b6d3e77b87380baa61c282ad7184b2fc05ca405
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-02-20 14:41:30 +0000

    Add management command to retrieve all project IDs

---
+++ main/management/commands/getprojects.py
@@ -0,0 +1,6 @@
+from django.core.management.base import BaseCommand
+from main.models import Project
+
+class Command(BaseCommand):
+		def handle(self, **options):
+				print(','.join(map(str, list(Project.objects.all().values_list('id', flat=True)))))
