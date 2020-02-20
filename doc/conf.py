a7482839e63c4daf17c40b0c5ef2a1e8dcc6a4e6
Brian Tate <brian.tate@cvisionai.com>
2020-02-20 14:37:21 -0500

    fix rtd config

--- doc/conf.py
+++ doc/conf.py
@@ -14,9 +14,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../scripts/packages/pytator/pytator'))
+sys.path.insert(0, os.path.abspath('../scripts/packages/pytator'))
 sys.path.insert(0, os.path.abspath('../scripts/packages/pytator/examples'))
-sys.path.insert(0, os.path.abspath('../scripts/packages/pytator/docs'))
 
 # -- Project information -----------------------------------------------------
 
