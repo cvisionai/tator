21e8c0dff8bbe6fda580f1a7a56aa16ef9c4c89f
Brian Tate <brian.tate@cvisionai.com>
2020-03-06 15:41:18 -0500

    Revert "Allow for blank choices"
    This reverts commit 0f3a0f43c1bc18ef73dadf0f7ff77d5cb1edec2b.

--- main/models.py
+++ main/models.py
@@ -703,7 +703,7 @@ def float_type_save(sender, instance, **kwargs):
 class AttributeTypeEnum(AttributeTypeBase):
     attr_name = "Enum"
     dtype = "enum"
-    choices = ArrayField(CharField(max_length=64, blank=True))
+    choices = ArrayField(CharField(max_length=64))
     default = CharField(max_length=64, null=True, blank=True)
 
 @receiver(post_save, sender=AttributeTypeEnum)
