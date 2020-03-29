e5fe43a74d5d555af3345ab5dc51e2f29f1bbcaa
Brian Tate <brian.tate@cvisionai.com>
2020-03-29 19:15:31 -0400

    Make rate control a drop down, Fixes #79

--- main/static/js/annotation/rate-control.js
+++ main/static/js/annotation/rate-control.js
@@ -1,49 +1,38 @@
 class RateControl extends TatorElement {
   constructor() {
     super();
-
-    const details = document.createElement("details");
-    details.setAttribute("class", "position-relative");
-    this._shadow.appendChild(details);
   
     const summary = document.createElement("summary");
     summary.style.cursor = "pointer";
     summary.setAttribute("class", "annotation__setting d-flex flex-items-center px-3 rounded-1");
-    details.appendChild(summary);
+    this._shadow.appendChild(summary);
 
     const rateSpan = document.createElement("div");
     rateSpan.setAttribute("class", "text-gray");
     rateSpan.textContent = "Rate:";
     summary.appendChild(rateSpan);
 
-    const span = document.createElement("span");
-    span.setAttribute("class", "px-1");
-    span.textContent = "1x";
-    summary.appendChild(span);
-
-    const styleDiv = document.createElement("div");
-    styleDiv.setAttribute("class", "files__main files-wrap");
-    details.appendChild(styleDiv);
-
     const div = document.createElement("div");
-    div.setAttribute("class", "more d-flex flex-column f2 py-3 px-2");
-    styleDiv.appendChild(div);
+    div.setAttribute("class", "px-1");
+    summary.appendChild(div);
 
-    const slider = document.createElement("input");
-    slider.setAttribute("class", "range flex-grow");
-    slider.setAttribute("type", "range");
-    slider.setAttribute("step", "1");
-    slider.setAttribute("value", "3");
-    slider.setAttribute("min", "0");
-    slider.setAttribute("max", "6");
-    div.appendChild(slider);
+    const select = document.createElement("select");
+    select.setAttribute("class", "form-select has-border select-sm1");
+    select.style.color = "#FFFFFF";
+    div.appendChild(select);
 
     const rates = [0.125, 0.25, 0.5, 1, 2, 4, 8];
+    for (const rate of rates)
+    {
+      let option = document.createElement("option");
+      option.setAttribute("value", rate);
+      option.textContent = `${rate}x`;
+      select.append(option);
+    }
+    select.selectedIndex = 3; //represents 1x
 
-    slider.addEventListener("input", evt => {
-      const index = evt.target.value;
-      const rate = rates[index];
-      span.textContent = rate + "x";
+    select.addEventListener("change", evt => {
+      const rate = Number(evt.target.value);
       this.dispatchEvent(new CustomEvent("rateChange", {
         detail: {rate: rate},
         composed: true
