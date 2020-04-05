39fafd6e35e5230ec3a481c77fdf98a15439727e
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-04-05 15:29:23 +0000

    Only use attributes with order > 0 for identifying attribute

--- main/static/js/util/identifying-attribute.js
+++ main/static/js/util/identifying-attribute.js
@@ -1,8 +1,10 @@
 function identifyingAttribute(dataTypes) {
   const sorted = dataTypes.columns.sort((a, b) => a.order - b.order);
   for (const dataType of sorted) {
-    if ((dataType.dtype == "str") || (dataType.dtype == "enum")) {
-      return dataType;
+    if (dataType.order >= 0) {
+      if ((dataType.dtype == "str") || (dataType.dtype == "enum")) {
+        return dataType;
+      }
     }
   }
 }
