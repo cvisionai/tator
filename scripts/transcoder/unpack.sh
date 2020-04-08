73fee1145c4b8fcd0581baf0a1a441b46e64c8b7
Brian Tate <brian.tate@cvisionai.com>
2020-04-08 13:31:07 -0400

    Jar doesn't handle paths that well, fix cwd

--- scripts/transcoder/unpack.sh
+++ scripts/transcoder/unpack.sh
@@ -12,7 +12,9 @@
 if [ `echo $1 | grep ".tar" | wc -l` -eq 1 ]; then
     tar -xf $1 -C $2
 elif [ `echo $1 | grep ".zip" | wc -l` -eq 1 ]; then
-    jar -xf $1 -C $2
+    pushd $2
+    jar -xf $1
+    popd
 fi
 
 rm -f $1
