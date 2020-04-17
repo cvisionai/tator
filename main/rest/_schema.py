ae81384995579c7390e3f66362f7ea4f27ce8aa3
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-04-17 05:55:31 +0000

    Store validator instead of spec as static variable

--- main/rest/_schema.py
+++ main/rest/_schema.py
@@ -1,7 +1,5 @@
 import logging
 
-from rest_framework.schemas.openapi import AutoSchema
-from rest_framework.compat import coreschema
 from rest_framework.schemas.openapi import SchemaGenerator
 from openapi_core import create_spec
 from openapi_core.validation.request.validators import RequestValidator
@@ -10,13 +8,13 @@ from openapi_core.contrib.django import DjangoOpenAPIRequest
 logger = logging.getLogger(__name__)
 
 def parse(request):
-    if parse.openapi_spec is None:
+    if parse.validator is None:
         generator = SchemaGenerator(title='Tator REST API')
         spec = generator.get_schema()
-        parse.openapi_spec = create_spec(spec)
-    validator = RequestValidator(parse.openapi_spec)
+        openapi_spec = create_spec(spec)
+        parse.validator = RequestValidator(openapi_spec)
     openapi_request = DjangoOpenAPIRequest(request)
-    result = validator.validate(openapi_request)
+    result = parse.validator.validate(openapi_request)
     result.raise_for_errors()
     out = {
         **result.parameters.path,
@@ -26,4 +24,5 @@ def parse(request):
         out = {**out, **result.body}
     return out
 
-parse.openapi_spec = None
+parse.validator = None
+
