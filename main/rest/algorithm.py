ad5fdde18fcde3d9b37169b32a90d122fdd67e6f
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-04-17 14:43:20 +0000

    Update schema for algorithm list

--- main/rest/algorithm.py
+++ main/rest/algorithm.py
@@ -1,25 +1,40 @@
 from rest_framework.generics import ListAPIView
-from rest_framework.schemas import AutoSchema
-from rest_framework.compat import coreschema, coreapi
+from rest_framework.schemas.openapi import AutoSchema
 
 from ..models import Algorithm
 from ..serializers import AlgorithmSerializer
 
+from ._schema import parse
 from ._permissions import ProjectViewOnlyPermission
+
+class AlgorithmListSchema(AutoSchema):
+    def get_operation(self, path, method):
+        operation = super().get_operation(path, method)
+        operation['tags'] = ['Algorithm']
+        return operation
+
+    def _get_path_parameters(self, path, method):
+        return [{
+            'name': 'project',
+            'in': 'path',
+            'required': True,
+            'description': 'A unique integer identifying a project.',
+            'schema': {'type': 'integer'},
+        }]
+
+    def _get_filter_parameters(self, path, method):
+        return []
+
+    def _get_request_body(self, path, method):
+        return {}
 
 class AlgorithmListAPI(ListAPIView):
     serializer_class = AlgorithmSerializer
-    schema = AutoSchema(manual_fields=[
-        coreapi.Field(name='project',
-                      required=True,
-                      location='path',
-                      schema=coreschema.String(description='A unique integer value identifying a "project_id"')
-        ),
-    ])
+    schema = AlgorithmListSchema()
     permission_classes = [ProjectViewOnlyPermission]
 
     def get_queryset(self):
-        project_id = self.kwargs['project']
-        qs = Algorithm.objects.filter(project__id=project_id)
+        params = parse(self.request)
+        qs = Algorithm.objects.filter(project__id=params['project'])
         return qs
 
