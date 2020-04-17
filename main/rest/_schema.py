c0c006ad9b9f2f6094cdca2d351b734ec9e59a6d
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-04-17 18:49:57 +0000

    Move custom generator into _schema.py

--- main/rest/_schema.py
+++ main/rest/_schema.py
@@ -7,6 +7,47 @@ from openapi_core.contrib.django import DjangoOpenAPIRequest
 
 logger = logging.getLogger(__name__)
 
+class CustomGenerator(SchemaGenerator):
+    def get_schema(self, request=None, public=False):
+        schema = super().get_schema(request, public)
+        schema['paths']['/rest/Token']['post']['requestBody'] = {
+            'content': {'application/json': {
+                'schema': {
+                    'type': 'object',
+                    'required': ['username', 'password'],
+                    'properties': {
+                        'username': {
+                            'description': 'Account username.',
+                            'type': 'string',
+                        },
+                        'password': {
+                            'description': 'Account password.',
+                            'type': 'string',
+                        },
+                    },
+                },
+            }},
+        }
+        schema['paths']['/rest/Token']['post']['responses'] = {
+            '200': {
+                'description': 'Login credentials accepted.',
+                'content': {'application/json': {
+                    'schema': {
+                        'type': 'object',
+                        'properties': {
+                            'token': {
+                                'description': 'API token.',
+                                'type': 'string',
+                            },
+                        },
+                    },
+                }},
+            },
+            400: {'description': 'Login credentials invalid.'},
+        }
+        schema['paths']['/rest/Token']['post']['tags'] = ['Token']
+        return schema
+
 def parse(request):
     if parse.validator is None:
         generator = SchemaGenerator(title='Tator REST API')
--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,6 @@ from django.urls import include
 from django.conf.urls import url
 
 from rest_framework.authtoken import views
-from rest_framework.schemas.openapi import SchemaGenerator
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.documentation import include_docs_urls
@@ -23,49 +22,9 @@ from .views import AuthProjectView
 from .views import AuthAdminView
 
 from .rest import *
+from .rest._schema import CustomGenerator
 
 logger = logging.getLogger(__name__)
-
-class CustomGenerator(SchemaGenerator):
-    def get_schema(self, request=None, public=False):
-        schema = super().get_schema(request, public)
-        schema['paths']['/rest/Token']['post']['requestBody'] = {
-            'content': {'application/json': {
-                'schema': {
-                    'type': 'object',
-                    'required': ['username', 'password'],
-                    'properties': {
-                        'username': {
-                            'description': 'Account username.',
-                            'type': 'string',
-                        },
-                        'password': {
-                            'description': 'Account password.',
-                            'type': 'string',
-                        },
-                    },
-                },
-            }},
-        }
-        schema['paths']['/rest/Token']['post']['responses'] = {
-            200: {
-                'description': 'Login credentials accepted.',
-                'content': {'application/json': {
-                    'schema': {
-                        'type': 'object',
-                        'properties': {
-                            'token': {
-                                'description': 'API token.',
-                                'type': 'string',
-                            },
-                        },
-                    },
-                }},
-            },
-            400: {'description': 'Login credentials invalid.'},
-        }
-        schema['paths']['/rest/Token']['post']['tags'] = ['Token']
-        return schema
 
 schema_view = get_schema_view(
     title='Tator REST API',
