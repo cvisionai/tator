5e21fde3c2678a6674062ff7220d36f7ce32c338
Brian Tate <brian.tate@cvisionai.com>
2020-04-28 15:40:41 -0400

    Add post implementation

--- main/rest/temporary_file.py
+++ main/rest/temporary_file.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import generics
 from rest_framework import views
+from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from ._permissions import ProjectEditPermission
 
@@ -13,6 +14,9 @@ from ..schema import TemporaryFileDetailSchema
 from ..schema import TemporaryFileListSchema
 from rest_framework.schemas.openapi import AutoSchema
 from ..schema import parse
+import datetime
+
+import os
 
 class TemporaryFileAPI(generics.RetrieveDestroyAPIView):
     """ Access a detailed view of a temporary file given an id """
@@ -36,19 +40,36 @@ class TemporaryFileListAPI(generics.ListAPIView):
         response=Response({})
         try:
             params = parse(request)
+            url = params['url']
+            project = params['project']
+            name = params['name']
+            hours = params['hours']
+            if hours == None:
+                hours = 24
+            eol = datetime.datetime.now() + datetime.timedelta(hours=hours)
+            extension = os.path.splitext(name)[-1]
+            local_file_path = os.path.join(settings.UPLOAD_ROOT,url.split('/')[-1])
+            destination_fp=os.path.join(settings.MEDIA_ROOT, f"{project}", f"{uuid.uuid1()}{extension}")
+            shutil.copyfile(local_file_path, destination_fp)
+            temporary_file = TemporaryFile(name=params['name'],
+                                           project=project,
+                                           user=request.user,
+                                           path=destination_fp,
+                                           lookup=params['lookup'],
+                                           eol_datetime = eol)
+            temporary_file.save()
         except Exception as e:
             response=Response({'message' : str(e),
                                'details': traceback.format_exc()}, status=status.HTTP_400_BAD_REQUEST)
             logger.warning(traceback.format_exc())
         finally:
             # Delete files from the uploads directory.
-            if 'upload_paths' in locals():
-                for key in upload_paths:
-                    logger.info(f"Removing uploaded file {upload_paths[key]}")
-                    if os.path.exists(upload_paths[key]):
-                        logger.info(f"{upload_paths[key]} exists and is being removed!")
-                        os.remove(upload_paths[key])
-                    info_path = os.path.splitext(upload_paths[key])[0] + '.info'
-                    if os.path.exists(info_path):
-                        os.remove(info_path)
+            if 'local_file_path' in locals():
+                logger.info(f"Removing uploaded file {local_file_path}")
+                if os.path.exists(local_file_path):
+                    logger.info(f"{local_file_path} exists and is being removed!")
+                    os.remove(local_file_path)
+                info_path = os.path.splitext(local_file_path)[0] + '.info'
+                if os.path.exists(info_path):
+                    os.remove(info_path)
             return response;
