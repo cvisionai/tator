6234b7d20589ef30858cedb45f6028358cb3a13e
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-05-05 12:55:14 +0000

    Document response content from project endpoint

--- main/schema/project.py
+++ main/schema/project.py
@@ -1,5 +1,8 @@
 from rest_framework.schemas.openapi import AutoSchema
 
+from ._errors import error_responses
+from ._message import message_schema
+
 project_properties = {
     'name': {
         'description': 'Name of the project.',
@@ -9,6 +12,41 @@ project_properties = {
         'description': 'Summary of the project.',
         'type': 'string',
         'default': '',
+    },
+}
+
+project_get_schema = {
+    'type': 'object',
+    'description': 'Project object.',
+    'properties': {
+        'id': {
+            'type': 'integer',
+            'description': 'Unique integer identifying the project.',
+        },
+        **project_properties,
+        'thumb': {
+            'type': 'string',
+            'description': 'URL of thumbnail used to represent the project.',
+        },
+        'num_files': {
+            'type': 'integer',
+            'description': 'Number of files in the project.',
+        },
+        'size': {
+            'type': 'integer',
+            'description': 'Size of the project in bytes.',
+        },
+        'usernames': {
+            'type': 'array',
+            'description': 'List of usernames of project members.',
+            'items': {
+                'type': 'string',
+            },
+        },
+        'permission': {
+            'type': 'string',
+            'description': 'Permission level of user making request.',
+        },
     },
 }
 
@@ -45,17 +83,25 @@ class ProjectListSchema(AutoSchema):
         return body
 
     def _get_responses(self, path, method):
-        responses = {}
-        responses['404'] = {'description': 'Failure to find project with given ID.'}
-        responses['400'] = {'description': 'Bad request.'}
+        responses = error_responses()
         if method == 'GET':
-            responses['200'] = {'description': 'Successful retrieval of project list.'}
+            responses['200'] = {
+                'description': 'Successful retrieval of project list.',
+                'content': {'application/json': {'schema': {
+                    'type': 'array',
+                    'items': project_get_schema,
+                }}},
+            }
         elif method == 'POST':
             responses['201'] = {
                 'description': 'Successful creation of project.',
                 'content': {'application/json': {'schema': {
                     'type': 'object',
                     'properties': {
+                        'message': {
+                            'type': 'string',
+                            'description': 'Message indicating successful creation of project.',
+                        },
                         'id': {
                             'type': 'integer',
                             'minimum': 1,
@@ -106,13 +152,14 @@ class ProjectDetailSchema(AutoSchema):
         return body
 
     def _get_responses(self, path, method):
-        responses = {}
-        responses['404'] = {'description': 'Failure to find project with given ID.'}
-        responses['400'] = {'description': 'Bad request.'}
+        responses = error_responses()
         if method == 'GET':
-            responses['200'] = {'description': 'Successful retrieval of project.'}
-        elif method in ['PATCH', 'PUT']:
-            responses['200'] = {'description': 'Successful update of project.'}
+            responses['200'] = {
+                'description': 'Successful retrieval of project.',
+                'content': {'application/json': {'schema': project_get_schema}},
+            }
+        elif method == 'PATCH':
+            responses['200'] = message_schema('update', 'project')
         elif method == 'DELETE':
             responses['204'] = {'description': 'Successful deletion of project.'}
         return responses
