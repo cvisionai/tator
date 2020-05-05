43117a5498603d6ab16cb082f315fad7d0a6234e
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-05-05 13:03:44 +0000

    Add schema for message with id

--- main/schema/_message.py
+++ main/schema/_message.py
@@ -11,3 +11,21 @@ def message_schema(action, name):
             },
         }}},
     }
+
+def message_with_id_schema(name):
+    return {
+        'description': f'Successful creation of {name}.',
+        'content': {'application/json': {'schema': {
+            'type': 'object',
+            'properties': {
+                'message': {
+                    'type': 'string',
+                    'description': f'Message indicating successful creation of {name}.',
+                },
+                'id': {
+                    'type': 'integer',
+                    'description': f'Unique integer identifying created {name}.',
+                },
+            },
+        }}},
+    }
