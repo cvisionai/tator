d2fa44371f6a9d73c09e1b9babaadf3997982c2c
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-05-06 01:29:32 +0000

    Disable aliases when dumping out yaml spec

--- main/management/commands/getschema.py
+++ main/management/commands/getschema.py
@@ -4,8 +4,12 @@ from django.core.management.base import BaseCommand
 
 from main.schema import CustomGenerator
 
+class NoAliasDumper(yaml.Dumper):
+    def ignore_aliases(self, data):
+        return True
+
 class Command(BaseCommand):
     def handle(self, **options):
         generator = CustomGenerator(title='Tator REST API')
         spec = generator.get_schema()
-        print(yaml.dump(spec))
+        print(yaml.dump(spec, Dumper=NoAliasDumper))
