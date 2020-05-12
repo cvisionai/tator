86c7dbedc8256399e8051575e28f47cf96d9ac5b
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-05-12 12:03:00 +0000

    Move GetFrame into its own file

--- main/rest/media.py
+++ main/rest/media.py
@@ -250,97 +250,13 @@ class MediaDetailAPI(RetrieveUpdateDestroyAPIView):
         finally:
             return response;
 
-class GetFrameAPI(APIView):
-    schema = GetFrameSchema()
-    renderer_classes = (PngRenderer, JpegRenderer, GifRenderer, Mp4Renderer)
-    permission_classes = [ProjectViewOnlyPermission]
-
-    def get_queryset(self):
-        return EntityBase.objects.all()
-
-    def get(self, request, **kwargs):
-        """ Facility to get a frame(jpg/png) of a given video frame, returns a square tile of
-            frames based on the input parameter
-        """
-        try:
-            # upon success we can return an image
-            params = parse(request)
-            video = EntityMediaVideo.objects.get(pk=params['id'])
-            frames = params.get('frames', '0')
-            tile = params.get('tile', None)
-            animate = params.get('animate', None)
-            roi = params.get('roi', None)
-            quality = params.get('quality', None)
-
-            for frame in frames:
-                if int(frame) >= video.num_frames:
-                    raise Exception(f"Frame {frame} is invalid. Maximum frame is {video.num_frames-1}")
-            tile_size = tile
-
-            if tile and animate:
-                raise Exception("Can't supply both tile and animate arguments")
-
-
-            # compute the crop argument
-            roi_arg = []
-            if roi:
-                crop_filter = [None] * len(frames)
-                roi_list = roi.split(',')
-                logger.info(roi_list)
-                if len(roi_list) == 1:
-                    # Repeat the same roi if only 1 is given for a set
-                    comps = roi_list[0].split(':')
-                    if len(comps) == 4:
-                        box_width = float(comps[0])
-                        box_height = float(comps[1])
-                        x = float(comps[2])
-                        y = float(comps[3])
-                        roi_arg = [(box_width,box_height,x,y)]*len(frames)
-                else:
-                    # If each individual roi is supplied manually set each one
-                    if len(roi_list) != len(frames):
-                        raise Exception(f'Explicit roi list{len(roi_list)} is different length than frame list{len(frames)}')
-                    for idx,frame_roi in enumerate(roi_list):
-                        comps = frame_roi.split(':')
-                        if len(comps) == 4:
-                            box_width = float(comps[0])
-                            box_height = float(comps[1])
-                            x = float(comps[2])
-                            y = float(comps[3])
-                            roi_arg.append((box_width,box_height,x,y))
 
 
 
-            with tempfile.TemporaryDirectory() as temp_dir:
-                media_util = MediaUtil(video, temp_dir, quality)
-                if len(frames) > 1 and animate:
-                    # Default to gif for animate, but mp4 is also supported
-                    if any(x is request.accepted_renderer.format for x in ['mp4','gif']):
-                        pass
-                    else:
-                        request.accepted_renderer = GifRenderer()
-                    gif_fp = media_util.getAnimation(frames, roi_arg, fps=animate, render_format=request.accepted_renderer.format)
-                    with open(gif_fp, 'rb') as data_file:
-                        response = Response(data_file.read())
-                else:
-                    logger.info(f"Accepted format = {request.accepted_renderer.format}")
-                    tiled_fp = media_util.getTileImage(frames, roi_arg, tile_size, render_format=request.accepted_renderer.format)
-                    with open(tiled_fp, 'rb') as data_file:
-                        response = Response(data_file.read())
 
 
-        except ObjectDoesNotExist as dne:
-            request.accepted_renderer.format = "jpg"
-            response=Response(MediaUtil.generate_error_image(404, "No Media Found"),
-                              status=status.HTTP_404_NOT_FOUND)
-            logger.warning(traceback.format_exc())
-        except Exception as e:
-            request.accepted_renderer.format = "jpg"
-            response=Response(MediaUtil.generate_error_image(400, str(e)),
-                              status=status.HTTP_400_BAD_REQUEST)
-            logger.warning(traceback.format_exc())
-        finally:
-            return response
+
+
 
 
     def get_queryset(self):
