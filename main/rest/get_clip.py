df463cd24ee92d34c27c2af2249cd4e8be260c54
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-05-12 12:02:22 +0000

    Move GetClip into its own file

---
+++ main/rest/get_clip.py
@@ -0,0 +1,53 @@
+import tempfile
+import traceback
+
+from ..models import TemporaryFile
+from ..serializers import TemporaryFileSerializer
+from ..schema import GetClipSchema
+
+from ._base_views import BaseDetailView
+from ._media_util import MediaUtil
+from ._permissions import ProjectViewOnlyPermission
+
+class GetClipAPI(BaseDetailView):
+    schema = GetClipSchema()
+    permission_classes = [ProjectViewOnlyPermission]
+    http_method_names = ['get']
+
+    def get_serializer(self):
+        """ This allows the AutoSchema to fill in the response details nicely"""
+        return TemporaryFileSerializer()
+
+    def get_queryset(self):
+        return Media.objects.all()
+
+    def _get(self, params):
+        """ Facility to get a clip from the server. Returns a temporary file object that expires in 24 hours.
+        """
+        # upon success we can return an image
+        params = parse(request)
+        video = Media.objects.get(pk=params['id'])
+        project = video.project
+        frameRangesStr = params.get('frameRanges', None)
+        frameRangesTuple=[frameRange.split(':') for frameRange in frameRangesStr]
+        frameRanges=[]
+        for t in frameRangesTuple:
+            frameRanges.append((int(t[0]), int(t[1])))
+
+        quality = params.get('quality', None)
+        h = hashlib.new('md5', f"{params}".encode())
+        lookup = h.hexdigest()
+
+        # Check to see if we already made this clip
+        matches=TemporaryFile.objects.filter(project=project, lookup=lookup)
+        if matches.exists():
+            temp_file = matches[0]
+        else:
+            with tempfile.TemporaryDirectory() as temp_dir:
+                media_util = MediaUtil(video, temp_dir, quality)
+                fp = media_util.getClip(frameRanges)
+
+                temp_file = TemporaryFile.from_local(fp, "clip.mp4", project, request.user, lookup=lookup, hours=24)
+
+        responseData = TemporaryFileSerializer(temp_file, context={"view": self}).data
+        return responseData
--- main/rest/media.py
+++ main/rest/media.py
@@ -342,55 +342,6 @@ class GetFrameAPI(APIView):
         finally:
             return response
 
-class GetClipAPI(APIView):
-    schema = GetClipSchema()
-    permission_classes = [ProjectViewOnlyPermission]
-
-    def get_serializer(self):
-        """ This allows the AutoSchema to fill in the response details nicely"""
-        return TemporaryFileSerializer()
 
     def get_queryset(self):
-        return EntityBase.objects.all()
 
-    def get(self, request, **kwargs):
-        """ Facility to get a clip from the server. Returns a temporary file object that expires in 24 hours.
-        """
-        try:
-            # upon success we can return an image
-            params = parse(request)
-            video = EntityMediaVideo.objects.get(pk=params['id'])
-            project = video.project
-            frameRangesStr = params.get('frameRanges', None)
-            frameRangesTuple=[frameRange.split(':') for frameRange in frameRangesStr]
-            frameRanges=[]
-            for t in frameRangesTuple:
-                frameRanges.append((int(t[0]), int(t[1])))
-
-            quality = params.get('quality', None)
-            h = hashlib.new('md5', f"{params}".encode())
-            lookup = h.hexdigest()
-
-            # Check to see if we already made this clip
-            matches=TemporaryFile.objects.filter(project=project, lookup=lookup)
-            if matches.exists():
-                temp_file = matches[0]
-            else:
-                with tempfile.TemporaryDirectory() as temp_dir:
-                    media_util = MediaUtil(video, temp_dir, quality)
-                    fp = media_util.getClip(frameRanges)
-
-                    temp_file = TemporaryFile.from_local(fp, "clip.mp4", project, request.user, lookup=lookup, hours=24)
-
-            responseData = TemporaryFileSerializer(temp_file, context={"view": self}).data
-            response = Response(responseData)
-        except ObjectDoesNotExist as dne:
-            response=Response({"message": "Video Not Found"},
-                              status=status.HTTP_404_NOT_FOUND)
-            logger.warning(traceback.format_exc())
-        except Exception as e:
-            response=Response({"message" :str(e), "details": traceback.format_exc()},
-                              status=status.HTTP_400_BAD_REQUEST)
-            logger.warning(traceback.format_exc())
-        finally:
-            return response
