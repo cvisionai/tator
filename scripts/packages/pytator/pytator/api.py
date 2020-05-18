62b8a422e2ecc5407d4a0bc6d61bc0db569066f0
Brian Tate <brian.tate@cvisionai.com>
2020-05-18 14:08:37 -0400

    Send progress every tenth of a percent in download

--- scripts/packages/pytator/pytator/api.py
+++ scripts/packages/pytator/pytator/api.py
@@ -20,6 +20,7 @@ import os
 import progressbar
 import pandas as pd
 import numpy as np
+import math
 
 from pytator.md5sum import md5_sum
 from itertools import count
@@ -401,10 +402,21 @@ class Media(APIElement):
         # Supply token here for eventual media authorization
         with requests.get(url, stream=True, headers=self.headers) as r:
             r.raise_for_status()
+            total_size = r.headers['Content-Length']
+            total_chunks = math.ceil(int(total_size) / 8192)
+            chunk_count = 0
+            last_progress = 0
+            yield last_progress
             with open(out_path, 'wb') as f:
                 for chunk in r.iter_content(chunk_size=8192):
                     if chunk:
+                        chunk_count += 1
                         f.write(chunk)
+                        this_progress = round((chunk_count / total_chunks) *100,1)
+                        if this_progress != last_progress:
+                            yield this_progress
+                            last_progress = this_progress
+            yield 100
 
     def uploadFile(self, typeId, filePath, waitForTranscode=True, progressBars=True, md5=None,section=None, fname=None):
         """ Upload a new file to Tator """
