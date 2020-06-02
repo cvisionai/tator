e831acea5ce3a9c634bcdee3c82de4b25c3029b8
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-06-02 18:02:02 +0000

    Move attribute type properties into separate file, remove attribute type schema

---
+++ main/schema/_attribute_type.py
@@ -0,0 +1,113 @@
+attribute_type_properties = {
+    'name': {
+        'description': 'Name of the attribute.',
+        'type': 'string',
+    },
+    'description': {
+        'description': 'Description of the attribute.',
+        'type': 'string',
+        'default': '',
+    },
+    'dtype': {
+        'description': 'Data type of the attribute.',
+        'type': 'string',
+        'enum': ['bool', 'int', 'float', 'enum', 'string',
+                 'datetime', 'geopos'],
+    },
+    'required': {
+        'description': 'True if this attribute is required for POST requests.',
+        'type': 'boolean',
+        'default': False,
+    },
+    'order': {
+        'description': 'Integer specifying relative order this attribute '
+                       'is displayed in the UI. Negative values are hidden '
+                       'by default.',
+        'type': 'integer',
+        'default': 0,
+    },
+    'default': {
+        'description': 'Default value for the attribute.',
+        'oneOf': [
+            {'type': 'boolean'},
+            {'type': 'number'},
+            {'type': 'string'},
+            {'type': 'array', 'items': {'type': 'number'}, 'minLength': 2, 'maxLength': 2},
+        ]
+    },
+    'minimum': {
+        'description': 'Lower bound for int or float dtype.',
+        'type': 'number',
+    },
+    'maximum': {
+        'description': 'Upper bound for int or float dtype.',
+        'type': 'number',
+    },
+    'choices': {
+        'description': 'Array of possible values for enum dtype.',
+        'type': 'array',
+        'items': {'type': 'string'},
+    },
+    'labels': {
+        'description': 'Array of labels for enum dtype.',
+        'type': 'array',
+        'items': {'type': 'string'},
+    },
+    'autocomplete': {
+        'description': 'Object indicating URL of autocomplete service '
+                       'for string dtype.',
+        'type': 'object',
+    },
+    'use_current': {
+        'description': 'True to use current datetime as default for '
+                       'datetime dtype.',
+        'type': 'boolean',
+    },
+}
+
+attribute_type_example = [
+    {
+        'name': 'My Boolean',
+        'dtype': 'bool',
+        'default': False,
+    },
+    {
+        'name': 'My Integer',
+        'dtype': 'int',
+        'default': 0,
+        'minimum': -1,
+        'maximum': 1,
+    },
+    {
+        'name': 'My Float',
+        'dtype': 'float',
+        'default': 0.0,
+        'minimum': -1.0,
+        'maximum': 1.0,
+    },
+    {
+        'name': 'My Enumeration',
+        'dtype': 'enum',
+        'default': 'a',
+        'choices': ['a', 'b', 'c'],
+        'labels': ['a', 'b', 'c'],
+    },
+    {
+        'name': 'My String',
+        'dtype': 'string',
+        'default': '---',
+        'autocomplete': {
+            'serviceUrl': 'https://www.example.com/suggestion',
+        },
+    },
+    {
+        'name': 'My Datetime',
+        'dtype': 'datetime',
+        'use_current': True,
+    },
+    {
+        'name': 'My Geoposition',
+        'dtype': 'geopos',
+        'default': [-179.0, 90.0],
+    },
+]
