477a6a5621839f62980188526b32fd326816750e
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-06-07 15:03:05 +0000

    Use base views on user endpoint

--- main/rest/user.py
+++ main/rest/user.py
@@ -7,25 +7,42 @@ from ..serializers import UserSerializerBasic
 from ..schema import UserDetailSchema
 from ..schema import CurrentUserSchema
 
+from ._base_views import BaseDetailView
 from ._permissions import UserPermission
 
-class UserDetailAPI(RetrieveUpdateAPIView):
+class UserDetailAPI(BaseDetailView):
     """ Interact with an individual user.
     """
     schema = UserDetailSchema()
-    serializer_class = UserSerializerBasic
     queryset = User.objects.all()
     permission_classes = [UserPermission]
     lookup_field = 'id'
     http_method_names = ['get', 'patch']
 
-class CurrentUserAPI(APIView):
+    def _get(self, params):
+        user = User.objects.get(pk=params['id'])
+        return UserSerializerBasic(user).data
+
+    def _patch(self, params):
+        user = User.objects.get(pk=params['id'])
+        if 'username' in params:
+            user.username = params['username']
+        if 'first_name' in params:
+            user.first_name = params['first_name']
+        if 'last_name' in params:
+            user.last_name = params['last_name']
+        if 'email' in params:
+            user.email = params['email']
+        user.save()
+        return {'message': f'Updated user {params["id"]} successfully!'}
+
+class CurrentUserAPI(BaseDetailView):
     """ Returns the current user.
 
         This is the equivalent of a whoami() operation.
     """
     schema = CurrentUserSchema()
+    http_method_names = ['get']
 
-    def get(self, request, format=None, **kwargs):
-        return Response(UserSerializerBasic(request.user).data)
-
+    def _get(self, params):
+        return UserSerializerBasic(self.request.user).data
