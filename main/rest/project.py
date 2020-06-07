24ee019d7c7a67141e067d10850459e55248c86d
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-06-07 13:04:32 +0000

    Use base views for project endpoint

--- main/rest/project.py
+++ main/rest/project.py
@@ -1,24 +1,16 @@
-import traceback
-
-from rest_framework.generics import ListCreateAPIView
-from rest_framework.generics import RetrieveUpdateDestroyAPIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.core.exceptions import PermissionDenied
-from django.core.exceptions import ObjectDoesNotExist
-
 from ..models import Project
 from ..models import Membership
 from ..models import Permission
 from ..serializers import ProjectSerializer
 from ..schema import ProjectListSchema
 from ..schema import ProjectDetailSchema
-from ..schema import parse
 
+from ._base_views import BaseListView
+from ._base_views import BaseDetailView
 from ._permissions import ProjectFullControlPermission
 from ._permissions import ProjectOwnerPermission
 
-class ProjectListAPI(ListCreateAPIView):
+class ProjectListAPI(BaseListView):
     """ Interact with a list of projects.
 
         Projects are the object under which all data in Tator is grouped, including user
@@ -27,46 +19,33 @@ class ProjectListAPI(ListCreateAPIView):
 
         Project lists return all projects that the requesting user has access to.
     """
-    serializer_class = ProjectSerializer
     schema = ProjectListSchema()
+    http_method_names = ['get', 'post']
 
-    def post(self, request, format=None, **kwargs):
-        response=Response({})
+    def _get(self, params):
+        projects = self.get_queryset()
+        return ProjectSerializer(projects).data
 
-        try:
-            params = parse(request)
+    def _post(self, params):
+        if Project.objects.filter(
+            membership__user=self.request.user).filter(name=params['name']).exists():
+            raise Exception("Project with this name already exists!")
 
-            if Project.objects.filter(
-                membership__user=self.request.user).filter(name=params['name']).exists():
-                raise Exception("Project with this name already exists!")
+        project = Project.objects.create(
+            **params,
+            creator=self.request.user,
+            size=0,
+            num_files=0,
+        )
+        project.save()
 
-            project = Project.objects.create(
-                **params,
-                creator=self.request.user,
-                size=0,
-                num_files=0,
-            )
-            project.save()
-
-            member = Membership(
-                project=project,
-                user=self.request.user,
-                permission=Permission.FULL_CONTROL,
-            )
-            member.save()
-            response = Response(
-                {'message': f"Project {params['name']} created!", 'id': project.id},
-                status=status.HTTP_201_CREATED,
-            )
-
-        except ObjectDoesNotExist as dne:
-            response=Response({'message' : str(dne)},
-                              status=status.HTTP_404_NOT_FOUND)
-        except Exception as e:
-            response=Response({'message' : str(e),
-                               'details': traceback.format_exc()}, status=status.HTTP_400_BAD_REQUEST)
-        finally:
-            return response;
+        member = Membership(
+            project=project,
+            user=self.request.user,
+            permission=Permission.FULL_CONTROL,
+        )
+        member.save()
+        return {'message': f"Project {params['name']} created!", 'id': project.id}
 
     def get_queryset(self):
         memberships = Membership.objects.filter(user=self.request.user)
@@ -74,7 +53,7 @@ class ProjectListAPI(ListCreateAPIView):
         projects = Project.objects.filter(pk__in=project_ids).order_by("created")
         return projects
 
-class ProjectDetailAPI(RetrieveUpdateDestroyAPIView):
+class ProjectDetailAPI(BaseDetailView):
     """ Interact with an individual project.
 
         Projects are the object under which all data in Tator is grouped, including user
@@ -84,53 +63,24 @@ class ProjectDetailAPI(RetrieveUpdateDestroyAPIView):
         Only the project owner may patch or delete an individual project.
     """
     schema = ProjectDetailSchema()
-    serializer_class = ProjectSerializer
     queryset = Project.objects.all()
     permission_classes = [ProjectFullControlPermission, ProjectOwnerPermission]
     lookup_field = 'id'
     http_method_names = ['get', 'patch', 'delete']
 
-    def get(self, request, format=None, **kwargs):
-        # Try grabbing data from cache
-        try:
-            params = parse(request)
-            data=self.serializer_class(Project.objects.get(pk=params['id']),
-                                       context=self.get_renderer_context()).data
-            response=Response(data)
-        except ObjectDoesNotExist as dne:
-            response=Response({'message' : str(dne)},
-                              status=status.HTTP_404_NOT_FOUND)
-        except Exception as e:
-            response=Response({'message' : str(e),
-                               'details': traceback.format_exc()}, status=status.HTTP_400_BAD_REQUEST)
-        return response
+    def _get(self, params):
+        return ProjectSerializer(Project.objects.get(pk=params['id']),
+                                 context=self.get_renderer_context()).data
 
-    def patch(self, request, format=None, **kwargs):
-        response = Response({})
-        try:
-            params = parse(request)
-           
-            project = Project.objects.get(pk=params['id']) 
-            if 'name' in params:
-                project.name = params['name']
-            if 'summary' in params:
-                project.summary = params['summary']
-            project.save()
-            response = Response(
-                {'message': f"Project updated successfully!"},
-                status=status.HTTP_200_OK,
-            )
+    def _patch(self, params):
+        project = Project.objects.get(pk=params['id']) 
+        if 'name' in params:
+            project.name = params['name']
+        if 'summary' in params:
+            project.summary = params['summary']
+        project.save()
+        return {'message': f"Project {params['id']} updated successfully!"}
 
-        except PermissionDenied as err:
-            raise
-
-        except ObjectDoesNotExist as dne:
-            response=Response({'message' : str(dne)},
-                              status=status.HTTP_404_NOT_FOUND)
-        except Exception as e:
-            response=Response({'message' : str(e),
-                               'details': traceback.format_exc()}, status=status.HTTP_400_BAD_REQUEST)
-        return response;
-        
-
-
+    def _delete(self, params):
+        Project.objects.get(pk=params['id']).delete()
+        return {'message': f'Project {params["id"]} deleted successfully!'}
