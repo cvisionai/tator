63a86bd1f0f16c26d91f46756c33f7c26d6b2b45
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-06-07 15:16:52 +0000

    Use base views for version endpoint

--- main/rest/user.py
+++ main/rest/user.py
@@ -1,7 +1,3 @@
-from rest_framework.generics import RetrieveUpdateAPIView
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
 from ..models import User
 from ..serializers import UserSerializerBasic
 from ..schema import UserDetailSchema
