8669958dc85c50a0a71a45d6474f1c46a66b8503
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-06-10 17:16:46 +0000

    Add description for job and job group

--- main/schema/job_group.py
+++ main/schema/job_group.py
@@ -1,3 +1,5 @@
+from textwrap import dedent
+
 from rest_framework.schemas.openapi import AutoSchema
 
 from ._message import message_schema
@@ -10,6 +12,19 @@ class JobGroupDetailSchema(AutoSchema):
             operation['operationId'] = 'DeleteJobGroup'
         operation['tags'] = ['Tator']
         return operation
+
+    def get_description(self, path, method):
+        return dedent("""\
+        Cancel a group of background jobs.
+
+        Algorithms and transcodes create argo workflows that are annotated with two
+        uuid1 strings, one identifying the run and the other identifying the group.
+        Jobs that are submitted together have the same group id, but each workflow
+        has a unique run id.
+
+        This endpoint allows the user to cancel a group of jobs using the `group_id` 
+        returned by either the `AlgorithmLaunch` or `Transcode` endpoints.
+        """)
 
     def _get_path_parameters(self, path, method):
         return [{
