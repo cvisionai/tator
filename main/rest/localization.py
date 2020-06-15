07b590b15a082e3566acdb0fd60bd147d1899c1b
Brian Tate <brian.tate@cvisionai.com>
2020-06-15 17:23:54 -0400

    Disable this until parse routine is fixed

--- main/rest/localization.py
+++ main/rest/localization.py
@@ -272,7 +272,7 @@ class LocalizationDetailAPI(BaseDetailView):
         obj = Localization.objects.get(pk=params['id'])
 
         # Patch common attributes.
-        frame = params.get("frame", None)
+        frame = None #params.get("frame", None)
         x = params.get("x", None)
         y = params.get("y", None)
         if frame is not None:
