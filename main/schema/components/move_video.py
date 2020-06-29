19d7386ee0183b814b9924a4a0515a1b12dde9c4
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-06-29 21:26:31 +0000

    Remove url/segments_url as separate arguments to move video

--- main/schema/components/move_video.py
+++ main/schema/components/move_video.py
@@ -1,16 +1,8 @@
 move_video_spec = {
     'type': 'object',
-    'required': ['media_files', 'url'],
+    'required': ['media_files'],
     'properties': {
         'media_files': {'$ref': '#/components/schemas/MediaFiles'},
-        'url': {
-            'description': 'Upload URL for the file.',
-            'type': 'string',
-        },
-        'segments_url': {
-            'description': 'Upload URL for the segments file if this is a streaming file.',
-            'type': 'string',
-        },
     },
 }
 
