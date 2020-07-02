b57c604fe33740fcb5bfbc211a7fab86c5dd2011
Mark Taipan <mark.taipan@cvisionai.com>
2020-07-02 14:32:59 +0000

    Initial commit

--- main/schema/components/algorithm.py
+++ main/schema/components/algorithm.py
@@ -1,17 +1,33 @@
 algorithm = {
     'type': 'object',
     'properties': {
-        'id': {
-            'type': 'integer',
-            'description': 'Unique integer identifying the algorithm.',
-        },
         'name': {
             'type': 'string',
             'description': 'Name of the algorithm.',
+        },
+        'project': {
+            'type': 'integer',
+            'description': 'Unique integer identifying the project associated with the algorithm.',
+        },
+        'user': {
+            'type': 'integer',
+            'description': 'Unique integer identifying the user registering the algorithm.',
         },
         'description': {
             'type': 'string',
             'description': 'Description of the algorithm.',
         },
-    },
+        'manifest': {
+            'type': 'string',
+            'description': 'URL to argo manifest file (.yaml)',
+        },
+        'cluster': {
+            'type': 'integer',
+            'description': 'Unique integer identifying the cluster.',
+        },
+        'files_per_job': {
+            'type': 'integer',
+            'description': 'Number of media files to be submitted to each workflow.',
+        },
+    }, 
 }
