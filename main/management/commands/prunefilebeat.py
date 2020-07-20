bc66dade62f39160ed2619aff529a230fc0dcac2
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-07-20 19:57:15 +0000

    Add management command to delete old filebeat indices

---
+++ main/management/commands/prunefilebeat.py
@@ -0,0 +1,6 @@
+from django.core.management.base import BaseCommand
+from main.util import clearOldFilebeatIndices
+
+class Command(BaseCommand):
+		def handle(self, **options):
+				clearOldFilebeatIndices()
