1612f2b51429ee4b823b50af1cdabdf9aba32084
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-08-12 18:16:33 +0000

    Update csv import script to use tator-py

--- scripts/transcoder/importDataFromCsv.py
+++ scripts/transcoder/importDataFromCsv.py
@@ -4,7 +4,7 @@ import argparse
 import copy
 
 import pandas as pd
-import pytator
+import tator
 
 import math
 import os
@@ -36,16 +36,16 @@ def formatLocalization(row, media, default_object):
     print(new_obj)
     return new_obj
 
-def uploadStateData(row, media, endpoint, default_object):
+def formatState(row, media, default_object):
     new_obj = copy.copy(default_object)
     new_obj.update(row.to_dict())
     new_obj["media_ids"] = media["id"]
     print(new_obj)
-    endpoint.new(new_obj)
+    return new_obj
     
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description=__doc__)
-    parser = pytator.tator.cli_parser(parser)
+    parser = tator.get_parser()
     parser.add_argument("--media-md5", required=True)
     parser.add_argument("--mode", required=True,choices=["localizations",
                                                          "state"])
@@ -58,53 +58,52 @@ if __name__=="__main__":
         sys.exit(-1)
 
     type_id = os.path.splitext(os.path.basename(args.data_csv))[0]
-    tator = pytator.Tator(args.url, args.token, args.project)
+    api = tator.get_api(args.host, args.token)
 
-    matching_media_elements = tator.Media.filter({"md5": args.media_md5})
+    matching_media_elements = api.get_media_list(md5=args.media_md5)
     if len(matching_media_elements) != 1:
         print(f"Got {matching_media_elements} for {args.media_md5}")
         sys.exit(-1)
 
-    media_element = tator.Media.get(matching_media_elements[0]["id"])
+    media_element = api.get_media(matching_media_elements[0].id)
 
     if args.mode == "state":
-        type_endpoint = tator.StateType
-        endpoint = tator.State
+        type_description = api.get_state_type(type_id)
+        if not isinstance(type_description, tator.models.StateType):
+            print(f"Got {type_description} for {type_id}")
+            sys.exit(-1)
     else:
-        type_endpoint = tator.LocalizationType
-        endpoint = tator.Localization
-
-    print(type_id)
-    matching_types = type_endpoint.filter({"type": type_id})
-    if len(matching_media_elements) != 1:
-        print(f"Got {matching_types} for {type_id}")
-        sys.exit(-1)
-
-    type_description = matching_types[0]
-
+        type_description = api.get_localization_type(type_id)
+        if not isinstance(type_description, tator.models.LocalizationType):
+            print(f"Got {type_description} for {type_id}")
+            sys.exit(-1)
+            
     default_object = make_default_object(type_description)
 
     data_df = pd.read_csv(args.data_csv)
     if args.mode == "state":
-        data_df.apply(uploadStateData, axis=1, args=(media_element, endpoint, default_object))
+        state_df = data_df.apply(formatState, axis=1, args=(media_element, default_object))
+        objects = list(state_df)
+        create_func = api.create_state_list
     else:
         localizations_df = data_df.apply(formatLocalization, axis=1, args=(media_element, default_object))
-        localizations = list(localizations_df)
+        objects = list(localizations_df)
+        create_func = api.create_localization_list
 
-        # Upload in batches no larger than 25
-        upload_count = 0
-        upload_batch = 25
-        batch_count = math.ceil(len(localizations) / 25)
-        for idx in range(batch_count):
-            start_idx = 0+(idx*upload_batch)
-            current_batch=list(localizations[start_idx:start_idx+upload_batch])
-            try:
-                before=time.time()
-                tator.Localization.addMany(current_batch)
-                after=time.time()
-                print(f"Duration={(after-before)*1000}ms")
-            except:
-                traceback.print_exc(file=sys.stdout)
+    # Upload in batches no larger than 500
+    upload_count = 0
+    upload_batch = 500
+    batch_count = math.ceil(len(objects) / 500)
+    for idx in range(batch_count):
+        start_idx = 0+(idx*upload_batch)
+        current_batch=list(objects[start_idx:start_idx+upload_batch])
+        try:
+            before=time.time()
+            create_func(current_batch)
+            after=time.time()
+            print(f"Duration={(after-before)*1000}ms")
+        except:
+            traceback.print_exc(file=sys.stdout)
 
     
     
