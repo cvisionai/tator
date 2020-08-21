3a0f16e81f6401243d38db865cc0f2219d10eade
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-08-21 12:20:18 +0000

    Check for cert update failure

--- scripts/cert.sh
+++ scripts/cert.sh
@@ -4,12 +4,13 @@ openssl genrsa 4096 > /tmp/account.key
 openssl genrsa 4096 > /tmp/domain.key
 openssl req -new -sha256 -key /tmp/domain.key -subj "/CN=$MAIN_HOST" > /tmp/domain.csr
 mkdir -p /data/static/challenges
-python3 /acme_tiny.py --account-key /tmp/account.key \
-                      --csr /tmp/domain.csr \
-                      --acme-dir /data/static/challenges > /tmp/signed_chain.crt
-kubectl create secret generic tls-cert \
-  --from-file=/tmp/signed_chain.crt --dry-run -o yaml | kubectl apply -f -
-kubectl create secret generic tls-key \
-  --from-file=/tmp/domain.key --dry-run -o yaml | kubectl apply -f -
-kubectl rollout restart deployment nginx
+if python3 /acme_tiny.py --account-key /tmp/account.key \
+                         --csr /tmp/domain.csr \
+                         --acme-dir /data/static/challenges > /tmp/signed_chain.crt; then
+  kubectl create secret generic tls-cert \
+    --from-file=/tmp/signed_chain.crt --dry-run -o yaml | kubectl apply -f -
+  kubectl create secret generic tls-key \
+    --from-file=/tmp/domain.key --dry-run -o yaml | kubectl apply -f -
+  kubectl rollout restart deployment nginx
+fi
 
