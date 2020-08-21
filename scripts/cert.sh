f71f63628bbaf69c166128d2e892d7da1e296391
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-08-21 06:40:51 +0000

    Initial script for certificate renewal

---
+++ scripts/cert.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+openssl genrsa 4096 > /tmp/account.key
+openssl genrsa 4096 > /tmp/domain.key
+openssl req -new -sha256 -key domain.key -subj "/CN=$MAIN_HOST" > /tmp/domain.csr
+mkdir -p /data/static/challenges
+python3 /acme_tiny.py --account-key /tmp/account.key \
+                      --csr /tmp/domain.csr \
+                      --acme-dir /data/static/challenges > /tmp/signed_chain.crt
+kubectl create secret generic tls-cert \
+  --from-file=/tmp/signed_chain.crt --dry-run -o yaml | kubectl apply -f -
+kubectl create secret generic tls-key \
+  --from-file=/tmp/domain.key --dry-run -o yaml | kubectl apply -f -
+
