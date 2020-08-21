0338da70310129eae2fb1f5fe1dbfd3b407161c9
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-08-21 07:09:29 +0000

    Do rolling restart of nginx deployment

--- scripts/cert.sh
+++ scripts/cert.sh
@@ -11,4 +11,5 @@ kubectl create secret generic tls-cert \
   --from-file=/tmp/signed_chain.crt --dry-run -o yaml | kubectl apply -f -
 kubectl create secret generic tls-key \
   --from-file=/tmp/domain.key --dry-run -o yaml | kubectl apply -f -
+kubectl rollout restart deployment nginx
 
