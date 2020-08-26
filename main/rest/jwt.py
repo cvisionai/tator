e7b3484d4978167056b62e7f4bb5abc4f6e2cf21
Brian Tate <brian.tate@cvisionai.com>
2020-08-26 11:40:34 -0400

    fetch user for gateway endpoint

--- main/rest/jwt.py
+++ main/rest/jwt.py
@@ -3,6 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.conf import settings
 
+from django_cognito_jwt.validator import TokenError,TokenValidator
+
+from ..models import User
+
 import requests
 
 class JwtGatewayAPI(APIView):
@@ -25,7 +29,19 @@ class JwtGatewayAPI(APIView):
                                              "scope": "openid",
                                              "redirect_uri": "https://btate.duckdns.org/jwt-gateway"})
             try:
-                return Response(token_resp.json(),status=token_resp.status_code)
-            except:
-                return Response({"message": token_resp.text, "code": token_resp.status_code},status=token_resp.status_code)
+                aws = token_resp.json()
+                response={"aws": aws}
+                id_token = aws['id_token']
+                validator = TokenValidator(settings.COGNITO_AWS_REGION,
+                                           settings.COGNITO_USER_POOL,
+                                           settings.COGNITO_AUDIENCE)
+                jwt_payload = validator.validate(id_token)
+                response.update({"jwt": jwt_payload})
+                user = User.objects.get_or_create_for_cognito(jwt_payload)
+                response.update({"user": user.username})
+                return Response(response,status=token_resp.status_code)
+            except TokenError as e:
+                return Response({"message": "invalid token"},status=status.HTTP_404_NOT_FOUND)
+            #except Exception as e:
+            #    return Response({"message": str(e)},status=token_resp.status_code)
     
