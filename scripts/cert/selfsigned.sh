cb82e077b621b25ec8855ed71ab1d6ca1a62b700
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-09-02 19:19:23 +0000

    Use default values for environment variables if they are not supplied in certificate scripts

--- scripts/cert/selfsigned.sh
+++ scripts/cert/selfsigned.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# The following must be supplied:
-#   DOMAIN - domain name
-#   DOMAIN_KEY - path to domain key file
-#   SIGNED_CHAIN - path to signed certficate file
-#   CERT_SECRET_NAME - name of kubernetes secret containing signed certificate
-#   KEY_SECRET_NAME - name of kubernetes secret containing key
+# The following may be supplied:
+#   DOMAIN - domain name (defaults to www.tatorapp.com)
+#   DOMAIN_KEY - path to domain key file (defaults to /tmp/domain.key)
+#   SIGNED_CHAIN - path to signed certficate file (defaults to /tmp/signed_chain.crt)
+#   CERT_SECRET_NAME - name of kubernetes secret containing signed certificate (defaults to tls-cert)
+#   KEY_SECRET_NAME - name of kubernetes secret containing key (defaults to tls-key)
 #
 # Example:
 #   DOMAIN=mysite.duckdns.org \
@@ -14,6 +14,41 @@
 #   CERT_SECRET_NAME=tls-cert \
 #   KEY_SECRET_NAME=tls-key \
 #   ./selfsigned.sh
+
+if [ -z $DOMAIN ]; then
+  echo "DOMAIN not supplied, will be set to default www.tatorapp.com!"
+  DOMAIN=www.tatorapp.com
+else
+  echo "DOMAIN is set to '$DOMAIN'"
+fi
+
+if [ -z $DOMAIN_KEY ]; then
+  echo "DOMAIN_KEY not supplied, will be set to default /tmp/domain.key!"
+  DOMAIN_KEY=/tmp/domain.key
+else
+  echo "DOMAIN_KEY is set to '$DOMAIN_KEY'"
+fi
+
+if [ -z $SIGNED_CHAIN ]; then
+  echo "SIGNED_CHAIN not supplied, will be set to default /tmp/signed_chain.key!"
+  SIGNED_CHAIN=/tmp/signed_chain.key
+else
+  echo "SIGNED_CHAIN is set to '$SIGNED_CHAIN'"
+fi
+
+if [ -z $CERT_SECRET_NAME ]; then
+  echo "CERT_SECRET_NAME not supplied, will be set to default tls-cert!"
+  CERT_SECRET_NAME=tls-cert
+else
+  echo "CERT_SECRET_NAME is set to '$CERT_SECRET_NAME'"
+fi
+
+if [ -z $KEY_SECRET_NAME ]; then
+  echo "KEY_SECRET_NAME not supplied, will be set to default tls-key!"
+  KEY_SECRET_NAME=tls-key
+else
+  echo "KEY_SECRET_NAME is set to '$KEY_SECRET_NAME'"
+fi
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/CN=$DOMAIN" -keyout $DOMAIN_KEY -out $SIGNED_CHAIN
 kubectl create secret generic $CERT_SECRET_NAME \
