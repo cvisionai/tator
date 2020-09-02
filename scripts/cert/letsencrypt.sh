336c8dd635ec42c3113116a0c24a8000858ab7cd
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-09-02 05:03:46 +0000

    Modify certificate scripts to accept filename and secret names as environment variables

--- scripts/cert/letsencrypt.sh
+++ scripts/cert/letsencrypt.sh
@@ -1,21 +1,34 @@
 #!/usr/bin/env bash
 
+# The following must be supplied:
+#   DOMAIN - domain name
+#   DOMAIN_KEY - path to domain key file
+#   SIGNED_CHAIN - path to signed certficate file
+#   CERT_SECRET_NAME - name of kubernetes secret containing signed certificate
+#   KEY_SECRET_NAME - name of kubernetes secret containing key
+#
+# Example:
+#   DOMAIN=mysite.duckdns.org \
+#   DOMAIN_KEY=/tmp/domain.key \
+#   SIGNED_CHAIN=/tmp/signed_chain.crt \
+#   CERT_SECRET_NAME=tls-cert \
+#   KEY_SECRET_NAME=tls-key \
+#   ./letsencrypt.sh
+
 ACCOUNT_KEY=/tmp/account.key
-DOMAIN_KEY=/tmp/domain.key
 DOMAIN_CSR=/tmp/domain.csr
-SIGNED_CHAIN=/tmp/signed_chain.crt
 CHALLENGES_DIR=/data/static/challenges
 
 openssl genrsa 4096 > $ACCOUNT_KEY
 openssl genrsa 4096 > $DOMAIN_KEY
-openssl req -new -sha256 -key $DOMAIN_KEY -subj "/CN=$MAIN_HOST" > $DOMAIN_CSR
+openssl req -new -sha256 -key $DOMAIN_KEY -subj "/CN=$DOMAIN" > $DOMAIN_CSR
 mkdir -p $CHALLENGES_DIR
 if python3 /acme_tiny.py --account-key $ACCOUNT_KEY \
                          --csr $DOMAIN_CSR \
                          --acme-dir $CHALLENGES_DIR > $SIGNED_CHAIN; then
-  kubectl create secret generic tls-cert \
+  kubectl create secret generic $CERT_SECRET_NAME \
     --from-file=$SIGNED_CHAIN --dry-run -o yaml | kubectl apply -f -
-  kubectl create secret generic tls-key \
+  kubectl create secret generic $KEY_SECRET_NAME \
     --from-file=$DOMAIN_KEY --dry-run -o yaml | kubectl apply -f -
   if kubectl rollout restart deployment nginx; then
     echo "Starting nginx rollout!"
--- scripts/cert/selfsigned.sh
+++ scripts/cert/selfsigned.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
-DOMAIN_KEY=/tmp/domain.key
-SIGNED_CHAIN=/tmp/signed_chain.crt
+# The following must be supplied:
+#   DOMAIN - domain name
+#   DOMAIN_KEY - path to domain key file
+#   SIGNED_CHAIN - path to signed certficate file
+#   CERT_SECRET_NAME - name of kubernetes secret containing signed certificate
+#   KEY_SECRET_NAME - name of kubernetes secret containing key
+#
+# Example:
+#   DOMAIN=mysite.duckdns.org \
+#   DOMAIN_KEY=/tmp/domain.key \
+#   SIGNED_CHAIN=/tmp/signed_chain.crt \
+#   CERT_SECRET_NAME=tls-cert \
+#   KEY_SECRET_NAME=tls-key \
+#   ./selfsigned.sh
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/CN=$MAIN_HOST" -keyout $DOMAIN_KEY -out $SIGNED_CHAIN
-kubectl create secret generic tls-cert \
+kubectl create secret generic $CERT_SECRET_NAME \
   --from-file=$SIGNED_CHAIN --dry-run -o yaml | kubectl apply -f -
-kubectl create secret generic tls-key \
+kubectl create secret generic $KEY_SECRET_NAME \
   --from-file=$DOMAIN_KEY --dry-run -o yaml | kubectl apply -f -
 if kubectl rollout restart deployment nginx; then
   echo "Starting nginx rollout!"
