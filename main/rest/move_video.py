84610f36fd7c347f59ce9ef78bd083588c763588
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-09-10 00:53:11 +0000

    Get variables working in move video endpoint

--- main/rest/move_video.py
+++ main/rest/move_video.py
@@ -1,10 +1,12 @@
 import logging
 import os
 import mimetypes
+import random
 from uuid import uuid1
 
 from rest_framework.authtoken.models import Token
 from django.conf import settings
+from urllib import parse as urllib_parse
 
 from ..kube import TatorMove
 from ..models import Media
@@ -67,38 +69,31 @@ class MoveVideoAPI(BaseListView):
                     ext = mimetypes.guess_extension(video_def['codec_mime'].split(';')[0])
                 else:
                     ext = os.path.splitext(media.name)[1]
-                upload_base = os.path.basename(video_def['url'])
                 path = f"{project}/{str(uuid1())}{ext}"
                 dst = os.path.join(get_destination_path(settings.RAW_ROOT), path)
-                upload_uid = get_upload_uid(video_def['url'])
+                upload_uid = get_upload_uid(urllib_parse.urlsplit(video_def['url']).path)
                 move_list.append({
-                    'url': upload_base,
+                    'url': video_def['url'],
                     'dst': dst,
                     'upload_uid': upload_uid,
                 })
                 video_def['path'] = dst
                 del video_def['url']
-                logger.info(f"ARCHIVAL UPLOAD")
-                logger.info(f"URL: {upload_base}")
-                logger.info(f"DST: {dst}")
-                logger.info(f"UPLOAD_UID: {upload_uid}")
         if 'streaming' in media_files:
             for video_def in media_files['streaming']:
-                upload_base = os.path.basename(video_def['url'])
-                segment_upload_base = os.path.basename(video_def['segments_url'])
                 uuid = str(uuid1())
                 path = f"{project}/{uuid}.mp4"
                 segment_info = f"{project}/{uuid}_segments.json"
                 dst = os.path.join(get_destination_path(settings.MEDIA_ROOT), path)
                 segment_dst = os.path.join(get_destination_path(settings.MEDIA_ROOT), segment_info)
-                upload_uid = get_upload_uid(video_def['url'])
-                segment_upload_uid = get_upload_uid(video_def['segments_url'])
+                upload_uid = get_upload_uid(urllib_parse.urlsplit(video_def['url']).path)
+                segment_upload_uid = get_upload_uid(urllib_parse.urlsplit(video_def['segments_url']).path)
                 move_list += [{
-                    'url': upload_base,
+                    'url': video_def['url'],
                     'dst': dst,
                     'upload_uid': upload_uid,
                 }, {
-                    'url': segment_upload_base,
+                    'url': video_def['segments_url'],
                     'dst': segment_dst,
                     'upload_uid': segment_upload_uid,
                 }]
@@ -106,19 +101,11 @@ class MoveVideoAPI(BaseListView):
                 video_def['segment_info'] = '/media/' + segment_info
                 del video_def['url']
                 del video_def['segments_url']
-                logger.info(f"STREAMING UPLOAD")
-                logger.info(f"URL: {upload_base}")
-                logger.info(f"DST: {dst}")
-                logger.info(f"UPLOAD_UID: {upload_uid}")
-                logger.info(f"URL: {segment_upload_base}")
-                logger.info(f"DST: {segment_dst}")
-                logger.info(f"UPLOAD_UID: {segment_upload_uid}")
         if 'audio' in media_files:
             for audio_def in media_files['audio']:
-                upload_base = os.path.basename(audio_def['url'])
                 path = f"{project}/{str(uuid1())}.m4a"
                 dst = os.path.join(get_destination_path(settings.MEDIA_ROOT), path)
-                upload_uid = get_upload_uid(audeo_def['url'])
+                upload_uid = get_upload_uid(urllib_parse.urlsplit(audio_def['url']).path)
                 move_list.append({
                     'url': audio_def['url'],
                     'dst': dst,
@@ -126,17 +113,11 @@ class MoveVideoAPI(BaseListView):
                 })
                 audio_def['path'] = '/media/' + path
                 del audio_def['url']
-                logger.info(f"AUDIO UPLOAD")
-                logger.info(f"URL: {upload_base}")
-                logger.info(f"DST: {dst}")
-                logger.info(f"UPLOAD_UID: {upload_uid}")
 
         # Create the move workflow
-        """
         response = TatorMove().move_video(project, params['id'], str(token), move_list,
                                           media_files, media.gid, media.uid)
 
-        """
         response_data = {'message': f"Moving video for media {params['id']} in workflow "
                                     f"{response['metadata']['name']}!",
                          'id': params['id']}
