e2ae1c96c715a828627fc949f1bc975e066bc16b
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-09-10 12:46:50 +0000

    Move destination path selector into upload utils

--- main/rest/_uploads.py
+++ main/rest/_uploads.py
@@ -1,5 +1,6 @@
 """ Utilities to interact with uploaded files from rest endpoints. """
 import subprocess
+import os
 
 from rest_framework.authtoken.models import Token
 from urllib import parse as urllib_parse
@@ -21,3 +22,22 @@ def download_uploaded_file(url, user, dst):
            '-O', f'{dst}',
            url]
     subprocess.run(cmd, check=True)
+
+def get_destination_path(default, project):
+    """ Selects random destination media path with a given default.
+
+    :param default: The default path if shards are not enabled.
+    :param project: Project directory. This path will be created if it does not exist.
+    :returns: Subpath corresponding to shard location (does not include project).
+    """
+    # Select a shard or use default.
+    media_shards = os.getenv('MEDIA_SHARDS')
+    if media_shards is None:
+        path = default
+    else:
+        path = f"/{random.choice(media_shards.split(','))}"
+    # Make sure project path exists.
+    project_path = os.path.join(path, str(project))
+    os.makedirs(project_path, exist_ok=True)
+    return path
+
