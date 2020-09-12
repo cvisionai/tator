2d52e764b96ad69c7052bddecc67e79a66d0de85
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-09-12 21:06:04 +0000

    Add functions to retrieve file paths of uploads and create symlinks to uploads given an upload url

--- main/uploads.py
+++ main/uploads.py
@@ -1,12 +1,17 @@
 """ Utilities to interact with uploaded files from rest endpoints. """
+import logging
 import subprocess
 import random
+import socket
+import requests
 import os
 
 from rest_framework.authtoken.models import Token
 from urllib import parse as urllib_parse
 
 from .cache import TatorCache
+
+logger = logging.getLogger(__name__)
 
 def download_uploaded_file(url, user, dst):
     """ Attempts to download the uploaded file using the given token.
@@ -42,3 +47,30 @@ def get_destination_path(default, project):
     os.makedirs(project_path, exist_ok=True)
     return path
 
+def get_file_path(url, token):
+    """ Given an upload url, finds the path on disk.
+    """
+    upload_shards = os.getenv('UPLOAD_SHARDS')
+    if upload_shards is None:
+        # No media shards, the path is /uploads
+        path = '/uploads'
+    else:
+        # Get metadata for this URL.
+        parsed = urllib_parse.urlsplit(url)
+        upload_uid = TatorCache().get_upload_uid_cache(parsed.path)
+        response = requests.head(f"{urllib_parse.urljoin('http://nginx-internal-svc', parsed.path)}",
+                                 allow_redirects=True,
+                                 headers={'Authorization': f'Token {token}',
+                                          'Upload-Uid': f'{upload_uid}'})
+        upstream = response.headers['X-Upstream']
+        logger.info(f"Upstream for URL {url} is {upstream}")
+        hostname = socket.getfqdn(upstream.split(':')[0])
+        logger.info(f"Hostname for URL {url} is {hostname}")
+        path = f'/{hostname.split("-")[1]}'
+    path += f'/{parsed.path.split("/")[-1]}'
+    logger.info(f"Path is {path}")
+    return path
+
+def make_symlink(url, token, dst):
+    src = get_file_path(url, token)
+    os.symlink(src, dst)
