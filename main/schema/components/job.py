c637e2f0830443bac5c285a5131afedaf0f6991c
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-09-20 16:44:36 +0000

    Define a schema for jobs

--- main/schema/components/job.py
+++ main/schema/components/job.py
@@ -1,4 +1,63 @@
+job_node = {
+    'type': 'object',
+    'description': 'Represents a step or node (for DAGs) in a job.',
+    'properties': {
+        'id': {
+            'description': 'Unique identifier of the job node.',
+            'type': 'string',
+        },
+        'children': {
+            'type': 'array',
+            'items': {
+                'description': 'ID of child node.',
+                'type': 'string',
+            },
+        },
+        'task': {
+            'description': 'Name of task performed by this node.',
+            'type': 'string',
+        },
+        'status': {
+            'description': 'Status of this node.',
+            'type': 'string',
+        },
+        'start_time': {
+            'description': 'Start time of this node.',
+            'type': 'string',
+            'nullable': True,
+        },
+        'stop_time': {
+            'description': 'Stop time of this node.',
+            'type': 'string',
+            'nullable': True,
+        },
+    }
+}
+
 job = {
     'type': 'object',
-    'additionalProperties': True,
+    'properties': {
+        'id': {
+            'description': 'Unique identifier of the job.',
+            'type': 'string',
+        },
+        'nodes': {
+            'type': 'array',
+            'items': {'$ref': '#/components/schemas/JobNode'},
+        },
+        'status': {
+            'description': 'Status of this job.',
+            'type': 'string',
+        },
+        'start_time': {
+            'description': 'Start time of this job.',
+            'type': 'string',
+            'nullable': True,
+        },
+        'stop_time': {
+            'description': 'Stop time of this job.',
+            'type': 'string',
+            'nullable': True,
+        },
+    },
 }
