a039fb5e8f25bd1cf798c35b4d177a123403f58b
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-09-30 15:46:27 +0000

    Add cache functions for jobs

--- main/cache.py
+++ main/cache.py
@@ -20,7 +20,7 @@ class TatorCache:
         key = f'creds_{project_id}_{user_id}'
         val = None
         if self.rds.hexists(group, key):
-            val = self.rds.hget(group,key)
+            val = self.rds.hget(group, key)
             if val == 'True':
                 val = True
             else:
@@ -55,6 +55,59 @@ class TatorCache:
             uid = uid.decode()
         return uid
 
+    def set_job(self, job):
+        """ Stores a job for cancellation or authentication. Job is a dict including
+            uid, gid, user id, project id, algorithm id (-1 if not an algorithm), 
+            and time created.
+        """
+        val = json.dumps(job)
+        uid = job['uid']
+        gid = job['gid']
+        project = job['project']
+
+        # Set the job data by UID.
+        self.rds.hset('jobs', uid, val)
+
+        # Store list of UIDs under GID key.
+        if self.rds.exists(gid):
+            self.rds.append(gid, f',{uid}')
+        else:
+            self.rds.set(gid, uid)
+
+        # Store list of UIDs under project key.
+        project_key = f'jobs_{project}'
+        if self.rds.exists(project_key):
+            self.rds.append(project_key, f',{uid}')
+        else:
+            self.rds.set(project_key, uid)
+
+    def get_jobs_by_uid(self, uid):
+        """ Retrieves job using UID.
+        """
+        val = self.rds.hget('jobs', uid).decode()
+        return [json.loads(val)]
+
+    def get_jobs_by_gid(self, gid, first_only=False):
+        """ Retrieves jobs using GID. Set first_only=True to only retrieve first job.
+        """
+        uids = self.rds.get(gid).decode().split(',')
+        if first_only:
+            jobs = [json.loads(self.rds.hget('jobs', uids[0]).decode())]
+        else:
+            jobs = [json.loads(self.rds.hget('jobs', uid).decode()) for uid in uids]
+        return jobs
+
+    def get_jobs_by_project(self, project, first_only=False):
+        """ Retrieves jobs using project ID. Set first_only=True to only retrieve first job.
+        """
+        project_key = f'jobs_{project}'
+        uids = self.rds.get(project_key).decode().split(',')
+        if first_only:
+            jobs = [json.loads(self.rds.hget('jobs', uids[0]).decode())]
+        else:
+            jobs = [json.loads(self.rds.hget('jobs', uid).decode()) for uid in uids]
+        return jobs
+            
     def invalidate_all(self):
         """Invalidates all caches.
         """
