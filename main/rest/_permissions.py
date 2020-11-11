0dafc19da82c9f723de1df0a5573d354a9f6ceaa
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-11-11 13:43:09 +0000

    Add special permission class for cloning media

--- main/rest/_permissions.py
+++ main/rest/_permissions.py
@@ -167,3 +167,21 @@ class UserPermission(BasePermission):
 
         return False
 
+class ClonePermission(ProjectPermissionBase):
+    """ Special permission that checks for transfer permission in two
+        projects.
+    """
+    message = "Insufficient permission to clone media between these projects."
+    insufficient_permissions = [Permission.VIEW_ONLY, Permission.CAN_EDIT]
+
+    def has_permission(self, request, view):
+        src_project = view.kwargs['project']
+        src_project = get_object_or_404(Project, pk=int(src_project))
+        dst_project = request.body['project']
+        dst_project = get_object_or_404(Project, pk=int(dst_project))
+        return (self._validate_project(request, src_project)
+                and self._validate_project(request, dst_project))
+
+    def has_object_permission(self, request, view, obj):
+        return False
+    
