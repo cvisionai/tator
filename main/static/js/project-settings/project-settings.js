e3d1d6111c63f4575e1dc9d3f66a1688ed063365
Erin Butler <erin.butler@cvisionai.com>
2020-12-04 18:18:37 -0500

    project settings progress fetch projects and mediatypes

--- main/static/js/project-settings/project-settings.js
+++ main/static/js/project-settings/project-settings.js
@@ -1,54 +1,132 @@
+class ProjectEditName extends TatorElement {
+  constructor() {
+    super();
+  }
+
+  init(val, t){
+    console.log("/////////INIT PROJECT MAIN SETTTINGS OUTPUT//////////////////");
+    console.log(val);
+
+  }
+}
+
+customElements.define("project-edit-name", ProjectEditName);
+
+///////////
+
+
+
+
+class ProjectEditMediaTypes extends TatorElement {
+  constructor() {
+    super();
+  }
+
+  init(val, t){
+    console.log("###### INIT MEDIA TYPES OUTPUT ##############################");
+    console.log(val);
+  
+  }
+}
+
+customElements.define("project-edit-media-types", ProjectEditMediaTypes);
+
+
+///////////
+
 class ProjectSettings extends TatorPage {
   constructor() {
     super();
 
     const main = document.createElement("main");
     main.setAttribute("class", "layout-max py-4");
+    main.id = "mainId";
     this._shadow.appendChild(main);
 
     const header = document.createElement("div");
     header.setAttribute("class", "main__header d-flex flex-items-center flex-justify-center py-6");
     main.appendChild(header);
 
+    // Project Settings h1.
     const h1 = document.createElement("h1");
     h1.setAttribute("class", "h1");
     header.appendChild(h1);
 
     this._headerText = document.createTextNode("");
+    this._headerText.nodeValue = `Set rules and configurations.`;
     h1.appendChild(this._headerText);
-
-    this._progress = document.createElement("progress-summary");
-    this._shadow.insertBefore(this._progress, main);
   }
 
+  /** UNDER DEVELOPMENT **/
   static get observedAttributes() {
-    return ["project-id"].concat(TatorPage.observedAttributes);
+    return ["project-id", "token"].concat(TatorPage.observedAttributes);
+  }
+
+  _init() {
+    console.log("running init");
+    const projectId = this.getAttribute("project-id");
+    console.log(projectId);
+    // Project ID used by init promises.
+
+
+    // Get info about the project.
+    const projectPromise = fetch("/rest/Project/" + projectId, {
+      method: "GET",
+      credentials: "same-origin",
+      headers: {
+        "X-CSRFToken": getCookie("csrftoken"),
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      }
+    });
+    // Get media type list.
+    const mediaTypesPromise = fetch("/rest/MediaTypes/" + projectId, {
+      method: "GET",
+      credentials: "same-origin",
+      headers: {
+        "X-CSRFToken": getCookie("csrftoken"),
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      }
+    });
+
+    Promise.all([
+      projectPromise,
+      mediaTypesPromise
+    ])
+    .then( ([projectResponse, mediaTypesResponse]) => {
+      const projectData = projectResponse.json();
+      const mediaTypesData = mediaTypesResponse.json();
+
+      Promise.all( [projectData, mediaTypesData] )
+      .then( ([project, mediaTypes]) => {
+        console.log(project);
+        // Edit block for project name and description.
+        const projectEditBlock = document.createElement("project-edit-name");
+        projectEditBlock.init(project, this);
+
+        console.log(mediaTypes, this);
+        // Edit block for mediaTypes
+        // Loops over returned object creatinf and appending media Type
+        const mediaTypesBlock = document.createElement("project-edit-media-types");
+        mediaTypesBlock.init(mediaTypes);
+      })
+    });
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
     TatorPage.prototype.attributeChangedCallback.call(this, name, oldValue, newValue);
     switch (name) {
+      case "username":
+        break;
       case "project-id":
-        // Get info about the project.
-        fetch("/rest/Project/" + newValue, {
-          method: "GET",
-          credentials: "same-origin",
-          headers: {
-            "X-CSRFToken": getCookie("csrftoken"),
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-          }
-        })
-        .then(response => response.json())
-        .then(data => {
-          this._headerText.nodeValue = data.name + " Settings";
-          this._collaborators.usernames = data.usernames;
-        })
-        .catch(err => console.log("Failed to retrieve project data: " + err));
-
+        this._init();
+        break;
+      case "token":
         break;
     }
   }
+
 }
 
 customElements.define("project-settings", ProjectSettings);
