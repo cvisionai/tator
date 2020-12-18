0e3cb8893f1516b09426939fdc9fbdea51700e39
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-12-18 13:16:38 +0000

    Replace tabs with spaces

--- main/management/commands/buildsearchindices.py
+++ main/management/commands/buildsearchindices.py
@@ -2,10 +2,10 @@ from django.core.management.base import BaseCommand
 from main.util import buildSearchIndices
 
 class Command(BaseCommand):
-		def add_arguments(self, parser):
-				parser.add_argument('project_id', type=int)
-				parser.add_argument('section', type=str)
+    def add_arguments(self, parser):
+        parser.add_argument('project_id', type=int)
+        parser.add_argument('section', type=str)
         parser.add_argument('chunk', type=int)
 
-		def handle(self, **options):
-				buildSearchIndices(options['project_id'], options['section'], 'index', options['chunk'])
+    def handle(self, **options):
+        buildSearchIndices(options['project_id'], options['section'], 'index', options['chunk'])
