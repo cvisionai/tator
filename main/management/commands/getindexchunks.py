0e948f3103459bb6ed1587e3a27bc2ad8632691d
Brian Tate <brian.tate@cvisionai.com>
2020-12-30 17:21:57 -0500

    Add missing cmd line arg

--- main/management/commands/getindexchunks.py
+++ main/management/commands/getindexchunks.py
@@ -5,6 +5,7 @@ class Command(BaseCommand):
 		def add_arguments(self, parser):
 				parser.add_argument('project_id', type=int)
 				parser.add_argument('section', type=str)
+        parser.add_argument('max_age_days', type=int)
 
 		def handle(self, **options):
-				print(list(range(get_num_index_chunks(options['project_id'], options['section']))))
+				print(list(range(get_num_index_chunks(options['project_id'], options['section'], options['max_age_days']))))
