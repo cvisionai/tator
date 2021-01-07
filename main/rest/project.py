514255ed391bfffa6e0554c24e8fa26b1d293f92
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-01-07 15:47:16 +0000

    Replace project serializer with function based on database_qs

--- main/rest/project.py
+++ main/rest/project.py
@@ -8,13 +8,27 @@ from ..models import Organization
 from ..models import Affiliation
 from ..models import Permission
 from ..models import Media
-from ..serializers import ProjectSerializer
+from ..models import database_qs
 from ..schema import ProjectListSchema
 from ..schema import ProjectDetailSchema
+from ..s3 import TatorS3
 
 from ._base_views import BaseListView
 from ._base_views import BaseDetailView
 from ._permissions import ProjectFullControlPermission
+
+def _serialize_projects(projects, user_id):
+    project_data = database_qs(projects)
+    s3 = TatorS3()
+    for idx, project in enumerate(projects):
+        if project.creator.pk == user_id:
+            project_data[idx]['permission'] = 'Creator'
+        else:
+            project_data[idx]['permission'] = str(project.user_permission(user_id))
+        del project_data[idx]['attribute_type_uuids']
+        if project_data[idx]['thumb']:
+            project_data[idx]['thumb'] = s3.get_download_url(project_data[idx]['thumb'], 28800)
+    return project_data
 
 class ProjectListAPI(BaseListView):
     """ Interact with a list of projects.
@@ -30,7 +44,7 @@ class ProjectListAPI(BaseListView):
 
     def _get(self, params):
         projects = self.get_queryset()
-        return ProjectSerializer(projects, many=True, context=self.get_renderer_context()).data
+        return _serialize_projects(projects, self.request.user.pk)
 
     def _post(self, params):
         # If user does not have admin privileges within the organization, raise a 403.
@@ -84,8 +98,8 @@ class ProjectDetailAPI(BaseDetailView):
     http_method_names = ['get', 'patch', 'delete']
 
     def _get(self, params):
-        return ProjectSerializer(Project.objects.get(pk=params['id']),
-                                 context=self.get_renderer_context()).data
+        projects = Project.objects.filter(pk=params['id'])
+        return _serialize_projects(projects, self.request.user.pk)[0]
 
     @transaction.atomic
     def _patch(self, params):
