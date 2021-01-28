3eb359986f6118a3f9ea0edaeb62883958137559
Ubuntu <ubuntu@ip-172-26-2-245.us-east-2.compute.internal>
2021-01-28 15:35:40 +0000

    file clean up with prettier, and removed a inherited methods

--- main/static/js/project-settings/project-main-edit.js
+++ main/static/js/project-settings/project-main-edit.js
@@ -6,50 +6,50 @@ class ProjectMainEdit extends SettingsSection {
 
     // New heading element.
     this.h1 = document.createElement("h1");
-    this.h1.setAttribute("class", "h2 pb-3");
+    this.h1.setAttribute("class", "h2 pb-3"); 
     this.settingsSectionDiv.appendChild(this.h1);
 
     // Name the Main Div.
     this.settingsSectionDiv.id = "projectMain";
-    this.h1.innerHTML = `Project details.`;
+    this.h1.innerHTML = `Project details.`; 
 
-    this._shadow.appendChild(this.settingsSectionDiv);
+    this._shadow.appendChild(this.settingsSectionDiv); 
   }
 
-  _getSectionForm(){
+  _getSectionForm() {
     this.boxOnPage = this.boxHelper.boxWrapDefault({
-        "children" : document.createTextNode("")
-    });
+      "children": document.createTextNode("")
+    }); 
 
     //
     let _form = document.createElement("form");
-    _form.id = "project-"+this.data.id;
-    this.boxOnPage.appendChild( _form );
+    _form.id = "project-" + this.data.id;
+    this.boxOnPage.appendChild(_form); 
 
     // Image upload
-    this._thumbInput = this._getHiddenThumbInput( this.data.thumb );
-    _form.appendChild( this._thumbInput );
+    this._thumbInput = this._getHiddenThumbInput(this.data.thumb);
+    _form.appendChild(this._thumbInput);
     this._thumbEdit = this._getThumbnailEdit()
-    _form.appendChild( this._thumbEdit );
+    _form.appendChild(this._thumbEdit);
 
     // Input for name
-    this._editName = this._setNameInput( this._getNameFromData() );
-    _form.appendChild( this._editName );
+    this._editName = this._setNameInput(this._getNameFromData());
+    _form.appendChild(this._editName);
 
     // Input for name
-    this._editSummary = this._setSummaryInput( this._getSummaryFromData() );
-    _form.appendChild( this._editSummary );
+    this._editSummary = this._setSummaryInput(this._getSummaryFromData());
+    _form.appendChild(this._editSummary);
 
     // Enable downloads at project level,
-    this._downloadEnable = this._setDownloadInput( );
-    _form.appendChild( this._downloadEnable );
+    this._downloadEnable = this._setDownloadInput();
+    _form.appendChild(this._downloadEnable);
 
     let formElements = [this._editName, this._editSummary];
 
     return this.boxOnPage;
   }
 
-  _fetchGetPromise({id = this.projectId} = {}){
+  _fetchGetPromise({ id = this.projectId } = {}) {
     return fetch("/rest/Project/" + id, {
       method: "GET",
       credentials: "same-origin",
@@ -61,10 +61,10 @@ class ProjectMainEdit extends SettingsSection {
     });
   }
 
-  _fetchPatchPromise({id = this.projectId} = {}){
+  _fetchPatchPromise({ id = this.projectId } = {}) {
     let data = this._getFormData();
 
-    if(Object.entries(data).length === 0){
+    if (Object.entries(data).length === 0) {
       return false;
     } else {
       // Set in child element,
@@ -83,35 +83,35 @@ class ProjectMainEdit extends SettingsSection {
 
   }
 
-  _setDownloadInput({data = this.data} = {}){
+  _setDownloadInput({ data = this.data } = {}) {
     return this.inputHelper.inputRadioSlide({
-      "value" : data.enable_downloads,
-      "labelText" : "Enable Download",
-      "name" : "enable_downloads"
+      "value": data.enable_downloads,
+      "labelText": "Enable Download",
+      "name": "enable_downloads"
     });
   }
 
-  _getThumbnailEdit({data = this.data} = {}){
+  _getThumbnailEdit({ data = this.data } = {}) {
     let key = "thumb";
     return this.inputHelper.editImageUpload({
-      "value" : data.thumb, // img path
-      "labelText" : "Thumbnail",
-      "name" : key,
-      "forId" : key
+      "value": data.thumb, // img path
+      "labelText": "Thumbnail",
+      "name": key,
+      "forId": key
 
     });
   }
 
-  _getHiddenThumbInput(thumb){
+  _getHiddenThumbInput(thumb) {
     let key = "thumb";
     let styleHidden = "position: absolute; left: -99999rem";
-    let input = this.inputHelper.inputText( {
+    let input = this.inputHelper.inputText({
       "labelText": null,
       "name": key,
       "value": thumb,
       "type": "file",
-      "forId" : key
-    } );
+      "forId": key
+    });
 
     input.style = styleHidden;
     input.id = key;
@@ -123,28 +123,28 @@ class ProjectMainEdit extends SettingsSection {
     return input;
   }
 
-  _getThumbInputValue(){
+  _getThumbInputValue() {
     return this._thumbInput.value;
   }
 
-  _getThumbValueData({ data = this.data} = {}){
+  _getThumbValueData({ data = this.data } = {}) {
     return data.thumb;
   }
 
-  _getDownloadEnableValue(){
+  _getDownloadEnableValue() {
     let radioSet = this._shadow.querySelectorAll(".radio-slide-wrap input");
 
-    for(let s of radioSet){
-      if(s.id.indexOf("on") > -1 && s.checked == true) return true
-      if(s.id.indexOf("off") > -1 && s.checked == true) return false
+    for (let s of radioSet) {
+      if (s.id.indexOf("on") > -1 && s.checked == true) return true
+      if (s.id.indexOf("off") > -1 && s.checked == true) return false
     }
   }
 
-  _getDownloadEnableValueData({ data = this.data} = {}){
+  _getDownloadEnableValueData({ data = this.data } = {}) {
     return data.enable_downloads;
   }
 
-  _thumbnailPreview( event ){
+  _thumbnailPreview(event) {
     let outputElement = this._thumbEdit.querySelector(".projects__image");
     outputElement.src = URL.createObjectURL(event.target.files[0]);
     // outputElement.onload = function() {
@@ -153,77 +153,70 @@ class ProjectMainEdit extends SettingsSection {
     return outputElement;
   }
 
-  reset(){
-    this._setNameInputValue( this._getNameFromData() );
-    this._setSummaryInputValue( this._getSummaryFromData() );
+  reset() {
+    this._setNameInputValue(this._getNameFromData());
+    this._setSummaryInputValue(this._getSummaryFromData());
     console.log("[Reset with previously fetched project data.]");
   }
 
-  resetHard(){
+  resetHard() {
     this._fetchNewProjectData();
     this.reset();
     console.log("[Reset with newly fetched project data.]");
   }
 
-  _nameChanged(){
-    if(this._getNameInputValue() === this._getNameFromData()) return false;
+
+  // input methods for unique 
+  _summaryChanged() {
+    if (this._getSummaryInputValue() === this._getSummaryFromData()) return false;
+    return true;
+  }
+  _downloadEnabledChanged() {
+    if (this._getDownloadEnableValue() == this._getDownloadEnableValueData()) return false;
     return true;
   }
 
-  _summaryChanged(){
-    if(this._getSummaryInputValue() === this._getSummaryFromData()) return false;
-    return true;
-  }
-  _downloadEnabledChanged(){
-    if(this._getDownloadEnableValue() == this._getDownloadEnableValueData()) return false;
+  _thumbInputChanged() {
+    if (this._getThumbInputValue() === this._getThumbValueData()) return false;
     return true;
   }
 
-  _thumbInputChanged(){
-    if(this._getThumbInputValue() === this._getThumbValueData()) return false;
-     return true;
-  }
-
-  _getFormData(){
+  // save and formdata
+  _getFormData() {
     let formDataJSON = {};
 
-    if(this._nameChanged()) formDataJSON.name = this._getNameInputValue();
-    if(this._summaryChanged()) formDataJSON.summary = this._getSummaryInputValue();
-    if(this._downloadEnabledChanged()) formDataJSON.enable_downloads = this._getDownloadEnableValue();
-    if(this._thumbInputChanged()) {
+    if (this._nameChanged()) formDataJSON.name = this._getNameInputValue();
+    if (this._summaryChanged()) formDataJSON.summary = this._getSummaryInputValue();
+    if (this._downloadEnabledChanged()) formDataJSON.enable_downloads = this._getDownloadEnableValue();
+    if (this._thumbInputChanged()) {
       let thumbVal = this._getThumbInputValue();
-      if(thumbVal != "" && thumbVal != null)formDataJSON.thumb = thumbVal;
+      if (thumbVal != "" && thumbVal != null) formDataJSON.thumb = thumbVal;
     }
 
     return formDataJSON;
   }
 
-  _changed(){
-    return  true;
-  }
+  _save({ id = -1 } = {}) {
+    console.log(`Project _save method for id: ${id}`);
+    const patch = this._fetchPatchPromise({ "id": id });
 
-  _save({id = -1} = {}){
-    console.log("Default _save method for id: "+id);
-    const patch = this._fetchPatchPromise({"id":id});
-
-    // Check if anything changed
-    if(patch != false){
+    if (patch != false) {
       patch.then(response => {
-          return response.json().then( data => {
-            console.log("Save response status: "+response.status)
-            if (response.status == "200") {
-              this._modalSuccess(data.message);
-              this._fetchNewProjectData();
-            } else {
-              this._modalError(data.message);
-            }
-          })
-        }
+        return response.json().then(data => {
+          console.log("Save response status: " + response.status)
+          if (response.status == "200") {
+            this._modalSuccess(data.message);
+            this._fetchNewProjectData();
+          } else {
+            this._modalError(data.message);
+          }
+        })
+      }
       )
-      .catch(error => {
-        console.log('Error:', error.message);
-        this._modalError("Internal error: "+error.message);
-      });
+        .catch(error => {
+          console.log('Error:', error.message);
+          this._modalError("Internal error: " + error.message);
+        });
     } else {
       this._modalSuccess("Nothing new to save!")
     }
