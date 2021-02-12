a2fd51f3c0242c54f1e0b8013c750ad8b09dd43a
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-02-12 17:12:57 +0000

    Don't set cognito client if cognito disabled

--- main/cognito.py
+++ main/cognito.py
@@ -9,16 +9,13 @@ class TatorCognito:
     """
     @classmethod
     def setup_cognito(cls):
-        if os.getenv('COGNITO_ENABLED'):
+        if os.getenv('COGNITO_ENABLED') == 'TRUE':
             with open('/cognito/cognito.yaml', 'r') as f:
                 cls.config = yaml.safe_load(f)
             cls.cognito = boto3.client('cognito-idp',
                                         region_name=cls.config['aws-region'],
                                         aws_access_key_id=cls.config['access-key'],
                                         aws_secret_access_key=cls.config['secret-key'])
-        else:
-            # Client generator will not have env variables defined
-            cls.cognito = boto3.client('cognito-idp')
 
     def create_user(self, user):
         response = self.cognito.admin_create_user(UserPoolId=self.config['pool-id'],
