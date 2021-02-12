80d0a46936e18c58c85cf3377bad520242bff120
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-02-12 18:49:38 +0000

    make email_verified an argument, raise exception if cognito_id already set, allow setting temp password

--- main/cognito.py
+++ main/cognito.py
@@ -17,12 +17,17 @@ class TatorCognito:
                                         aws_access_key_id=cls.config['access-key'],
                                         aws_secret_access_key=cls.config['secret-key'])
 
-    def create_user(self, user):
+    def create_user(self, user, email_verified=False, temp_pw=None):
+        if user.cognito_id:
+            raise RuntimeError("Cannot create cognito ID for user {user}, cognito_id already exists!")
+        pw_args = {}
+        if temp_pw:
+            pw_args['TemporaryPassword'] = temp_pw
         response = self.cognito.admin_create_user(UserPoolId=self.config['pool-id'],
                                                   UserName=user.username,
                                                   UserAttributes=[{
                                                      'Name': 'email_verified',
-                                                     'Value': False,
+                                                     'Value': email_verified,
                                                   }, {
                                                      'Name': 'email',
                                                      'Value': user.email,
@@ -32,7 +37,8 @@ class TatorCognito:
                                                   }, {
                                                      'Name': 'family_name',
                                                      'Value': user.last_name,
-                                                  }])
+                                                  }],
+                                                  **pw_args)
         return response
 
 TatorCognito.setup_cognito()
