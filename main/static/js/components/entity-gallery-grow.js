a78cf15851070455e7f464623fbc73f1c158184c
ermbutler <erm.butler@gmail.com>
2021-03-01 00:27:57 +0000

    grow card tool

--- main/static/js/components/entity-gallery-grow.js
+++ main/static/js/components/entity-gallery-grow.js
@@ -8,13 +8,38 @@ class EntityCardGrow extends TatorElement {
 
       this._slideInput = document.createElement("input");
       this._slideInput.setAttribute("type", "range");
-      this._slideInput.setAttribute("min", "1");
-      this._slideInput.setAttribute("max", "100");
-      this._slideInput.setAttribute("value", "50");
+      this._slideInput.setAttribute("min", "50");
+      this._slideInput.setAttribute("max", "250");
+      this._slideInput.setAttribute("value", "150");
       this._slideInput.setAttribute("class", "slider");
       this._slideInput.setAttribute("id", "growAnnotationImages");
       this._sliderContainer.appendChild(this._slideInput);
+
+      // Init holder for gallery UL
+      this._gallery = document.createElement("ul");
       
+    }
+
+    _initGallery(galleryUL){
+      this._gallery = galleryUL
+      
+
+      this._slideInput.addEventListener("change", this._rangeHandler.bind(this));
+    }
+
+    _rangeHandler(e){
+      let growValue = e.target.value;
+      let growValuePerc = parseFloat( growValue / 100 );
+
+      // Gallery UL by default
+      // * grid-template-columns: repeat(auto-fill,minmax(272px,1fr));
+      const defaultMinMax = 272;
+      let newMinMax =  defaultMinMax * growValuePerc;
+
+      //Img height by default .project__file img
+      // height: 130px
+
+      return this._gallery.style.gridTemplateColumns = `repeat(auto-fill,minmax(${newMinMax}px,1fr))`;
     }
    
   }
