2e0bfe50b30323ff62d9b63ec9e23a71d0b0cf42
ermbutler <erm.butler@gmail.com>
2021-03-03 19:09:58 +0000

    cleaned up input for color with commenting and in append order; added color preview for default using hex for TEAL from annotation.js

--- main/static/js/project-settings/settings-input-helpers.js
+++ main/static/js/project-settings/settings-input-helpers.js
@@ -77,46 +77,11 @@ class SettingsInput {
       // unspecified default
       value = "#ffffff";
     }
-
-    // Container
-    const colorInput = document.createElement("div");
-    const forId = this._getUniqueIdentifier(name);
-
-    // Choose color label
-    const labelForColor = document.createElement("label");
-    labelForColor.setAttribute("for", forId);
-    labelForColor.setAttribute("class", "px-2");
-    labelForColor.innerHTML = "Default: "
-
-    // Color radio option
-    const inputColorRadio = document.createElement("input");
-    inputColorRadio.setAttribute("type", "radio");
-    inputColorRadio.setAttribute("value", "color");
-    inputColorRadio.checked = specified ? true : false;
-    inputColorRadio.setAttribute("name", "unspecifiedColor");
-    inputColorRadio.setAttribute("id", "inputColorRadio");
-    inputColorRadio.setAttribute("class", "px-2");
-
-    // Color input
-    const inputTextElement = document.createElement("input");
-    inputTextElement.setAttribute("type", type);
-    inputTextElement.setAttribute("value", value);
-    inputTextElement.setAttribute("name", name);
-    inputTextElement.setAttribute("id", forId);
-
-    //
-    colorInput.setAttribute("class", "float-left col-md-9 col-sm-12 dflex");
-    colorInput.appendChild(inputColorRadio);
-    colorInput.appendChild(inputTextElement);
-
+    
+    /* Unspecified Option*/
     // Container
     const unspecColorInput = document.createElement("div");
-
-    // Unspecified label
-    const labelForUnspec = document.createElement("label");
-    labelForUnspec.setAttribute("for", "unspecifiedColor");
-    labelForUnspec.setAttribute("class", "px-2");
-    labelForUnspec.innerHTML = "N/A"
+    unspecColorInput.setAttribute("class", "float-left col-md-5 col-sm-12");
 
     // Unspecified radio option
     const inputElementUnspec = document.createElement("input");
@@ -127,28 +92,77 @@ class SettingsInput {
     inputElementUnspec.setAttribute("id", "unspecifiedColor");
     //labelForUnspec.append(inputElementUnspec);
 
-    // 
-    unspecColorInput.setAttribute("class", "float-left col-md-3 col-sm-12");
+    // Unspecified label
+    const labelForUnspec = document.createElement("label");
+    labelForUnspec.setAttribute("for", "unspecifiedColor");
+    labelForUnspec.setAttribute("class", "px-2");
+    labelForUnspec.innerHTML = "Default: "
+
+    // Unspecified Color preview Input
+    const unspecPreview = document.createElement("input");
+    unspecPreview.setAttribute("type", type);
+    unspecPreview.setAttribute("value", "#40E0D0");
+    unspecPreview.disabled = true;
+    //unspecPreview.setAttribute("id", forId);
+
+    // Unspecified Container contents
     unspecColorInput.appendChild(inputElementUnspec);
     unspecColorInput.appendChild(labelForUnspec);
+    unspecColorInput.appendChild(unspecPreview);
 
+    /* Choose your own Option*/
+    // Container
+    const colorInput = document.createElement("div");
+    const forId = this._getUniqueIdentifier(name);
+    colorInput.setAttribute("class", "float-left col-md-7 col-sm-12 dflex");
+
+    // Choose Color radio option
+    const inputColorRadio = document.createElement("input");
+    inputColorRadio.setAttribute("type", "radio");
+    inputColorRadio.setAttribute("value", "color");
+    inputColorRadio.checked = specified ? true : false;
+    inputColorRadio.setAttribute("name", "unspecifiedColor");
+    inputColorRadio.setAttribute("id", "inputColorRadio");
+    inputColorRadio.setAttribute("class", "px-2");
+
+    // Choose color label
+    const labelForColor = document.createElement("label");
+    labelForColor.setAttribute("for", forId);
+    labelForColor.setAttribute("class", "px-2");
+    labelForColor.innerHTML = "Choose: "
+
+    // Choose Color input
+    const inputTextElement = document.createElement("input");
+    inputTextElement.setAttribute("type", type);
+    inputTextElement.setAttribute("value", value);
+    inputTextElement.setAttribute("name", name);
+    inputTextElement.setAttribute("id", forId);
+
+    // Choose Container contents
+    colorInput.appendChild(inputColorRadio);
+    colorInput.appendChild(labelForColor);
+    colorInput.appendChild(inputTextElement);
+
+    /* Listeners */
     // Uncheck unspecifed when using color picker
     inputTextElement.addEventListener("click", () => {
       inputElementUnspec.checked = false;
       inputColorRadio.checked = true;
     });
+    // Open color picker if radio button clicked
     inputColorRadio.addEventListener("input", (e) => {     
       if(e.target.value){
         inputTextElement.click();
       }
     });
 
-   // Put into 2 cols
-   const colorInputs = document.createElement("div");
-   colorInputs.setAttribute("class", "clearfix col-md-9 col-sm-4")
-   colorInputs.appendChild(unspecColorInput);
-   colorInputs.appendChild(colorInput);
+    // Put into 2 cols
+    const colorInputs = document.createElement("div");
+    colorInputs.setAttribute("class", "clearfix col-md-9 col-sm-4")
+    colorInputs.appendChild(unspecColorInput);
+    colorInputs.appendChild(colorInput);
 
+    // Add Settings page Label
     if(labelText == null){
       // No label also means inline warning needs to be set separately
       return colorInputs;
