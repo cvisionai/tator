da1b04f04c699d057a9fdc9423dbbe861f386cb9
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-03-05 15:12:17 +0000

    Accept bucket object in `TatorS3` constructor rather than using static variables

--- main/s3.py
+++ main/s3.py
@@ -6,24 +6,32 @@ import boto3
 class TatorS3:
     """Interface for object storage.
     """
-    @classmethod
-    def setup_s3(cls):
-        endpoint = os.getenv('OBJECT_STORAGE_HOST')
-        region = os.getenv('OBJECT_STORAGE_REGION_NAME')
-        access_key = os.getenv('OBJECT_STORAGE_ACCESS_KEY')
-        secret_key = os.getenv('OBJECT_STORAGE_SECRET_KEY')
+    def __init__(self, bucket=None):
+        """ Creates the S3 interface.
+        """
+        if bucket is None:
+            endpoint = os.getenv('OBJECT_STORAGE_HOST')
+            region = os.getenv('OBJECT_STORAGE_REGION_NAME')
+            access_key = os.getenv('OBJECT_STORAGE_ACCESS_KEY')
+            secret_key = os.getenv('OBJECT_STORAGE_SECRET_KEY')
+            self.bucket_name = os.getenv('BUCKET_NAME')
+        else:
+            endpoint = bucket.endpoint_url
+            region = bucket.region
+            access_key = bucket.access_key
+            secret_key = bucket.secret_key
+            self.bucket_name = bucket.name
         if endpoint:
-            cls.s3 = boto3.client('s3',
-                          endpoint_url=f'{endpoint}',
-                          region_name=region,
-                          aws_access_key_id=access_key,
-                          aws_secret_access_key=secret_key)
+            self.s3 = boto3.client('s3',
+                                   endpoint_url=f'{endpoint}',
+                                   region_name=region,
+                                   aws_access_key_id=access_key,
+                                   aws_secret_access_key=secret_key)
         else:
             # Client generator will not have env variables defined
-            cls.s3 = boto3.client('s3')
+            self.s3 = boto3.client('s3')
 
     def get_download_url(self, path, expiration):
-        bucket_name = os.getenv('BUCKET_NAME')
         external_host = os.getenv('OBJECT_STORAGE_EXTERNAL_HOST')
         if os.getenv('REQUIRE_HTTPS') == 'TRUE':
             PROTO = 'https'
@@ -31,7 +39,7 @@ class TatorS3:
             PROTO = 'http'
         # Generate presigned url.
         url = self.s3.generate_presigned_url(ClientMethod='get_object',
-                                             Params={'Bucket': bucket_name,
+                                             Params={'Bucket': self.bucket_name,
                                                      'Key': path},
                                              ExpiresIn=expiration)
         # Replace host if external host is given.
@@ -41,4 +49,3 @@ class TatorS3:
             url = urlunsplit(parsed)
         return url
 
-TatorS3.setup_s3()
