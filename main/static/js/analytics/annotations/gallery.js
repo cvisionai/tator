9f8cfc80339e31feb750690d5063b647aa00d5fc
ermbutler <erm.butler@gmail.com>
2021-03-13 00:18:23 +0000

    rside behavior

--- main/static/js/analytics/annotations/gallery.js
+++ main/static/js/analytics/annotations/gallery.js
@@ -12,21 +12,37 @@ class AnnotationsGallery extends EntityCardGallery {
     this._ul.style.gridTemplateColumns = `repeat(auto-fill,minmax(${this.colSize}px,1fr))`
 
     // Heading
+    this._h3.hidden = true;
     //this._h3Text = document.createTextNode("All Annotations")
     //this._h3.appendChild( this._h3Text );
 
+    // Count text
+    this._p.classList.add("col-3");
+    this._p.classList.add("px-2");
+
     // @TODO Tools: Card labels display
-    // this._labelsDropDown = document.createElement('entity-gallery-labels');
-    // this._tools.appendChild( this._labelsDropDown );
+    this.labelContainer = document.createElement("div");
+    this.labelContainer.setAttribute("class", "col-3")
+    this._labelsDropDown = document.createElement('entity-gallery-labels');
+    this.labelContainer.appendChild( this._labelsDropDown );
+    this._tools.appendChild( this.labelContainer );
 
     // Tools: Slider to resize images
+    this.sliderContainer = document.createElement("div");
+    this.sliderContainer.setAttribute("class", "col-3")
     this._resizeCards = document.createElement('entity-card-resize');
     this._resizeCards._initGallery(this._ul, this.colSize);
-    this._tools.appendChild( this._resizeCards );
-
+    this.sliderContainer.appendChild( this._resizeCards );
+    this._tools.appendChild( this.sliderContainer );
+    
     // Tools: Show @aspect ratio
+    this.aspectToolContainer = document.createElement("div");
+    this.aspectToolContainer.setAttribute("class", "col-3")
     this._aspectToggle = document.createElement('entity-gallery-aspect-ratio');
-    this._tools.appendChild( this._aspectToggle );
+    this.aspectToolContainer.appendChild( this._aspectToggle );
+    this._tools.appendChild( this.aspectToolContainer );
+
+    // Init aspect toggle
     this._aspectToggle.init(this);
 
     this.panelContainer = null;
@@ -42,16 +58,18 @@ class AnnotationsGallery extends EntityCardGallery {
   // Provide access to side panel for events
   _initPanel({
     panelContainer,
-    localizationTypes
+    localizationTypes,
+    panelControls
   }){
     this.panelContainer = panelContainer;
+    this.panelControls = panelControls;
 
     // Init gallery with data for filtering
-    // this._labelsDropDown.init({
-    //   gallery : this,
-    //   localizationTypes
-    // });
-    //this.addEventListener("labels-changed", this.handleLabelChange.bind(this));
+    this._labelsDropDown.init({
+      gallery : this,
+      localizationTypes
+    });
+    this.addEventListener("labels-changed", this.handleLabelChange.bind(this));
   }
 
   handleLabelChange(e){
@@ -92,16 +110,63 @@ class AnnotationsGallery extends EntityCardGallery {
     this.makeCards(cardList.cards)
   }
 
-  /**
-   * Updates the specific card's thumbnail image
-   * @param {integer} id
-   * @param {image} image
-   */
-  updateCardImage(id, image) {
-    if (id in this._currentCardIndexes) {
-      var info = this._cardElements[this._currentCardIndexes[id]];
-      info.card.setImage(image);
-      info.annotationPanel.setImage(image);
+  // Accepts a cardList object and appends each card to the page web component
+  appendCardList(cardList){    
+    for(let cardObj of cardList){
+      let card = document.createElement("annotations-card");
+      
+      
+      // Resize Tool needs to change style within card on change
+      this._resizeCards._slideInput.addEventListener("change", (e) => {
+        let resizeValue = e.target.value;
+        let resizeValuePerc = parseFloat( resizeValue / 100 );
+        return card._img.style.height = `${130 * resizeValuePerc}px`;
+      });
+
+      // Inner div of side panel
+      let annotationPanelDiv = document.createElement("div");
+      annotationPanelDiv.setAttribute("class", "entity-panel--div hidden")
+      annotationPanelDiv.setAttribute("data-loc-id", cardObj.id)
+      this.panelContainer.appendChild( annotationPanelDiv );
+
+      // Init a side panel that can be triggered from card
+      let annotationPanel = document.createElement("entity-attributes-panel");
+      annotationPanel.init( cardObj, this.panelContainer );
+      annotationPanelDiv.appendChild(annotationPanel);
+
+      // Update view
+      annotationPanelDiv.addEventListener("unselected", () => {
+        card._li.classList.remove("is-selected");
+        annotationPanelDiv.classList.remove("is-selected");
+        annotationPanelDiv.classList.add("hidden");
+        console.log(annotationPanelDiv.classList);
+        console.log("Hiding "+annotationPanelDiv.dataset.locId);
+      });
+
+      // Open panel if a card is clicked
+      card.addEventListener("click", () => {
+        // if the panel is closed and you click, open it...
+        console.log("attempting to call cardClicked");
+        this.panelControls.cardClicked();
+        console.log(this.panelControls);
+      });
+
+      // Listen for all clicks on the document
+      window.addEventListener('click', function (event) {
+        if (event.target.tagName == "BODY" && card._li.classList.contains("is-selected")) {
+          card._li.click();
+        }
+
+      }, false);
+
+      // Update view
+      this.addEventListener("view-change", (e) => {
+        card._li.classList.toggle("aspect-true");
+      });
+
+      // init and append card
+      card.init( cardObj, this.panelContainer, annotationPanelDiv);
+      this._ul.appendChild(card);
     }
   }
 
---
+++ main/static/js/components/entity-attributes-panel-top.js
@@ -0,0 +1,101 @@
+class EntityAttrPanelTop extends TatorElement {
+    constructor() {
+      super();
+
+        //default state
+        this.open = true;
+
+        // Panel top bar
+        this._topBar = document.createElement("div");
+        this._topBar.setAttribute("class", "entity-panel--container--top-bar  px-3");
+        this._shadow.appendChild(this._topBar);
+
+        // topbar arrow
+        this._topBarArrow = document.createElement("span");
+        this._topBarArrow.setAttribute("class", "top-bar-arrow");
+        const svg = document.createElementNS(svgNamespace, "svg");
+        svg.setAttribute("class", "f2 icon-chevron-right");
+        //svg.setAttribute("viewBox", "0 0 32 32");
+        svg.setAttribute("height", "40px");
+        svg.setAttribute("width", "20px");
+        this._topBarArrow.appendChild(svg);
+    
+        const path = document.createElementNS(svgNamespace, "path");
+        path.setAttribute("d", "M12.943 24.943l8-8c0.521-0.521 0.521-1.365 0-1.885l-8-8c-0.521-0.521-1.365-0.521-1.885 0s-0.521 1.365 0 1.885l7.057 7.057-7.057 7.057c-0.521 0.521-0.521 1.365 0 1.885s1.365 0.521 1.885 0z");
+        svg.appendChild(path);
+        this._topBar.appendChild(this._topBarArrow);
+
+        // Text box
+        this._box = document.createElement("div");
+        this._box.setAttribute("class", "py-6");
+        this._topBar.appendChild(this._box);
+
+        // Panel name
+        this._topBarH3 = document.createElement("h3");
+        this._topBarH3.setAttribute("class", "entity-panel--container--top-bar--h3 text-semibold h3 ");
+        this._topBarH3.appendChild( document.createTextNode("Annotation Viewer") );
+        this._box.appendChild(this._topBarH3);
+
+        // Panel text
+        this._topBarP = document.createElement("p");
+        this._topBarP.setAttribute("class", "entity-panel--container--top-bar--p text-gray py-2 ");
+        this._topBarP.appendChild( document.createTextNode("Hover over localizations in gallery to preview annotations. Click to pin in the viewer.") );
+        this._box.appendChild(this._topBarP); 
+    }
+
+    _toggleRightOnClick({lside, rside}){
+      this.lside = lside;
+      this.rside = rside;
+        // CLOSE
+        /* DEFAULT lside = col-9, and rside = col-2 */
+        this._topBarArrow.addEventListener("click", () => {
+          if(this.open){
+            lside.classList.add("col-12");
+            rside.classList.add("slide-close");
+            lside.classList.remove("col-9");
+            lside.style.marginRight = "2%";
+            this.open = false;
+            this._topBarArrow.style.transform ="scaleX(-1)";
+            return this.open;
+          } else {
+            rside.classList.remove("slide-close");
+            lside.classList.add("col-9");
+            lside.classList.remove("col-12");
+            lside.style.marginRight = "0";
+            this._topBarArrow.style.transform ="scaleX(1)";
+            this.open = true;
+            return this.open;
+          }
+
+        });
+
+        rside.addEventListener("mouseenter", () => {
+          if(rside.classList.contains("slide-close")){
+            rside.classList.add("show-arrow");
+            lside.classList.remove("col-11");
+          }
+        });
+
+        rside.addEventListener("mouseleave", () => {
+          if(rside.classList.contains("slide-close")){
+            rside.classList.remove("show-arrow");
+            lside.classList.remove("col-11");
+          }
+        });
+
+    }
+
+    cardClicked(){
+      console.log("card click made it here!");
+      if(!this.open){
+        this.rside.classList.remove("slide-close");
+        this.lside.classList.add("col-9");
+        this.lside.classList.remove("col-12");
+        this.open = true;
+        return this.open;
+      }
+    }
+   
+  }
+  
+  customElements.define("entity-attr-panel-top", EntityAttrPanelTop);  
