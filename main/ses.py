3b10358e816f3cff4344f405e4f5c508e8f0e8f7
Mark Taipan <mark.taipan@cvisionai.com>
2021-04-05 20:21:07 +0000

    Fixed email attachment bug

--- main/ses.py
+++ main/ses.py
@@ -54,7 +54,7 @@ class TatorSES:
         # #TODO Potentially limit the attachment size(s)
         for attachment in attachments:
             f_p = io.BytesIO()
-            TatorS3().download_fileobj(os.getenv('BUCKET_NAME'), attachment['key'], f_p)
+            TatorS3().download_fileobj(attachment['key'], f_p)
             f_p.seek(0)
             part = MIMEApplication(f_p.read())
             part.add_header('Content-Disposition', 'attachment', filename=attachment['name'])
