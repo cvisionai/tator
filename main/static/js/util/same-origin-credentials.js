5b014a35c7021581f960c97896c35583389e57aa
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-04-09 14:59:40 +0000

    Add same origin credentials function to save some typing

---
+++ main/static/js/util/same-origin-credentials.js
@@ -0,0 +1,10 @@
+function sameOriginCredentials() {
+  return {
+    credentials: "same-origin",
+    headers: {
+      "X-CSRFToken": getCookie("csrftoken"),
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    }
+  };
+}
