8b566536f07dc398ce326e89d15977b7653dbd33
ermbutler <erm.butler@gmail.com>
2021-05-04 15:37:55 +0000

    small visual clean up to view in annotator button and heading in entity gallery panel

--- main/static/js/components/entity-gallery-panel.js
+++ main/static/js/components/entity-gallery-panel.js
@@ -5,76 +5,66 @@ class EntityGalleryPanel extends TatorElement {
       // Panel Container
       this._main = document.createElement("div");
       this._main.setAttribute("class", "entity-panel px-3");
-      //this._main.hidden = true;
+      this._main.style.marginTop = "-20px"
       this._shadow.appendChild(this._main);
 
-      // Content id
-      this._heading = document.createElement("h2");
-      this._heading.setAttribute("class", "h2 py-3")
-      this._main.appendChild(this._heading);
+      // View in media
+      this._mediaLinkEl = document.createElement("a");
+      this._mediaLinkEl.appendChild(document.createTextNode("View In Annotator"));
+      this._mediaLinkEl.setAttribute("class", "text-gray hover-text-white f3 clickable float-right");
+      this._mediaLinkEl.setAttribute("href", "#");
+      this._mediaLinkEl.setAttribute("target", "_blank");
+      this._main.appendChild(this._mediaLinkEl);            
 
-      // Entity Data in Form ( @TODO Editable? or display only )
+      // Entity Data heading
+      const entityHeading = document.createElement("h3");
+      entityHeading.setAttribute("class", "py-3 text-semibold");
+      entityHeading.appendChild(document.createTextNode("Entity Data"));
+      this._main.appendChild(entityHeading);
+
+      // Entity Data in Form
       this.entityData = document.createElement("entity-gallery-panel-form");
       this._main.appendChild(this.entityData);
 
+      // Go to frame icon button
+      this.goToFrameButton = document.createElement("entity-frame-button");
+      this.goToFrameButton.button.setAttribute("class", "btn-clear px-2 py-2 rounded-2 f2 text-white entity__button");
+      this.goToFrameButton.button.setAttribute("tooltip", "View In Annotator");
+      this.goToFrameButton.setAttribute("target", "_blank");
+      this.goToFrameButton.style.marginRight = "16px";
+
       // #TODO Encapsulate this class into a LocalizationGalleryPanel
       const mediaHeading = document.createElement("h3");
-      mediaHeading.setAttribute("class", "h3 py-3");
+      mediaHeading.setAttribute("class", "py-3 text-semibold");
       mediaHeading.style.margintop = "10px";
-      mediaHeading.textContent = "Associated Media";
+      mediaHeading.appendChild(this.goToFrameButton);
+      mediaHeading.appendChild(document.createTextNode("Associated Media"));
       this._main.appendChild(mediaHeading)
-
-      // View media button link to annotation media
-      this._linksDiv = document.createElement("div");
-      this._linksDiv.setAttribute("class", "annotation__panel-group py-1 text-gray f1 d-flex col-11");
-      this._main.appendChild(this._linksDiv);
-
-      const mediaLinkDiv = document.createElement("div");
-      mediaLinkDiv.setAttribute("class", "d-flex flex-items-center py-1");
-      this._linksDiv.appendChild(mediaLinkDiv);
-
-      const mediaLinkLabel = document.createElement("label");
-      mediaLinkLabel.textContent = "View In Annotator";
-      mediaLinkDiv.appendChild(mediaLinkLabel);
-
-      this._mediaLink = document.createElement("a");
-      this._mediaLink.setAttribute("href", "#");
-      const goToFrameButton = document.createElement("entity-frame-button");
-      goToFrameButton.style.marginLeft = "16px";
-      mediaLinkDiv.appendChild(goToFrameButton);
-
+      
+  
+      
+      //this._linksDiv.appendChild(mediaLinkDiv);
       this.mediaData = document.createElement("entity-gallery-panel-form");
       this._main.appendChild(this.mediaData);
-
-      // actions.appendChild(this.viewMedia);
-
-      // // View submission
-      // this.viewSubmission = document.createElement("button");
-      // this.viewSubmission.setAttribute("class", "btn btn-charcoal col-6");
-      // let vsText = document.createTextNode("View Submission");
-      // this.viewSubmission.appendChild(vsText);
-      // actions.appendChild(this.viewSubmission);
-
-      this._mediaLink = "#";
-      goToFrameButton.addEventListener("click", () => {
-        window.location = this._mediaLink;
-      });
     }
 
   async init( {
     cardObj
   }){
-      this.cardObj = cardObj;
-
-      // Heading
-      //this._heading.textContent = `Annotation Information (ID: ${cardObj.id})`;
-
-      // Setup linkout and the entity data for panel here
-      this._mediaLink = this.cardObj.mediaLink;
-      this.entityData._init(this.cardObj);
-      this.mediaData._init(this.cardObj.mediaInfo);
-    }
-
+    this.cardObj = cardObj;
+  
+    // Setup linkout and the entity data for panel here
+    this._mediaLink = this.cardObj.mediaLink;
+    this._mediaLinkEl.setAttribute("href", this._mediaLink );
+    this.goToFrameButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      window.open(this._mediaLink);
+    });
+    
+    this.entityData._init(this.cardObj);
+    this.mediaData._init(this.cardObj.mediaInfo);
   }
 
-  customElements.define("entity-gallery-panel", EntityGalleryPanel);
+}
+
+customElements.define("entity-gallery-panel", EntityGalleryPanel);
