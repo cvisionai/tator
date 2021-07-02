2727c1172d61ed9b0668dc70ac40dae98f4ae4ce
Mark Taipan <mark.taipan@cvisionai.com>
2021-07-02 15:23:56 +0000

    Remove collections and reports button for merge to master

--- main/static/js/analytics/dashboard/dashboard.js
+++ main/static/js/analytics/dashboard/dashboard.js
@@ -1,64 +1,70 @@
 class AnalyticsDashboard extends TatorPage {
-    constructor() {
-      super();
+  constructor() {
+    super();
 
-      this.projectId = window.location.pathname.split("/")[1];
+    this.projectId = window.location.pathname.split("/")[1];
 
-            //
-      // Header
-      //
-      const header = document.createElement("div");
-      this._headerDiv = this._header._shadow.querySelector("header");
-      header.setAttribute("class", "annotation__header d-flex flex-items-center flex-justify-between px-6 f3");
-      const user = this._header._shadow.querySelector("header-user");
-      user.parentNode.insertBefore(header, user);
+          //
+    // Header
+    //
+    const header = document.createElement("div");
+    this._headerDiv = this._header._shadow.querySelector("header");
+    header.setAttribute("class", "annotation__header d-flex flex-items-center flex-justify-between px-6 f3");
+    const user = this._header._shadow.querySelector("header-user");
+    user.parentNode.insertBefore(header, user);
 
-      const div = document.createElement("div");
-      div.setAttribute("class", "d-flex flex-items-center");
-      header.appendChild(div);
+    const div = document.createElement("div");
+    div.setAttribute("class", "d-flex flex-items-center");
+    header.appendChild(div);
 
-      this._breadcrumbs = document.createElement("analytics-breadcrumbs");
-      div.appendChild(this._breadcrumbs);
-      this._breadcrumbs.setAttribute("analytics-name", "Dashboard");
+    this._breadcrumbs = document.createElement("analytics-breadcrumbs");
+    div.appendChild(this._breadcrumbs);
+    this._breadcrumbs.setAttribute("analytics-name", "Dashboard");
 
-      this.main = document.createElement("main");
-      this.main.setAttribute("class", "layout-max py-4 d-flex flex-items-center flex-row");
-      this._shadow.appendChild(this.main);;
+    this.main = document.createElement("main");
+    this.main.setAttribute("class", "layout-max d-flex flex-justify-center");
+    this._shadow.appendChild(this.main);;
 
-      // Annotations
-      const annotationsBox = this._getDashboardBox({
-          name : "Annotations",
-          href : `/${this.projectId}/analytics/annotations`,
-          iconName: "grid-icon"
-        });
-      this.main.appendChild(annotationsBox);
+    // Annotations
+    const annotationsBox = this._getDashboardBox({
+        name : "Annotations",
+        href : `/${this.projectId}/analytics/annotations`,
+        iconName: "grid-icon"
+      });
+    this.main.appendChild(annotationsBox);
 
-      // Collections
-      const collectionsBox = this._getDashboardBox({
-          name : "Collections",
-          href : `/${this.projectId}/analytics/collections`,
-          iconName: "layers-icon"
-        });
-      this.main.appendChild(collectionsBox);
-
-      // Reports #TODO
-      /*
-      const visualizationBox = this._getDashboardBox({
-          name : "Data Visualization",
-          href : `/${this.projectId}/analytics/visualization`,
-          iconName: "bar-chart-icon"
-        });
-      this.main.appendChild(visualizationBox);
+    // Collections
+    /*
+    const collectionsBox = this._getDashboardBox({
+        name : "Collections",
+        href : `/${this.projectId}/analytics/collections`,
+        iconName: "layers-icon",
+        disabled: true,
+      });
+    this.main.appendChild(collectionsBox);
     */
 
-      // Reports
-      const reportsBox = this._getDashboardBox({
-          name : "Reports",
-          href : ``,
-          iconName: "file-text-icon"
-        });
-      this.main.appendChild(reportsBox);
-    }
+    // Visualization
+    /*
+    const visualizationBox = this._getDashboardBox({
+        name : "Data Visualization",
+        href : `/${this.projectId}/analytics/visualization`,
+        iconName: "bar-chart-icon"
+      });
+    this.main.appendChild(visualizationBox);
+  */
+
+    // Reports
+    /*
+    const reportsBox = this._getDashboardBox({
+        name : "Reports",
+        href : ``,
+        iconName: "file-text-icon"
+      });
+    this.main.appendChild(reportsBox);
+    reportsBox.setAttribute("disabled", "");
+    */
+  }
 
   attributeChangedCallback(name, oldValue, newValue) {
     TatorPage.prototype.attributeChangedCallback.call(this, name, oldValue, newValue);
@@ -76,39 +82,45 @@ class AnalyticsDashboard extends TatorPage {
     return ["project-name", "project-id"].concat(TatorPage.observedAttributes);
   }
 
-    _getDashboardBox({name, href, iconName}={}){
-      const height = "2em";
-      const width = "2em";
+  _getDashboardBox({name, href, iconName, disabled}={}){
+    const height = "2em";
+    const width = "2em";
 
-      const iconDiv = document.createElement("div");
-      iconDiv.setAttribute("class", "pb-3 d-block color-white");
+    const iconDiv = document.createElement("div");
+    iconDiv.setAttribute("class", "pb-3 d-block color-white");
 
-      const dashboardIcon = new SvgDefinition({iconName, height, width});
-      iconDiv.appendChild(dashboardIcon);
+    const dashboardIcon = new SvgDefinition({iconName, height, width});
+    iconDiv.appendChild(dashboardIcon);
 
-      const dashboardText = document.createElement("p");
-      dashboardText.setAttribute("class", "h3 analysis__dashboard-text text-normal text-gray");
-      dashboardText.textContent = name;
+    const dashboardText = document.createElement("p");
+    dashboardText.setAttribute("class", "h3 analysis__dashboard-text text-normal text-gray");
+    dashboardText.textContent = name;
 
-      return this._newRoundedBox(iconDiv, dashboardText, href);
-    }
-
-    _newRoundedBox(iconDiv, dashboardText, href){
-      const roundedBox = document.createElement("a");
-      roundedBox.setAttribute("href", href);
-      roundedBox.setAttribute("class", "analysis__dashboard-box d-flex flex-items-center rounded-2");
-
-      const content = document.createElement("div");
-      content.setAttribute("class", "flex-items-center text-center");
-      content.style.width = "100%";
-      roundedBox.appendChild(content);
-
-      content.appendChild(iconDiv);
-      content.appendChild(dashboardText);
-
-      return roundedBox;
-    }
-
+    return this._newRoundedBox(iconDiv, dashboardText, href, disabled);
   }
+
+  _newRoundedBox(iconDiv, dashboardText, href, disabled){
+    var roundedBox;
+    if (disabled) {
+      roundedBox = document.createElement("button");
+    }
+    else {
+      roundedBox = document.createElement("a");
+    }
+    roundedBox.setAttribute("class", "analysis__dashboard-box d-flex flex-items-center rounded-2");
+    roundedBox.setAttribute("href", href);
+
+    const content = document.createElement("div");
+    content.setAttribute("class", "flex-items-center text-center");
+    content.style.width = "100%";
+    roundedBox.appendChild(content);
+
+    content.appendChild(iconDiv);
+    content.appendChild(dashboardText);
+
+    return roundedBox;
+  }
+
+}
 
 customElements.define("analytics-dashboard", AnalyticsDashboard);
