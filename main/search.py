fb767bbebc2f6d722a4b4281f174ffc3e103c35d
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-07-20 21:45:54 +0000

    Set refresh interval explicitly to avoid setting "idle" indices

--- main/search.py
+++ main/search.py
@@ -125,6 +125,7 @@ class TatorSearch:
                     'settings': {
                         'number_of_shards': 1,
                         'number_of_replicas': 1,
+                        'refresh_interval': '1s',
                         'analysis': {
                             'normalizer': {
                                 'lower_normalizer': {
