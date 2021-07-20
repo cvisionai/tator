f9ce28d646b4afd926c4350e5d8b6ff66d44a03b
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-07-19 23:52:06 -0400

    Increment to 0.1.1

--- doc/conf.py
+++ doc/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, CVision AI'
 author = 'CVision AI'
 
 # The short X.Y version
-version = '0.1.0'
+version = '0.1.1'
 # The full version, including alpha/beta/rc tags
 release = ''
 
