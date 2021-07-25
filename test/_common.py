dfc23603a5699480a95fb448451d413d0a8d29d3
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-07-25 05:05:36 +0000

    Simplify shadow tree element search

--- test/_common.py
+++ test/_common.py
@@ -17,25 +17,6 @@ class ShadowManager:
             shadow_root = self.expand_shadow_element(element)
         return shadow_root
 
-    def find_shadow_tree_element(self, dom, by, value):
-        """ Given description of the element you want to find, searches full shadow tree
-            on the page for that element and returns the first one.
-        """
-        element = None
-        # Try to find the specified element in the dom.
-        try:
-            element = dom.find_element(by, value)
-        except NoSuchElementException:
-            # Not found, so keep searching in nested shadow doms.
-            elements = dom.find_elements_by_tag_name("*")
-            for element in elements:
-                shadow = self.expand_shadow_element(element)
-                if shadow:
-                    element = self.find_shadow_tree_element(shadow, by, value)
-                if element:
-                    break
-        return element
-
     def find_shadow_tree_elements(self, dom, by, value):
         """ Given description of the element you want to find, searches full shadow tree
             on the page for any elements that match and returns a list.
@@ -50,3 +31,14 @@ class ShadowManager:
             if shadow:
                 elements += self.find_shadow_tree_elements(shadow, by, value)
         return elements
+
+    def find_shadow_tree_element(self, dom, by, value):
+        """ Given description of the element you want to find, searches full shadow tree
+            on the page for that element and returns the first one.
+        """
+        elements = self.find_shadow_tree_elements(dom, by, value)
+        if len(elements) > 0:
+            element = elements[0]
+        else:
+            raise NoSuchElementException
+        return element
