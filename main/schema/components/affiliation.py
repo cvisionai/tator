a48e125e2c4ffc1859def0a98cb99899ea665014
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-08-25 16:42:31 +0000

    Add user info to `Affiliations` endpoint to be consistent with `Memberships`

--- main/schema/components/affiliation.py
+++ main/schema/components/affiliation.py
@@ -32,6 +32,22 @@ affiliation = {
             'description': 'Unique integer identifying a affiliation.',
         },
         'user': affiliation_properties['user'],
+        'username': {
+            'description': 'Username for the membership.',
+            'type': 'string',
+        },
+        'first_name': {
+            'type': 'string',
+            'description': 'First name of user.',
+        },
+        'last_name': {
+            'type': 'string',
+            'description': 'Last name of user.',
+        },
+        'email': {
+            'type': 'string',
+            'description': 'Email address of user.',
+        },
         'permission': affiliation_properties['permission'],
         'organization': {
             'description': 'Unique integer identifying an organization.',
