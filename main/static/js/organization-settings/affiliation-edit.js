faa54ee80bd0b0331af18091f8452f858483a8bf
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-08-26 20:53:18 +0000

    Fix svg size

--- main/static/js/organization-settings/affiliation-edit.js
+++ main/static/js/organization-settings/affiliation-edit.js
@@ -3,7 +3,7 @@ class AffiliationEdit extends OrganizationTypeForm {
     super();
     this.typeName = "Affiliation";
     this.readableTypeName = "Affiliation";
-    this.icon = '<svg class="SideNav-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>';
+    this.icon = '<svg class="SideNav-icon" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>';
   }
 
   init(data) {
