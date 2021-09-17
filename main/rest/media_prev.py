872d2f028982cd6dbc027fd5903869dac12a0edc
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-09-17 15:01:03 +0000

    Update `MediaNext` and `MediaPrev` endpoints to deal with duplicate filenames

--- main/rest/media_prev.py
+++ main/rest/media_prev.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+from copy import deepcopy
+import logging
 
 from ..models import Media
 from ..search import TatorSearch
@@ -7,6 +9,8 @@ from ..schema import MediaPrevSchema
 from ._base_views import BaseDetailView
 from ._media_query import get_media_es_query
 from ._permissions import ProjectViewOnlyPermission
+
+logger = logging.getLogger(__name__)
 
 class MediaPrevAPI(BaseDetailView):
     """ Retrieve ID of previous media in a media list.
@@ -29,19 +33,33 @@ class MediaPrevAPI(BaseDetailView):
         # Get query associated with media filters.
         query = get_media_es_query(media.project.pk, params)
 
-        # Modify the query to only retrieve previous media.
-        query['sort']['_exact_name'] = 'desc'
-        range_filter = [{'range': {'_exact_name': {'lt': media.name}}}]
+        # Find media with either the same name and lower ID or lower name.
+        prev_filter = [{
+            'bool': {
+                'should': [{
+                    'bool': {
+                        'must': [
+                            {'match': {'_exact_name': {'query': media.name}}},
+                            {'range': {'_postgres_id': {'lt': media.id}}},
+                        ],
+                    },
+                }, {
+                    'range': {'_exact_name': {'lt': media.name}},
+                }],
+                'minimum_should_match': 1,
+            },
+        }]
         if query['query']['bool']['filter']:
-            query['query']['bool']['filter'] += range_filter
+            query['query']['bool']['filter'] += prev_filter
         else:
-            query['query']['bool']['filter'] = range_filter
+            query['query']['bool']['filter'] = prev_filter
+        query['sort'] = [{'_exact_name': 'desc'}, {'_postgres_id': 'desc'}]
         query['size'] = 1
         media_ids, count = TatorSearch().search(media.project.pk, query)
-        if count > 0:
-            response_data = {'prev': media_ids.pop()}
-        else:
+        if len(media_ids) == 0:
             response_data = {'prev': -1}
+        else:
+            response_data = {'prev': media_ids[0]}
 
         return response_data
 
