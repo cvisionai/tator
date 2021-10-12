d34a09752439905df90aaeca8acb02c6f84ccc12
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-10-12 14:52:10 +0000

    Add put method to count endpoint schemas

--- main/schema/media_count.py
+++ main/schema/media_count.py
@@ -6,21 +6,27 @@ from ._errors import error_responses
 from ._media_query import media_filter_parameter_schema
 from ._attributes import attribute_filter_parameter_schema
 
+boilerplate = dedent("""\
+This endpoint accepts the same query parameters as a GET or PUT request to the 
+`Medias` endpoint, but only returns the number of media.
+""")
+
 class MediaCountSchema(AutoSchema):
     def get_operation(self, path, method):
         operation = super().get_operation(path, method)
         if method == 'GET':
             operation['operationId'] = 'GetMediaCount'
+        elif method == 'PUT':
+            operation['operationId'] = 'GetMediaCountById'
         operation['tags'] = ['Tator']
         return operation
 
     def get_description(self, path, method):
-        return dedent("""\
-        Retrieve count of media in a media list.
-
-        This endpoint accepts the same query parameters as a GET request to the `Medias` endpoint,
-        but only returns the number of media.
-        """)
+        if method == 'GET':
+            short_desc = "Get media list count."
+        elif method == 'PUT':
+            short_desc = "Get media list count by ID."
+        return f"{short_desc}\n\n{boilerplate}"
 
     def _get_path_parameters(self, path, method):
         return [{
@@ -33,16 +39,25 @@ class MediaCountSchema(AutoSchema):
 
     def _get_filter_parameters(self, path, method):
         params = []
-        if method  == 'GET':
+        if method in ['GET', 'PUT']:
             params = media_filter_parameter_schema + attribute_filter_parameter_schema
         return params
 
     def _get_request_body(self, path, method):
-        return {}
+        body = {}
+        if method == 'PUT':
+            body = {
+                'required': True,
+                'content': {'application/json': {
+                'schema': {
+                    '$ref': '#/components/schemas/MediaIdQuery',
+                },
+            }}}
+        return body
 
     def _get_responses(self, path, method):
         responses = error_responses()
-        if method == 'GET':
+        if method in ['GET', 'PUT']:
             responses['200'] = {
                 'description': 'Number of media in the list corresponding to query.',
                 'content': {'application/json': {'schema': {
