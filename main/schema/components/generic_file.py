03605cd5f436938958be75a3f2c0c56580128787
Mark Taipan <mark.taipan@cvisionai.com>
2021-10-25 13:54:43 +0000

    Rename HTMLFile endpoint to GenericFile

--- main/schema/components/generic_file.py
+++ main/schema/components/generic_file.py
@@ -1,30 +1,30 @@
 from types import SimpleNamespace
 
-html_file_fields = SimpleNamespace(
+generic_file_fields = SimpleNamespace(
     project="project",
     name="name",
     upload_url="upload_url",
     url="url")
 
-html_file = {
+generic_file = {
     'type': 'object',
     'properties': {
-        html_file_fields.url: {
-            'description': 'Name of HTML file',
+        generic_file_fields.url: {
+            'description': 'Name of generic, non-media file',
             'type': 'string',
         }
     },
 }
 
-html_file_spec = {
+generic_file_spec = {
     'type': 'object',
-    'description': 'HTML file save spec.',
+    'description': 'Generic file save spec.',
     'properties': {
-        html_file_fields.name: {
-            'description': 'Name of HTML file',
+        generic_file_fields.name: {
+            'description': 'Name of generic, non-media file',
             'type': 'string',
         },
-        html_file_fields.upload_url: {
+        generic_file_fields.upload_url: {
             'description': 'URL of the uploaded file',
             'type': 'string',
         },
