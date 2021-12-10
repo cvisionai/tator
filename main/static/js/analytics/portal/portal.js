b5c979d10e101f93ee84c1e2772ecbb41d30402c
Mark Taipan <mark.taipan@cvisionai.com>
2021-12-10 03:02:38 +0000

    Hide collections panel

--- main/static/js/analytics/portal/portal.js
+++ main/static/js/analytics/portal/portal.js
@@ -34,14 +34,16 @@ class AnalyticsPortal extends TatorPage {
     this.main.appendChild(localizationsBox);
 
     // Collections
+    /*
     const collectionsBox = this._getDashboardBox({
         name : "Collections",
         href : `/${this.projectId}/analytics/collections`,
         iconName: "track-icon"
       });
     this.main.appendChild(collectionsBox);
+    */
 
-    // Collections
+    // Corrections
     const correctionsBox = this._getDashboardBox({
       name : "Corrections",
       href : `/${this.projectId}/analytics/corrections`,
