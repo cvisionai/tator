60db6c8138b581be1d757624e466e6119b999ec7
Brian Tate <brian.tate@cvisionai.com>
2021-12-20 14:38:38 -0500

    Add image type to annotation test.

--- test/test_annotation.py
+++ test/test_annotation.py
@@ -6,6 +6,7 @@ from ._common import print_page_error
 # Common function for checking annotation capability
 def common_annotation(page, canvas):
     canvas_box = canvas.bounding_box()
+    print(f"Canvas info: {canvas_box}")
     canvas_center_x = canvas_box['x'] + canvas_box['width'] / 2
     canvas_center_y = canvas_box['y'] + canvas_box['height'] / 2
     print("Drawing three boxes with different enum values...")
@@ -17,6 +18,7 @@ def common_annotation(page, canvas):
         x, y = start
         x += canvas_center_x
         y += canvas_center_y
+        print(f"Create Box {x},{y}")
         width = 100
         height = 100
         page.mouse.move(x, y)
@@ -42,6 +44,7 @@ def common_annotation(page, canvas):
         selector.wait_for_selector(f'#current-index :text("{idx+1}")')
         page.mouse.move(x+50, y+50)
         page.mouse.down()
+        time.sleep(1)
         page.mouse.move(x, y)
         page.mouse.up()
         light = page.query_selector('#tator-success-light')
@@ -58,6 +61,7 @@ def common_annotation(page, canvas):
         selector.wait_for_selector(f'#current-index :text("{idx+1}")')
         page.mouse.move(x+45, y+45)
         page.mouse.down()
+        time.sleep(1)
         page.mouse.move(x+95, y+95)
         page.mouse.up()
         light = page.query_selector('#tator-success-light')
@@ -66,10 +70,20 @@ def common_annotation(page, canvas):
 
 
 def test_video_annotation(authenticated, project, video):
-    print("Going to annotation view...")
+    print("[Video] Going to annotation view...")
     page = authenticated.new_page()
     page.goto(f"/{project}/annotation/{video}")
     page.on("pageerror", print_page_error)
     page.wait_for_selector('video-canvas')
     canvas = page.query_selector('video-canvas')
+    common_annotation(page, canvas)
+    
+def test_image_annotation(authenticated, project, image):
+    print("[Image] Going to annotation view...")
+    page = authenticated.new_page()
+    page.goto(f"/{project}/annotation/{image}")
+    page.on("pageerror", print_page_error)
+    page.wait_for_selector('image-canvas')
+    canvas = page.query_selector('image-canvas')
+    time.sleep(2)
     common_annotation(page, canvas)
