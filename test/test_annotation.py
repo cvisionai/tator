1eeb91ad8385690468848006287adf5bc20dcac6
Brian Tate <brian.tate@cvisionai.com>
2021-12-20 17:26:41 -0500

    Fix annotation test for images, multi, and video.
    - Use multi mouse movements
    - Wait for box tool to be ready.
    - Assure a large enough viewport for multi to be usable.

--- test/test_annotation.py
+++ test/test_annotation.py
@@ -4,7 +4,7 @@ import time
 from ._common import print_page_error
 
 # Common function for checking annotation capability
-def common_annotation(page, canvas):
+def common_annotation(page, canvas, bias=0):
     canvas_box = canvas.bounding_box()
     print(f"Canvas info: {canvas_box}")
     canvas_center_x = canvas_box['x'] + canvas_box['width'] / 2
@@ -14,7 +14,7 @@ def common_annotation(page, canvas):
                 ((-50, -50), 'Test Choice 2'),
                 ((100, -50), 'Test Choice 3')]
     for idx, (start, enum_value) in enumerate(box_info):
-        page.click('box-button')
+        page.click('box-button:not(.disabled)')
         x, y = start
         x += canvas_center_x
         y += canvas_center_y
@@ -41,11 +41,10 @@ def common_annotation(page, canvas):
         y += canvas_center_y
         page.mouse.click(x+50, y+50)
         selector = page.query_selector('entity-selector:visible')
-        selector.wait_for_selector(f'#current-index :text("{idx+1}")')
-        page.mouse.move(x+50, y+50)
+        selector.wait_for_selector(f'#current-index :text("{idx+1+bias}")')
+        page.mouse.move(x+50, y+50,steps=50)
         page.mouse.down()
-        time.sleep(1)
-        page.mouse.move(x, y)
+        page.mouse.move(x, y, steps=50)
         page.mouse.up()
         light = page.query_selector('#tator-success-light')
         light.wait_for_element_state('visible')
@@ -58,11 +57,10 @@ def common_annotation(page, canvas):
         y += canvas_center_y
         page.mouse.click(x+45, y+45)
         selector = page.query_selector('entity-selector:visible')
-        selector.wait_for_selector(f'#current-index :text("{idx+1}")')
-        page.mouse.move(x+45, y+45)
+        selector.wait_for_selector(f'#current-index :text("{idx+1+bias}")')
+        page.mouse.move(x+45, y+45, steps=50)
         page.mouse.down()
-        time.sleep(1)
-        page.mouse.move(x+95, y+95)
+        page.mouse.move(x+95, y+95, steps=50)
         page.mouse.up()
         light = page.query_selector('#tator-success-light')
         light.wait_for_element_state('visible')
@@ -72,6 +70,7 @@ def common_annotation(page, canvas):
 def test_video_annotation(authenticated, project, video):
     print("[Video] Going to annotation view...")
     page = authenticated.new_page()
+    page.set_viewport_size({"width": 2560, "height": 1440}) # Annotation decent screen
     page.goto(f"/{project}/annotation/{video}")
     page.on("pageerror", print_page_error)
     page.wait_for_selector('video-canvas')
@@ -81,9 +80,22 @@ def test_video_annotation(authenticated, project, video):
 def test_image_annotation(authenticated, project, image):
     print("[Image] Going to annotation view...")
     page = authenticated.new_page()
+    page.set_viewport_size({"width": 2560, "height": 1440}) # Annotation a decent screen
     page.goto(f"/{project}/annotation/{image}")
     page.on("pageerror", print_page_error)
     page.wait_for_selector('image-canvas')
     canvas = page.query_selector('image-canvas')
-    time.sleep(2)
     common_annotation(page, canvas)
+
+def test_multi_annotation(authenticated, project, multi):
+    print("[Multi] Going to annotation view...")
+    page = authenticated.new_page()
+    page.set_viewport_size({"width": 2560, "height": 1440}) # Multi requires a decent screen
+    page.goto(f"/{project}/annotation/{multi}")
+    page.on("pageerror", print_page_error)
+    page.wait_for_selector('video-canvas')
+    canvas = page.query_selector_all('video-canvas')
+    # TODO: Make this wait on the video being ready (console log? element selection? Probably play button...)
+    assert(len(canvas) == 2)
+    common_annotation(page, canvas[0])
+    common_annotation(page, canvas[1], bias=3)
