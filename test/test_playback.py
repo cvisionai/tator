d26943439773c865f4afc0c1c99f7dc87a2b21d7
Brian Tate <brian.tate@cvisionai.com>
2021-12-21 21:17:56 -0500

    Finish playback tests.

--- test/test_playback.py
+++ test/test_playback.py
@@ -4,6 +4,8 @@ import os
 import cv2
 import tempfile
 import time
+import pytesseract
+import numpy as np
 
 def _get_canvas_color(canvas):
   """ Returns the RGB value of the canvas (mean) """
@@ -13,11 +15,45 @@ def _get_canvas_color(canvas):
     img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     return img.mean(axis=(0,1))
 
+def _get_canvas_frame(canvas):
+  """ Returns the frame number reported by the video """
+  with tempfile.TemporaryDirectory() as td:
+    canvas.screenshot(path=os.path.join(td, "canvas.png"))
+    img=cv2.imread(os.path.join(td, "canvas.png"))
+    img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    text = pytesseract.image_to_string(img)
+    _,val=text.strip().split('=')
+    return int(val)
+
 def _get_element_center(element):
   box = element.bounding_box()
   center_x = box['x'] + box['width'] / 2
   center_y = box['y'] + box['height'] / 2
   return center_x,center_y
+
+def test_playback_accuracy(authenticated, project, count_test):
+  print("[Video] Going to annotation view...")
+  page = authenticated.new_page()
+  page.set_viewport_size({"width": 2560, "height": 1440}) # Annotation decent screen
+  page.goto(f"/{project}/annotation/{count_test}?scrubQuality=360&seekQuality=720&playQuality=720")
+  page.on("pageerror", print_page_error)
+  page.wait_for_selector('video-canvas')
+  canvas = page.query_selector('video-canvas')
+  play_button = page.query_selector('play-button')
+  seek_handle = page.query_selector('seek-bar .range-handle')
+  display_div = page.query_selector('#frame_num_display')
+  time.sleep(5)
+  canvas_frame = _get_canvas_frame(canvas)
+  assert(canvas_frame == 0)
+  assert(int(display_div.inner_text())==0)
+
+  play_button.click()
+  time.sleep(5)
+  play_button.click()
+  canvas_frame = _get_canvas_frame(canvas)
+  assert(canvas_frame > 0)
+  assert(int(display_div.inner_text())==canvas_frame)
+  
 def test_buffer_usage(authenticated, project, rgb_test):
   # Tests play, scrub, and seek buffer usage
   print("[Video] Going to annotation view...")
@@ -30,34 +66,32 @@ def test_buffer_usage(authenticated, project, rgb_test):
   play_button = page.query_selector('play-button')
   seek_handle = page.query_selector('seek-bar .range-handle')
 
- 
+  canvas_color = None
   # Wait for hq buffer and verify it is red
-  time.sleep(5) # TODO make this better
-  print("Checking for RED")
-  canvas_color = _get_canvas_color(canvas)
-  print(f"GOT={canvas_color}")
-  assert canvas_color[0] > 200
-  assert canvas_color[1] < 40
-  assert canvas_color[2] < 40
+  for _ in range(30):
+    canvas_color = _get_canvas_color(canvas)
+    if np.argmax(canvas_color) == 0:
+      break
+    time.sleep(1)
+  assert np.argmax(canvas_color) == 0, f"{canvas_color}"
 
   play_button.click()
-  time.sleep(5)
-  print("Checking for GREEN")
-  canvas_color = _get_canvas_color(canvas)
-  print(f"GOT={canvas_color}")
-  assert canvas_color[0] < 40
-  assert canvas_color[1] > 200
-  assert canvas_color[2] < 40
+  for _ in range(30):
+    canvas_color = _get_canvas_color(canvas)
+    if np.argmax(canvas_color) == 1:
+      break
+    time.sleep(1)
+  assert np.argmax(canvas_color) == 1, f"{canvas_color}"
 
   # Pause the video
   play_button.click()
-  time.sleep(3)
-  print("Checking for RED")
-  canvas_color = _get_canvas_color(canvas)
-  print(f"GOT={canvas_color}")
-  assert canvas_color[0] > 200
-  assert canvas_color[1] < 40
-  assert canvas_color[2] < 40
+  for _ in range(30):
+    canvas_color = _get_canvas_color(canvas)
+    if np.argmax(canvas_color) == 0:
+      break
+    time.sleep(1)
+  assert np.argmax(canvas_color) == 0, f"{canvas_color}"
+  
 
   # Click the scrub handle
   seek_x,seek_y = _get_element_center(seek_handle)
@@ -68,27 +102,22 @@ def test_buffer_usage(authenticated, project, rgb_test):
   print("Checking for GREEN")
   canvas_color = _get_canvas_color(canvas)
   print(f"GOT={canvas_color}")
-  assert canvas_color[0] < 40
-  assert canvas_color[1] > 200
-  assert canvas_color[2] < 40
+  assert np.argmax(canvas_color) == 1, f"{canvas_color}"
 
   page.mouse.move(seek_x+1000, seek_y, steps=50)
   print("Checking for BLUE")
   canvas_color = _get_canvas_color(canvas)
   print(f"GOT={canvas_color}")
-  assert canvas_color[0] < 40
-  assert canvas_color[1] < 40
-  assert canvas_color[2] > 200
+  assert np.argmax(canvas_color) == 2, f"{canvas_color}"
 
   # Release the scrub
   page.mouse.up()
-  time.sleep(3)
-  print("Checking for RED")
-  canvas_color = _get_canvas_color(canvas)
-  print(f"GOT={canvas_color}")
-  assert canvas_color[0] > 200
-  assert canvas_color[1] < 40
-  assert canvas_color[2] < 40
+  for _ in range(30):
+    canvas_color = _get_canvas_color(canvas)
+    if np.argmax(canvas_color) == 0:
+      break
+    time.sleep(1)
+  assert np.argmax(canvas_color) == 0, f"{canvas_color}"
 
 
 
