252aff0e6260f6a624ad3f251663182b1cadc5b9
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2022-01-30 04:39:48 +0000

    Break bundles up by directory

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -2,9 +2,26 @@ const path = require("path")
 const WebpackShellPluginNext = require('webpack-shell-plugin-next');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    "project-settings": "./src/project-settings/index.js",
+    "project-detail": "./src/project-detail/index.js",
+    "organization-settings": "./src/organization-settings/index.js",
+    "password-reset": "./src/password-reset/index.js",
+    annotation: "./src/annotation/index.js",
+    organizations: "./src/organizations/index.js",
+    "account-profile": "./src/account-profile/index.js",
+    annotator: "./src/annotator/index.js",
+    token: "./src/token/index.js",
+    projects: "./src/projects/index.js",
+    tasks: "./src/tasks/index.js",
+    util: "./src/util/index.js",
+    registration: "./src/registration/index.js",
+    components: "./src/components/index.js",
+    "new-project": "./src/new-project/index.js",
+    analytics: "./src/analytics/index.js"
+  },
   output: {
-    filename: "main.js",
+    filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
