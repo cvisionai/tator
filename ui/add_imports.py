4bb3e673b17ac20100ed4c54271737eaaff6c02d
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2022-02-02 04:35:43 +0000

    Handle more base classes

--- ui/add_imports.py
+++ ui/add_imports.py
@@ -11,18 +11,32 @@ for root, dirs, files in os.walk('src/js'):
             content = f.read()
         if "extends TatorElement" in content:
             cls = "TatorElement"
-            cls_file = "tator-element.js"
+            cls_file = "components/tator-element.js"
         elif "extends ModalDialog" in content:
             cls = "ModalDialog"
-            cls_file = "modal-dialog.js"
+            cls_file = "components/modal-dialog.js"
         elif "extends TatorPage" in content:
             cls = "TatorPage"
-            cls_file = "tator-page.js"
+            cls_file = "components/tator-page.js"
+        elif "extends HTMLElement" in content:
+            continue
+        elif "extends UploadElement" in content:
+            cls = "UploadElement"
+            cls_file = "components/upload-element.js"
+        elif "extends OrganizationTypeForm" in content:
+            cls = "OrganizationTypeForm"
+            cls_file = "organization-settings/organization-type-form.js"
+        elif "extends TypeForm" in content:
+            cls = "TypeForm"
+            cls_file = "project-settings/type-form.js"
+        elif "extends AnnotationCanvas" in content:
+            cls = "AnnotationCanvas"
+            cls_file = "annotator/annotation.js"
         else:
             print(f"Need manual mods for {path}!")
             continue
         content = ("import { " + cls + " } from \"" + prefix +
-                   "../components/" + cls_file + ";\n\n" + content)
+                   cls_file + "\";\n\n" + content)
         content.replace("class ", "export class ")
         with open(path, 'w') as f:
             f.write(content)
