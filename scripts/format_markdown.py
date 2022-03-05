e47375dc7f92730c975432e7df2a378628d30090
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2022-03-05 03:26:18 +0000

    Add script for formatting tator-py markdown

---
+++ scripts/format_markdown.py
@@ -0,0 +1,22 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Formats markdown doc")
+    parser.add_argument('in_file', help="File to format")
+    parser.add_argument('out_file', help="Output file")
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+    out = open(args.out_file, 'w')
+    with open(args.in_file, 'r') as f:
+        skip_newlines = False
+        for line in f:
+            if '**Parameters**' in line:
+                skip_newlines = True
+            if '**Returns**' in line:
+                skip_newlines = False
+            if line.isspace() and skip_newlines:
+                continue
+            out.write(line)
+    out.close()
