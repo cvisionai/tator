900c3c549353579df12e77137026ed10615a2ead
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2022-03-06 20:13:38 +0000

    More formatting of generated markdown

--- scripts/format_markdown.py
+++ scripts/format_markdown.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Formats markdown doc")
@@ -10,19 +11,28 @@ if __name__ == '__main__':
     args = parse_args()
     out = open(args.out_file, 'w')
     with open(args.in_file, 'r') as f:
+        title = os.path.splitext(os.path.basename(args.out_file))[0]
+        title = title.capitalize()
+        out.write('---\n')
+        out.write(f'title: {title}\n')
+        out.write('---\n\n\n')
         skip_newlines = False
         for line in f:
+            line = line.replace('# noqa: E501', '')
             if '**Parameters**' in line:
                 skip_newlines = True
             if '**Returns**' in line:
                 out.write('\n')
                 skip_newlines = False
-            if line.startswith('### tator') and line.endswith(')\n'):
-                func = line.split('(')[0]
+            if ((line.startswith('### tator') 
+                 or line.startswith('### _class_')
+                 or line.startswith('### _exception_'))
+                and line.endswith(')\n')):
+                func = line.split('(')[0].replace(' _class_', '').replace(' _exception_', '')
                 signature = line.split(' ', 1)[1].strip('\n')
-                out.write('----------------------------------\n')
-                out.write(f"{func}\n")
-                out.write(f"`{signature}`\n")
+                out.write('----------------------------------\n\n')
+                out.write(f"{func}\n\n")
+                out.write(f"`{signature}`\n\n")
                 continue
             if not (line.isspace() and skip_newlines):
                 out.write(line)
