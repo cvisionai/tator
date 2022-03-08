9e31df071e1035d467f023c58959dd898f6ba385
Brian Tate <brian.tate@cvisionai.com>
2022-03-08 16:08:24 -0500

    Fix stylesheet usage to remove CSS duplication.

--- ui/src/js/components/tator-element.js
+++ ui/src/js/components/tator-element.js
@@ -2,20 +2,41 @@ import "../../css/styles.scss";
 
 export const svgNamespace = "http://www.w3.org/2000/svg";
 
+class StyleHolder {
+  constructor() {
+    this.css = new CSSStyleSheet();
+    this.ready = false;
+    this.elements=[];
+    fetch('/static/components.css').then(body => body.text())
+    .then(text => {
+        this.css.replaceSync(text);
+        this.ready = true;
+        for (let element of this.elements)
+        {
+          element.style.visibility = 'visible';
+        }
+      });
+  }
+}
+
+let global_style = new StyleHolder();
+
 export class TatorElement extends HTMLElement {
   constructor() {
     super();
     this._shadow = this.attachShadow({mode: "open"});
 
     // Create a css link for components.
-    const css = document.createElement("link");
-    css.setAttribute("rel", "stylesheet");
-    css.setAttribute("href", "/static/components.css");
-    this._shadow.appendChild(css);
-    css.addEventListener("load", () => {this.style.visibility = "visible";});
+   
+    this._shadow.adoptedStyleSheets = [global_style.css];
+    //css.addEventListener("load", () => {this.style.visibility = "visible";});
   }
 
   connectedCallback() {
-    this.style.visibility = "hidden";
+    if (global_style.ready != true)
+    {
+      this.style.visibility = "hidden";
+      global_style.elements.push(this);
+    }
   }
 }
