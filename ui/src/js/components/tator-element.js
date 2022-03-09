ee25dda551f7023c4697994160a5b585fe5078a8
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2022-03-09 18:00:12 +0000

    Fix flash of modal dialogs

--- ui/src/js/components/tator-element.js
+++ ui/src/js/components/tator-element.js
@@ -5,17 +5,13 @@ export const svgNamespace = "http://www.w3.org/2000/svg";
 class StyleHolder {
   constructor() {
     this.css = new CSSStyleSheet();
+    this.css.addRule("*", "visibility: hidden");
     this.ready = false;
     this.elements=[];
     fetch('/static/components.css').then(body => body.text())
     .then(text => {
         this.css.replaceSync(text);
-        this.ready = true;
-        for (let element of this.elements)
-        {
-          element.style.visibility = 'visible';
-        }
-      });
+    });
   }
 }
 
@@ -25,18 +21,6 @@ export class TatorElement extends HTMLElement {
   constructor() {
     super();
     this._shadow = this.attachShadow({mode: "open"});
-
-    // Create a css link for components.
-   
     this._shadow.adoptedStyleSheets = [global_style.css];
-    //css.addEventListener("load", () => {this.style.visibility = "visible";});
-  }
-
-  connectedCallback() {
-    if (global_style.ready != true)
-    {
-      this.style.visibility = "hidden";
-      global_style.elements.push(this);
-    }
   }
 }
