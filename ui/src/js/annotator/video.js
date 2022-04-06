f3adaae1fb9f1f16959eff61330737f9336bbb69
Brian Tate <brian.tate@cvisionai.com>
2022-04-06 10:15:33 -0400

    Refactor out periodic task profiler to stand-alone module.

--- ui/src/js/annotator/video.js
+++ ui/src/js/annotator/video.js
@@ -4,6 +4,7 @@ import { TatorVideoDecoder} from "./video-codec.js";
 import Hls from "hls.js";
 import { fetchRetry } from "../util/fetch-retry.js";
 import { getCookie } from "../util/get-cookie.js";
+import { PeriodicTaskProfiler } from "./periodic_task_profiler";
 
 
 // Video export class handles interactions between HTML presentation layer and the
@@ -72,54 +73,6 @@ var src_path="/static/js/annotator/";
 export const RATE_CUTOFF_FOR_ON_DEMAND = 16.0;
 const RATE_CUTOFF_FOR_AUDIO = 4.0;
 
-class PeriodicTaskProfiler
-{
-  constructor(name, alert_interval, verbose)
-  {
-    this._name = name
-    if (alert_interval == undefined)
-    {
-      alert_interval=1000;
-    }
-    this._alert_interval = alert_interval;
-    this._times=[]
-    this._verbose = verbose;
-  }
-  push(this_time)
-  {
-    return;
-    this._times.push(this_time);
-    if (this._times.length % this._alert_interval == 0)
-    {
-      this.stats(true);
-    }
-  }
-  stats(flush)
-  {
-    let maxDelta=Number.MIN_SAFE_INTEGER;
-    let minDelta=Number.MAX_SAFE_INTEGER;
-    let delta_sum = 0;
-    let max_delta = 0;
-    for (let idx=0; idx < this._times.length; idx++)
-    {
-      delta_sum += this._times[idx];
-      if (this._times[idx] > max_delta)
-      {
-        max_delta = this._times[idx];
-      }
-    }
-    let avg_delta = delta_sum / this._times.length;
-    console.info(`${this._name} performance ${avg_delta}ms - Worst = ${max_delta} ms`);
-    if (this._verbose)
-    {
-      console.info(`${this._name} TIMES = ${this._times}`);
-    }
-    if (flush)
-    {
-      this._times=[]
-    }
-  }
-}
 /// Support multiple off-screen videos at varying resolutions
 /// the intention is this export class is used to store raw video
 /// frames as they are downloaded.
