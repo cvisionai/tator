e298d52aa2a7cd47b34f7a0cee26df644c784e99
Hugh Enxing <hugh.enxing@cvisionai.com>
2022-04-20 13:27:11 -0400

    Improves archive lifecycle management
    This improves the handling of multiviews during archive and restore
    operations. They are now considered separately, after the `video` types,
    and will transition states based on an updated set of possible states
    its constituent media.

--- main/management/commands/archivemedia.py
+++ main/management/commands/archivemedia.py
@@ -24,19 +24,28 @@ class Command(BaseCommand):
     def handle(self, **options):
         min_delta = timedelta(days=options["min_age_days"])
         max_datetime = datetime.now(timezone.utc) - min_delta
-        archived_qs = Media.objects.filter(
+        base_qs = Media.objects.filter(
             deleted=False,
             archive_state="to_archive",
             archive_status_date__lte=max_datetime,
-        ).exclude(meta__dtype="multi")
+        )
+        # Handle multiviews after all singles because their transition is dependent on the singles'
+        # states
+        archived_qs = base_qs.exclude(meta__dtype="multi")
+        multi_qs = base_qs.filter(meta__dtype="multi")
 
-        if not archived_qs.exists():
+        if not (archived_qs.exists() or multi_qs.exists()):
             logger.info(f"No media to archive!")
             return
 
         # Update media ready for archiving
         target_state = {"archive_state": "archived", "restoration_requested": False}
-        not_ready = update_queryset_archive_state(archived_qs, target_state)
+        not_ready = {"cloned": {}, "original": {}}
+        if archived_qs.exists():
+            not_ready = update_queryset_archive_state(archived_qs, target_state)
+        if multi_qs.exists():
+            # Return will be empty when operating on all multiviews
+            update_queryset_archive_state(multi_qs, target_state)
 
         # Notify owners of blocked archive attempt
         ses = TatorSES() if settings.TATOR_EMAIL_ENABLED else None
