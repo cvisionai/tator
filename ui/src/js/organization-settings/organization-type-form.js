b71142cc6ed3199d1c2ed52ac428581a762d113e
Erin B <erm.butler@gmail.com>
2022-05-05 16:20:44 +0000

    Bug fix for orgData in invitation edit; Fix for test locating save button.

--- ui/src/js/organization-settings/organization-type-form.js
+++ ui/src/js/organization-settings/organization-type-form.js
@@ -29,13 +29,14 @@ export class OrganizationTypeForm extends TatorElement {
     this.saveButton = document.createElement("input"); // init early so we can disable via forms
   }
 
-  _init({ data, modal, sidenav }) {
+  _init({ data, modal, sidenav, orgData }) {
     // Log to verify init
     // console.log(`${this.readableTypeName} init.`);
     // console.log(data);
 
     // Initial values
     this.data = data;
+    this.orgData = orgData;
     this.modal = modal;
     this.organizationId = this.data.organization;
     this.typeId = this.data.id
@@ -169,8 +170,7 @@ export class OrganizationTypeForm extends TatorElement {
             data,
             modal: this.modal,
             sidenav: this.sideNav,
-            mediaListHandler: this.mediaListHandler,
-            versionListHandler: this.versionListHandler
+            orgData: this.orgData
           });
 
           // Add the item to navigation
