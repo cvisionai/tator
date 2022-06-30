dc23831a87e33eb169f60f53af2650bbdfd7ffa8
Erin B <erm.butler@gmail.com>
2022-06-30 17:37:48 +0000

    Fixes #676 error popup will happen once per session.

--- ui/src/js/annotation/annotation-common.js
+++ ui/src/js/annotation/annotation-common.js
@@ -1,8 +1,10 @@
 export function handle_video_error(evt, root)
 {
   let msg_html = "";
-  if (evt.detail.secureContext == false)
-  {
+  let errorType = null;
+
+  if (evt.detail.secureContext == false) {
+    errorType = "secureContext";
     const tator_link=`<span class='text-gray'><a class='nav__link' target='_new' href='https://www.tator.io/docs/administrator-guide/configuration/enable-https'>enable a secure context using TLS</a></span>`;
     const tutorial_link=`<span class='text-gray'><a class='nav__link' target='_new' href='https://www.tator.io/blog/secure-context-for-local-deployments'>secure origin</a></span>`;
     msg_html += "<div class='nav__secondary'>";
@@ -10,9 +12,8 @@ export function handle_video_error(evt, root)
     msg_html += `<br><br>To proceed, add <span class='text-purple'>${window.location.host}</span> as a ${tutorial_link}`; 
     msg_html += ` or contact your system administrator to ${tator_link} to your Tator deployment.`;
     msg_html += "</div>"
-  }
-  else if (evt.detail.videoDecoderPresent == false)
-  {
+  } else if (evt.detail.videoDecoderPresent == false) {
+    errorType = "videoDecoderPresent";
     const edge_link=`<span class='text-gray'><a class='nav__link' target='_new' href='https://www.microsoft.com/en-us/edge'>Microsoft Edge</a></span>`;
     const chrome_link=`<span class='text-gray'><a class='nav__link' target='_new' href='https://www.google.com/chrome/'>Google Chrome</a></span>`;
     msg_html += "<div class='nav__secondary'>";
@@ -21,10 +22,17 @@ export function handle_video_error(evt, root)
     msg_html += "</div>";
   }
 
-  let modalError = document.createElement("modal-notify");
-  root.appendChild(modalError);
-  modalError.init("System Incompatibility Warning", msg_html, 'error', 'Exit', true);
-  modalError.setAttribute("is-open", "");
+  const sessionValue = sessionStorage.getItem(`handle_error__${errorType}`);
+  if (sessionValue && sessionValue === 'true') {
+    console.error("Supressed System Incompatibility Warning: "+errorType);
+    return;
+  } else {
+    let modalError = document.createElement("modal-notify");
+    root.appendChild(modalError);
+    modalError.init("System Incompatibility Warning", msg_html, 'error', 'Exit', true);
+    modalError.setAttribute("is-open", "");
+    sessionStorage.setItem(`handle_error__${errorType}`, 'true');
+  }
 }
 
 // Class to handle the repetitive nature of graying out / disabling the play button
