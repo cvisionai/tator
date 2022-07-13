f50aaf14625650dfc22864caad572e519bbf5eb4
Erin B <erm.butler@gmail.com>
2022-07-13 14:23:07 +0000

    Temp remove polyfill code.

--- ui/src/js/components/tator-element.js
+++ ui/src/js/components/tator-element.js
@@ -1,5 +1,5 @@
 import "../../css/styles.scss";
-import "construct-style-sheets-polyfill";
+// import "construct-style-sheets-polyfill";
 export const svgNamespace = "http://www.w3.org/2000/svg";
 
 class StyleHolder {
