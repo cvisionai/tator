fece6f42241497dad06889154c2709b634662537
Erin B <erm.butler@gmail.com>
2022-07-21 16:28:45 +0000

    Start of draggable right-side panel.

--- ui/src/js/components/entity-panel/entity-panel-container.js
+++ ui/src/js/components/entity-panel/entity-panel-container.js
@@ -6,6 +6,7 @@ export class EntityPanelContainer extends TatorElement {
 
       //default state
       this.open = false;
+      this._movingPanel = false;
 
       // Close side panel bar with arrow and panel title
       this._panelTop = document.createElement("entity-gallery-panel-top");
@@ -13,15 +14,34 @@ export class EntityPanelContainer extends TatorElement {
 
       // this element (aside)
       this.el = null;
+
+
+
+
    }
 
    //
-   init({ position = "right", main, isMediaSection = false, aside, pageModal, modelData, gallery, contents }) {
-      this.lside = main;
-      this.rside = aside;
+   init({ position = "right", main, isMediaSection = false, aside, pageModal, modelData, gallery, contents, bulkEdit = null }) {
+      this.main = main;
+      this.aside = aside;
       this.gallery = gallery;
       this.position = position;
       this.contents = contents;
+      this.bulkEdit = bulkEdit;
+
+      this._resizer = document.createElement('div');
+      this._resizer.className = 'entity-panel-draghandle '+position;
+      this._resizer.style.width = '6px';
+      this._resizer.style.height = '100vh';
+
+
+      if (this.position == "right") {
+         this._shadow.appendChild(this._resizer);      
+         this.setUpResize();
+         if (this.bulkEdit !== null) {
+            this.bulkEdit._bulkEditBar.style.right = "40px";
+         }
+      }
 
       // listener to close panelContainer
       if (isMediaSection) {
@@ -74,7 +94,7 @@ export class EntityPanelContainer extends TatorElement {
 
    _toggleRightOnClick() {
       // CLOSE
-      /* DEFAULT lside = col-9, and rside = col-2 */
+      /* DEFAULT main = col-9, and aside = col-2 */
       if (this.open) {
          this._toggleShut();
       } else {
@@ -83,22 +103,21 @@ export class EntityPanelContainer extends TatorElement {
    }
 
    _toggleOpen() {
-      this.rside.classList.remove("slide-close");
-      
-      this.lside.classList.add("col-9");
-      this.lside.classList.remove("col-12");
-      this.lside.style.marginRight = "0";
+      this.aside.classList.remove("slide-close");    
+      this.main.classList.add("col-9");
+      this.main.classList.remove("col-12");
+      this.main.style.marginRight = "0";
 
       if (this.position == "left") {
-         this.lside.style.paddingLeft = "400px";
+         this._resizer.paddingLeft = "400px";
+         this.main.style.paddingLeft = "400px";
          this.gallery._main.classList.remove("ml-6");
          this.gallery._main.classList.add("ml-3");
       } else {
+         // this._resizer.paddingRight = "400px";
          this.gallery._main.classList.remove("mr-6");
          this.gallery._main.classList.add("mr-3");
-      }
-      
-      
+      }   
 
       this._panelTop._topBarArrow.style.transform = "scaleX(1)";
       this.open = true;
@@ -107,30 +126,99 @@ export class EntityPanelContainer extends TatorElement {
    }
 
    _toggleShut() {
-      this.lside.classList.add("col-12");
+      // remove any dragged sizing
+      this.main.style.removeProperty('width');
+      this.aside.style.removeProperty('width');
+
+      this.main.classList.add("col-12");
+      this.aside.classList.add("slide-close");
       
-      this.rside.classList.add("slide-close");
-      
-      this.lside.classList.remove("col-9");
-      this.lside.style.marginRight = "2%";
+      this.main.classList.remove("col-9");
+      this.main.style.marginRight = "2%";
+      this._resizer.marginRight = "2%";
 
       if (this.position == "left") {
-         this.lside.style.paddingLeft = "2%";
-         this.lside.style.marginRight = "0";
+         this._resizer.paddingLeft = "2%";
+         this.main.style.paddingLeft = "2%";
+         this.main.style.marginRight = "0";
+         this._resizer.marginRight = "0";
          this.gallery._main.classList.add("ml-6");
          this.gallery._main.classList.remove("ml-3");
-      } else {
+      } else{
          this.gallery._main.classList.add("mr-6");
          this.gallery._main.classList.remove("mr-3");
       }
-
-      
       
       this.open = false;
       this._panelTop._topBarArrow.style.transform = "scaleX(-1)";
 
       return this.open;
    }
+
+
+   setUpResize() {
+      console.log(this.main);
+      const initResizePanel = (e) => {
+         if (!this._movingPanel) {
+            this._movingPanel = true;
+            window.addEventListener('mousemove', resizePanel, false);
+            window.addEventListener('mouseup', stopResizePanel, false);
+         }
+      }
+      const resizePanel = (e) => {
+         console.log(this.main.style);
+         if (!this.open) {
+            // #todo take what you need from toggleOpen so this doesn't end up in the negative side
+            // but fix the jump when this happens
+            this._toggleOpen();
+         }
+         if (this._movingPanel) {
+            if (this.position == "left") {
+               this.aside.style.width = (e.clientX) + 'px';
+               this.main.style.width = `${this.aside.offsetLeft + 40} px`;
+               this._resizer.style.left = `${(e.clientX - 10)}px !important`;
+               console.log(this._resizer.style.left);
+               // #todo left isn't done, add bulkEdit when panel bugs fixed
+               // if (this.bulkEdit !== null) {
+                  
+               // }
+            } else {
+               //right panel 
+               console.log(this.main.style.width);
+               let num = window.innerWidth - this.aside.offsetLeft;
+               console.log(window.innerWidth -  Number(num) );
+               this.aside.style.width = (window.innerWidth - e.clientX + (.02 * window.innerWidth)) + 'px';
+               this.main.style.width = `${window.innerWidth - Number(num)}px`;
+               console.log(this.bulkEdit);
+               if (this.bulkEdit !== null) {
+                  this.bulkEdit._bulkEditBar.style.right = (window.innerWidth - e.clientX + (.02 * window.innerWidth)) + 'px';
+               }
+            }
+         }
+      }
+   
+      const stopResizePanel = (e) =>  {
+         window.removeEventListener('mousemove', resizePanel, false);
+         window.removeEventListener('mouseup', stopResizePanel, false);
+         if (this.position == "left") {
+            this.main.style.width = `${this.aside.offsetLeft + 40} px`;
+         } else {
+            let num = window.innerWidth - this.aside.offsetLeft - this.aside.offsetWidth;
+            console.log(num);
+            this.main.style.width = `${window.innerWidth - Number(num) } px`;
+         }
+         console.log(this.main.style.width)
+         console.log("STOP");
+         this._movingPanel = false;
+      }
+
+      this._resizer.addEventListener('mousedown', initResizePanel, false);
+   }
+
+   
+
+
+   
 }
 
 customElements.define("entity-panel-container", EntityPanelContainer);
