c9aa8146aa0b762a87c05d9973989b12d238ae96
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2022-08-08 04:35:14 +0000

    Clean up tator-py models docs

--- scripts/format_markdown.py
+++ scripts/format_markdown.py
@@ -24,6 +24,7 @@ if __name__ == '__main__':
         skip_section = False
         skip_parameter = False
         skip_sync_blurb = False
+        property_name = ""
         for line in f:
             if line.startswith('#### openapi_types') or line.startswith('#### attribute_map'):
                 continue
@@ -43,21 +44,47 @@ if __name__ == '__main__':
             # Skip asynchronous request blurb
             if line.startswith('This method makes a synch'):
                 skip_sync_blurb = True
-            if line.startswith('* **'):
+            if line.startswith('* **') and skip_sync_blurb:
                 out.write('\n')
                 skip_sync_blurb = False
             if skip_sync_blurb:
                 continue
+            # Reformat property docs
+            if line.startswith('#### _property_'):
+                line = line.replace('#### _property_ ', '*')
+                line = line.replace('()', '*')
+                property_name = line.strip('\n')
+                desc_next = True
+                type_next = False
+                continue
+            if property_name:
+                if desc_next:
+                    description = line.strip('\n')
+                    desc_next = False
+                if line.startswith('* **Return type**'):
+                    type_next = True
+                    lines_to_skip = 5
+                if type_next:
+                    if lines_to_skip:
+                        if len(line) > 1:
+                            type = line.strip(' \n')
+                        lines_to_skip -= 1
+                    else:
+                        out.write(f"{property_name} **{type}** - {description}\n")
+                        property_name = ""
+                        type_next = False
+                continue
             line = line.replace('# noqa: E501', '')
             line = line.replace('models.md#tator.models.', 'models.md#')
             line = line.replace('api.md#tator.api.', 'api.md#')
+            line = line.replace(', local_vars_configuration=None', '')
             hash_start = line.find('models.md#')
             hash_end = line.rfind(')')
             if hash_start != -1 and hash_end != -1:
                 line = line[:hash_start] + line[hash_start:hash_end].lower() + line[hash_end:]
             if '**Parameters**' in line:
                 skip_newlines = True
-            if '**Returns**' in line:
+            if '**Returns**' in line and title.lower() != 'models':
                 out.write('\n')
                 skip_newlines = False
             if ((line.startswith('### tator') 
@@ -66,11 +93,11 @@ if __name__ == '__main__':
                 and line.endswith(')\n')):
                 func = line.split('(')[0].replace(' _class_', '').replace(' _exception_', '').split('.')[-1]
                 signature = line.split(' ', 1)[1].strip('\n')
-                out.write('----------------------------------\n\n')
+                out.write('\n----------------------------------\n\n')
                 out.write(f"### {func}\n\n")
                 out.write(f"{signature}\n\n")
                 continue
-            if not (line.isspace() and skip_newlines):
+            if not ((line.isspace() and skip_newlines) or title.lower() == 'models'):
                 out.write(line)
             if line.startswith('## REST API'):
                 out.write("\n:::note\n\n")
