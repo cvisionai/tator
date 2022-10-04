52d377afac8e203fb558e5d8e7ba71d3dd83693e
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2022-10-04 01:36:55 +0000

    Fix path to aws cli

--- .circleci/push_aws.sh
+++ .circleci/push_aws.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo $(aws ecr get-login-password) | docker login "$AWS_REGISTRY" --username AWS --password-stdin
+docker login "$AWS_REGISTRY" --username AWS --password $(/home/ubuntu/.local/bin/aws ecr get-login-password)
 docker tag cvisionai/tator_online:$CIRCLE_SHA1 $AWS_REGISTRY/tator_online:$CIRCLE_SHA1
 docker push $AWS_REGISTRY/tator_online:$CIRCLE_SHA1
 docker tag cvisionai/tator_graphql:$CIRCLE_SHA1 $AWS_REGISTRY/tator_graphql:$CIRCLE_SHA1
