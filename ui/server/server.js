7cd97a3865010b04476b920deeafd1c75eeab288
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2022-11-01 14:07:07 +0000

    Add http proxy for express (will be used when run standalone)

--- ui/server/server.js
+++ ui/server/server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const favicon = require('serve-favicon');
+const proxy = require('express-http-proxy');
 const yargs = require('yargs/yargs');
 const app = express();
 const port = 3000;
@@ -100,9 +101,11 @@ app.get('/rest', (req, res) => {
   res.render('browser', params);
 });
 
-app.use((req, res, next) => {
-  res.status(404).end();
-});
+app.use('*', proxy(argv.backend, {
+  proxyReqPathResolver: function(req) {
+    return req.originalUrl;
+  }
+}));
 
 app.listen(port, () => {
   console.log('Started express server!');
