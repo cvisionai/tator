6fee8111d8471d3d8a87e9265fb668c150de0b2d
Brian Tate <brian.tate@cvisionai.com>
2022-11-04 14:26:40 +0000

    Fix path issues of worker.

--- main/worker.py
+++ main/worker.py
@@ -2,13 +2,21 @@ from redis import Redis
 from rq import Queue, Worker
 
 import os
+import sys
 
-def test_it():
+def push_job(function, *args, **kwargs):
+  """ Example:
+  push_job(print, args=('Hello',))
+  or
+  push_job(print, 'Hello')
+  See [https://python-rq.org/docs/] for information on available kwargs
+  """
   redis = Redis(host=os.getenv('REDIS_HOST'))
   queue = Queue('async_jobs', connection=redis)
-  queue.enqueue(print, args=('Hello World',), result_ttl=0)
+  queue.enqueue(function, *args, **kwargs)
 
 if __name__=="__main__":
+  sys.path.append("/tator_online") # lookup for source files from django
   redis = Redis(host=os.getenv('REDIS_HOST'))
   queue = Queue('async_jobs', connection=redis)
 
