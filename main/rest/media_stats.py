f81ff5daf9b28649fc6b77c30b959534084eec72
Brian Tate <brian.tate@cvisionai.com>
2022-11-07 20:36:38 +0000

    Re-implement MediaStats endpoint.

--- main/rest/media_stats.py
+++ main/rest/media_stats.py
@@ -1,14 +1,21 @@
+import logging
 from collections import defaultdict
 
-from django.db.models import Sum
+from django.db import connection
+from django.db.models import Sum, Avg, Count
 
 from ..models import Media
 from ..search import TatorSearch
 from ..schema import MediaStatsSchema
 
+from ._media_query import get_media_queryset
+
 from ._base_views import BaseDetailView
 from ._permissions import ProjectViewOnlyPermission
 
+logger = logging.getLogger(__name__)
+
+from collections import defaultdict
 class MediaStatsAPI(BaseDetailView):
     """ Count, download size, and total size of a media list.
 
@@ -21,7 +28,62 @@ class MediaStatsAPI(BaseDetailView):
 
     def _get(self, params):
         
-        # Do query.
-        response_data = {}
+        qs = get_media_queryset(params['project'], params)
+        # Count
+        # Download size
+        # total_size
+        # duration
+        duration = 0
+        counts=defaultdict(lambda : 0)
+        sizes=defaultdict(lambda : 0)
+
+        # Re-wrap query to get columns accessible
+        ids = [x['id'] for x in qs.values()]
+        qs = Media.objects.filter(pk__in=ids)
+
+        # Run aggregations
+        agg = qs.filter(meta__dtype='video').aggregate(total_frames=Sum('num_frames'), total_fps=Sum('fps'))
+
+        with connection.cursor() as cursor:
+            # Get counts of types
+            for key in ['video', 'image', 'multi', 'live']:
+                type_qs = qs.filter(meta__dtype=key)
+                counts[key] = type_qs.count()
+            
+                if key == 'image':
+                    type_ids = list(type_qs.values_list('id', flat=True))
+                    type_ids = [str(x) for x in type_ids]
+                    type_ids = ",".join(type_ids)
+                    cursor.execute(f"SELECT SUM(CAST(image->'size' AS int)) FROM (SELECT jsonb_array_elements(media_files->'image') AS image from main_media WHERE id IN ({type_ids})) AS selector")
+                    res = cursor.fetchone()
+                    if res[0]:
+                        sizes[key] += res[0]
+                elif key == 'video':
+                    type_ids = list(type_qs.values_list('id', flat=True))
+                    type_ids = [str(x) for x in type_ids]
+                    type_ids = ",".join(type_ids)
+                    sql_str = f"SELECT SUM(CAST(streaming->'size' AS int)) FROM (SELECT jsonb_array_elements(media_files->'streaming') AS streaming from main_media WHERE id IN ({type_ids})) AS selector"
+                    logger.info(sql_str)
+                    cursor.execute(sql_str)
+                    res = cursor.fetchone()
+                    if res[0]:
+                        sizes[key] =+ res[0]
+                    cursor.execute(f"SELECT SUM(CAST(archival->'size' AS int)) FROM (SELECT jsonb_array_elements(media_files->'archival') AS archival from main_media WHERE id IN ({type_ids})) AS selector")
+                    res = cursor.fetchone()
+                    if res[0]:
+                        sizes[key] += res[0]
+            
+        
+        if counts['video'] > 0:
+            avg_fps = agg['total_fps'] / counts['video']
+            duration = agg['total_frames'] / avg_fps
+
+            
+        response_data = {'count': qs.count(), 'duration': duration}
+        for key in ['video', 'image', 'multi', 'live']:
+            response_data[f"total_{key}_count"] = counts[key]
+        
+        for key in ['video', 'image']:
+            response_data[f"total_{key}_size"] = sizes[key]
         return response_data
 
--- main/schema/components/media_stats.py
+++ main/schema/components/media_stats.py
@@ -1,9 +1,14 @@
 media_stats = {
     'type': 'object',
     'properties': {
-        'count': {'type': 'integer', 'minimum': 0},
-        'download_size': {'type': 'integer', 'minimum': 0},
-        'total_size': {'type': 'integer', 'minimum': 0},
-        'duration': {'type': 'number', 'minimum': 0},
+        'count': {'type': 'integer', 'minimum': 0, 'description': 'Number of total media elements'},
+        'duration': {'type': 'number', 'minimum': 0, 'description': 'Number of seconds of video'},
+        'total_video_count': {'type': 'integer', 'minimum': 0, 'description': 'Total number of videos'},
+        'total_image_count': {'type': 'integer', 'minimum': 0, 'description': 'Total number of images'},
+        'total_live_count': {'type': 'integer', 'minimum': 0, 'description': 'Total number of live videos'},
+        'total_multi_count': {'type': 'integer', 'minimum': 0,'description':  'Total number of multi videos'},
+        'total_video_size': {'type': 'integer', 'minimum': 0, 'description': 'Total size of video in bytes'},
+        'total_image_size': {'type': 'integer', 'minimum': 0, 'description': 'Total size of images in bytes'},
+
     },
 }
