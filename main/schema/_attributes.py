f4c07a8610b1964f752ae2080f8d82fb75b23218
Brian Tate <brian.tate@cvisionai.com>
2022-11-10 13:29:10 +0000

    Add related_attribute to query params.

--- main/schema/_attributes.py
+++ main/schema/_attributes.py
@@ -81,6 +81,111 @@ attribute_filter_parameter_schema = [
         'explode': False,
     },
     {
+        'name': 'related_attribute',
+        'in': 'query',
+        'required': False,
+        'description': 'Attribute equality filter. Format is '
+                       'attribute1::value1,[attribute2::value2].'
+                       'This filter is applied to related data of the primary object.'
+                       'On the Media endpoint, this searches on related metadata(States/Localizations).'
+                       'On metadata endpoints, this searches on related media.',
+        'schema': {'type': 'array',
+                   'items': {'type': 'string'}},
+        'explode': False,
+    },
+    {
+        'name': 'related_attribute_lt',
+        'in': 'query',
+        'required': False,
+        'description': 'Attribute less than filter. Format is '
+                       'attribute1::value1,[attribute2::value2].'
+                        'This filter is applied to related data of the primary object.'
+                       'On the Media endpoint, this searches on related metadata(States/Localizations).'
+                       'On metadata endpoints, this searches on related media.',
+        'schema': {'type': 'array',
+                   'items': {'type': 'string'}},
+        'explode': False,
+    },
+    {
+        'name': 'related_attribute_lte',
+        'in': 'query',
+        'required': False,
+        'description': 'Attribute less than or equal filter. Format is '
+                       'attribute1::value1,[attribute2::value2].'
+                        'This filter is applied to related data of the primary object.'
+                       'On the Media endpoint, this searches on related metadata(States/Localizations).'
+                       'On metadata endpoints, this searches on related media.',
+        'schema': {'type': 'array',
+                   'items': {'type': 'string'}},
+        'explode': False,
+    },
+    {
+        'name': 'related_attribute_gt',
+        'in': 'query',
+        'required': False,
+        'description': 'Attribute greater than filter. Format is '
+                       'attribute1::value1,[attribute2::value2].'
+                       'This filter is applied to related data of the primary object.'
+                       'On the Media endpoint, this searches on related metadata(States/Localizations).'
+                       'On metadata endpoints, this searches on related media.',
+        'schema': {'type': 'array',
+                   'items': {'type': 'string'}},
+        'explode': False,
+    },
+    {
+        'name': 'related_attribute_gte',
+        'in': 'query',
+        'required': False,
+        'description': 'Attribute greater than or equal filter. Format is '
+                       'attribute1::value1,[attribute2::value2].'
+                       'This filter is applied to related data of the primary object.'
+                       'On the Media endpoint, this searches on related metadata(States/Localizations).'
+                       'On metadata endpoints, this searches on related media.',
+        'schema': {'type': 'array',
+                   'items': {'type': 'string'}},
+        'explode': False,
+    },
+    {
+        'name': 'related_attribute_contains',
+        'in': 'query',
+        'required': False,
+        'description': 'Attribute contains filter. Format is '
+                       'attribute1::value1,[attribute2::value2].'
+                       'This filter is applied to related data of the primary object.'
+                       'On the Media endpoint, this searches on related metadata(States/Localizations).'
+                       'On metadata endpoints, this searches on related media.',
+        'schema': {'type': 'array',
+                   'items': {'type': 'string'}},
+        'explode': False,
+    },
+    {
+        'name': 'related_attribute_distance',
+        'in': 'query',
+        'required': False,
+        'description': 'Range filter for geoposition attributes. Format is '
+                       'attribute1::distance_km2::lat2::lon2,'
+                       '[attribute2::distancekm2::lat2::lon2].'
+                       'This filter is applied to related data of the primary object.'
+                       'On the Media endpoint, this searches on related metadata(States/Localizations).'
+                       'On metadata endpoints, this searches on related media.',
+        'schema': {'type': 'array',
+                   'items': {'type': 'string'}},
+        'explode': False,
+    },
+    {
+        'name': 'related_attribute_null',
+        'in': 'query',
+        'required': False,
+        'description': 'Attribute null filter. Returns elements for which '
+                       'a given attribute is not defined.'
+                       'This filter is applied to related data of the primary object.'
+                       'On the Media endpoint, this searches on related metadata(States/Localizations).'
+                       'On metadata endpoints, this searches on related media.',
+        'schema': {'type': 'array',
+                   'items': {'type': 'string'}},
+        'explode': False,
+    },
+    {
         'name': 'start',
         'in': 'query',
         'required': False,
