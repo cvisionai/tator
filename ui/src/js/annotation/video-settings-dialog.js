f6f06b326bc79a5b82983a54abfe87be5571285b
Mark Taipan <mark.taipan@cvisionai.com>
2022-12-04 22:43:03 -0500

    Video settings UI cleanup and added warning when resolution in a multi are not the same

--- ui/src/js/annotation/video-settings-dialog.js
+++ ui/src/js/annotation/video-settings-dialog.js
@@ -1,5 +1,6 @@
 import { ModalDialog } from "../components/modal-dialog.js";
 import { fetchRetry } from "../util/fetch-retry.js";
+import { Utilities } from "../util/utilities.js";
 
 export class VideoSettingsDialog extends ModalDialog {
   constructor() {
@@ -14,12 +15,21 @@ export class VideoSettingsDialog extends ModalDialog {
 
     this._controlsButton = document.createElement("button");
     this._controlsButton.setAttribute("class", "tab-btn");
-    this._controlsButton.textContent = "Controls"
+    this._controlsButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="no-fill mr-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z"></path>
+    <circle cx="12" cy="12" r="3"></circle>
+ </svg> Controls`;
     this._tabsDiv.appendChild(this._controlsButton);
 
     this._infoButton = document.createElement("button");
     this._infoButton.setAttribute("class", "tab-btn");
-    this._infoButton.textContent = "Media Info";
+    this._infoButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="no-fill mr-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <circle cx="12" cy="12" r="9"></circle>
+    <line x1="12" y1="8" x2="12.01" y2="8"></line>
+    <polyline points="11 12 12 12 12 16 13 16"></polyline>
+ </svg> Media Info`;
     this._tabsDiv.appendChild(this._infoButton);
 
     this._controlsButton.addEventListener("click", () => {
@@ -228,7 +238,7 @@ export class VideoSettingsDialog extends ModalDialog {
     var tbody = document.createElement("tbody");
     table.appendChild(tbody);
 
-    var attrs = ["archive_state", "codec", "fps", "height", "id", "num_frames", "width"];
+    var attrs = ["archive_state", "codec", "fps", "height", "id", "name", "num_frames", "width"];
     for (const attr of attrs) {
       var trData = document.createElement("tr");
       tbody.appendChild(trData);
@@ -448,11 +458,40 @@ export class VideoSettingsDialog extends ModalDialog {
     }
 
   }
+
   /**
-   * #TODO only the first entry in the given media array is used.
-   *       Future versions of this should display the medias
-   *
-   * @param {string} mode - single|multi
+   * Currently only checks to see if the video medias referenced by the multi have the same resolutions.
+   * @precondition this._multiMedias must have been set
+   */
+  _checkMultiMediaValidity() {
+
+    for (const mediaA of this._multiMedias) {
+
+      var resA = new Set();
+      for (const media_file of mediaA.media_files.streaming) {
+        resA.add(`${media_file.resolution[0]},${media_file.resolution[1]}`);
+      }
+
+      for (const mediaB of this._multiMedias) {
+
+        var resB = new Set();
+        for (const media_file of mediaB.media_files.streaming) {
+          resB.add(`${media_file.resolution[0]},${media_file.resolution[1]}`);
+        }
+        
+        var sameResolutions = (resA.size === resB.size || [...resA].every(value => resB.has(value)));
+        if (!sameResolutions) {
+          Utilities.warningAlert("Videos do not have the same resolutions!");
+          break;
+        }
+
+      }
+    }
+
+  }
+
+  /**
+   * @param {string} mode - single|multi|live
    * @param {Tator.Media} media - Parent media
    */
   async mode(mode, media) {
@@ -466,6 +505,7 @@ export class VideoSettingsDialog extends ModalDialog {
     else if (this._mode == "multi") {
       await this._queryMultiMedia();
       var mainVideo = this._multiMedias[0];
+      this._checkMultiMediaValidity();
     }
     else {
       var mainVideo = this._parentMedia;
