d618ed755f80b51007f62d58e446dfd209f41af5
Hugh Enxing <hugh.enxing@cvisionai.com>
2023-01-04 13:39:17 -0500

    Updates UI for new bucket configuration
    (cherry picked from commit c8105ec47ca452f13a3b8a03295cb5838a226269)

--- ui/src/js/organization-settings/bucket-edit.js
+++ ui/src/js/organization-settings/bucket-edit.js
@@ -6,14 +6,6 @@ export class BucketEdit extends OrganizationTypeForm {
     this.typeName = "Bucket";
     this.readableTypeName = "Bucket";
     this.icon = '<svg class="SideNav-icon" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" ><path d="M11 1c-4.908 0-7.998 1.592-7.79 2.95 0.060 0.389 0.225 1.945 0.434 3.273-2.405 0.934-3.202 2.449-3.095 3.684 0.127 1.461 1.441 3.025 4.328 3.295 1.648 0.154 3.631-0.75 4.916-2.295-0.122-0.207-0.193-0.45-0.193-0.707 0-0.774 0.627-1.4 1.4-1.4s1.4 0.627 1.4 1.4c0 0.762-0.61 1.381-1.369 1.398-1.529 1.973-3.858 3.164-6.064 3.025 0.051 0.324 0.070 0.947 0.096 1.113 0.090 0.579 2.347 2.26 5.937 2.264 3.59-0.004 5.847-1.685 5.938-2.263 0.088-0.577 1.641-11.409 1.852-12.787 0.208-1.358-2.883-2.95-7.79-2.95zM1.943 10.785c-0.055-0.643 0.455-1.498 1.924-2.139 0.211 1.34 0.441 2.797 0.643 4.074-1.604-0.313-2.498-1.149-2.567-1.935zM11 6.024c-3.59-0.002-6.137-1.334-6.137-1.832-0.002-0.494 2.547-1.79 6.137-1.788 3.59-0.002 6.139 1.294 6.137 1.788 0 0.498-2.547 1.83-6.137 1.832z"></path></svg>';
-  
-    // used to set up form
-    this._currentBucketType = null;
-    this.bucketInputs = new Map(); // setup later
-    this._bucketFieldsByType = new Map();
-    this._bucketFieldsByType.set("aws", ["access_key", "secret_key", "endpoint_url", "region", "archive_sc", "live_sc"]);
-    this._bucketFieldsByType.set("gcs", ["archive_sc", "live_sc", "gcs_key_info"]);
-    this._allFields = ["access_key", "secret_key", "endpoint_url", "region", "archive_sc", "live_sc", "gcs_key_info"];
   }
 
   init(data) {
@@ -25,27 +17,17 @@ export class BucketEdit extends OrganizationTypeForm {
       "id": `New`,
       "organization": this.organizationId,
       "name": "",
-      "access_key": "",
-      "secret_key": "",
-      "endpoint_url": null,
-      "region": "",
-      "archive_sc": "",
-      "live_sc": "",
-      "gcs_key_info": null,
+      "store_type": null,
+      "external_host": null,
+      "archive_sc": "STANDARD",
+      "live_sc": "STANDARD",
+      "config": null,
       "form": "empty"
     };
   }
 
   _getAttributeSection() {
     return document.createElement("div");
-  }
-
-  _ignoreConfigField() {
-    return !(
-      this.data.config
-      && Object.keys(this.data.config).length === 0
-      && this.data.config.constructor === Object
-    )
   }
 
   _getSectionForm(data = null) {
@@ -76,8 +58,10 @@ export class BucketEdit extends OrganizationTypeForm {
     // type
     if (this.data.id == "New") {
       let bucketTypes = [
-        { name: "AWS", id: "aws", checked: false },
-        { name: "GCS", id: "gcs", checked: false },
+        { name: "AWS", id: "AWS", checked: false },
+        { name: "GCP", id: "GCP", checked: false },
+        { name: "MINIO", id: "MINIO", checked: false },
+        { name: "OCI", id: "OCI", checked: false },
       ];
 
       this._editBucketType = document.createElement("radio-set");
@@ -85,82 +69,30 @@ export class BucketEdit extends OrganizationTypeForm {
       this._editBucketType.setValue(bucketTypes);
       this._editBucketType.default = bucketTypes;
       this._form.appendChild(this._editBucketType);
-      this._editBucketType.addEventListener("change", this._setBucketType.bind(this)); 
     } 
 
-
-    // access key
-    this._editAccessKey = document.createElement("text-input");
-    this._editAccessKey.setAttribute("name", "AWS Access Key ID");
-    this._editAccessKey.setAttribute("type", "string");
-    if (this._ignoreConfigField()) {
-      this._editAccessKey.setValue(this.data.access_key);
-      this._editAccessKey.default = this.data.access_key;
-    } else {
-      this._editAccessKey.setValue(this.data.config.aws_access_key_id);
-      this._editAccessKey.default = this.data.config.aws_access_key_id;
-    }
-    this._editAccessKey.hidden = true;
-    this.bucketInputs.set("access_key", this._editAccessKey);
-    this._editAccessKey.addEventListener("change", this._formChanged.bind(this));
-    this._form.appendChild(this._editAccessKey);
-
-    // secret key
-    this._editSecretKey = document.createElement("text-input");
-    this._editSecretKey.setAttribute("name", "AWS Secret Access Key");
-    this._editSecretKey.setAttribute("type", "password");
-    if (this._ignoreConfigField()) {
-      this._editSecretKey.setValue(this.data.secret_key);
-      this._editSecretKey.default = this.data.secret_key;
-    } else {
-      this._editSecretKey.setValue(this.data.config.aws_secret_access_key);
-      this._editSecretKey.default = this.data.config.aws_secret_access_key;
-    }
-    this._editSecretKey.hidden = true;
-    this.bucketInputs.set("secret_key", this._editSecretKey);
-    this._editSecretKey.addEventListener("change", this._formChanged.bind(this));
-    this._form.appendChild(this._editSecretKey);
-
-    // endpoint url
-    this._editEndpointUrl = document.createElement("text-input");
-    this._editEndpointUrl.setAttribute("name", "Endpoint URL");
-    this._editEndpointUrl.setAttribute("type", "string");
-    if (this._ignoreConfigField()) {
-      this._editEndpointUrl.setValue(this.data.endpoint_url);
-      this._editEndpointUrl.default = this.data.endpoint_url;
-    } else {
-      this._editEndpointUrl.setValue(this.data.config.endpoint_url);
-      this._editEndpointUrl.default = this.data.config.endpoint_url;
-    }
-    this._editEndpointUrl.hidden = true;
-    this.bucketInputs.set("endpoint_url", this._editEndpointUrl);
-    this._editEndpointUrl.addEventListener("change", this._formChanged.bind(this));
-    this._form.appendChild(this._editEndpointUrl);
-
-    // region
-    this._editRegion = document.createElement("text-input");
-    this._editRegion.setAttribute("name", "Region Name");
-    this._editRegion.setAttribute("type", "string");
-    if (this._ignoreConfigField()) {
-      this._editRegion.setValue(this.data.region);
-      this._editRegion.default = this.data.region;
-    } else {
-      this._editRegion.setValue(this.data.config.region_name);
-      this._editRegion.default = this.data.config.region_name;
-    }
-    this._editRegion.hidden = true;
-    this.bucketInputs.set("region", this._editRegion);
-    this._editRegion.addEventListener("change", this._formChanged.bind(this));
-    this._form.appendChild(this._editRegion);
+    // external host
+    this._editExternalHost = document.createElement("text-input");
+    this._editExternalHost.setAttribute("name", "External Host");
+    this._editExternalHost.setAttribute("type", "string");
+    this._editExternalHost.setValue(this.data.external_host);
+    this._editExternalHost.default = this.data.external_host;
+    this._editExternalHost.hidden = false;
+    this._editExternalHost.addEventListener("change", this._formChanged.bind(this));
+    this._form.appendChild(this._editExternalHost);
 
     // archive storage class
     this._editArchiveSc = document.createElement("text-input");
     this._editArchiveSc.setAttribute("name", "Archive Storage Class");
     this._editArchiveSc.setAttribute("type", "string");
+    // if (this.data.id == "New") {
+    //   this._editArchiveSc.setValue("STANDARD");
+    //   this._editArchiveSc.default = "STANDARD";
+    // } else {
     this._editArchiveSc.setValue(this.data.archive_sc);
     this._editArchiveSc.default = this.data.archive_sc;
-    this._editArchiveSc.hidden = true;
-    this.bucketInputs.set("archive_sc", this._editArchiveSc);
+    // }
+    this._editArchiveSc.hidden = false;
     this._editArchiveSc.addEventListener("change", this._formChanged.bind(this));
     this._form.appendChild(this._editArchiveSc);
 
@@ -168,75 +100,33 @@ export class BucketEdit extends OrganizationTypeForm {
     this._editLiveSc = document.createElement("text-input");
     this._editLiveSc.setAttribute("name", "Live Storage Class");
     this._editLiveSc.setAttribute("type", "string");
+    // if (this.data.id == "New") {
+    //   this._editLiveSc.setValue("STANDARD");
+    //   this._editLiveSc.default = "STANDARD";
+    // } else {
     this._editLiveSc.setValue(this.data.live_sc);
     this._editLiveSc.default = this.data.live_sc;
-    this._editLiveSc.hidden = true;
-    this.bucketInputs.set("live_sc", this._editLiveSc);
+    // }
+    this._editLiveSc.hidden = false;
     this._editLiveSc.addEventListener("change", this._formChanged.bind(this));
     this._form.appendChild(this._editLiveSc);
 
-    // GCS key info
-    this._editGcsKeyInfo = document.createElement("text-area");
-    this._editGcsKeyInfo.setAttribute("name", "GCS Key Info");
-    this._editGcsKeyInfo.setAttribute("type", "json");
-    this._editGcsKeyInfo.setValue(this.data.gcs_key_info);
-    this._editGcsKeyInfo.default = this.data.gcs_key_info;
-    this._editGcsKeyInfo.hidden = true;
-    this.bucketInputs.set("gcs_key_info", this._editGcsKeyInfo);
-    this._editGcsKeyInfo.addEventListener("change", this._formChanged.bind(this));
-    this._form.appendChild(this._editGcsKeyInfo);
-
-    if(data.id !== "New") {
-      this._showBestGuess();
-    }
+    // config
+    this._editConfig = document.createElement("text-area");
+    this._editConfig.setAttribute("name", "Config");
+    this._editConfig.setAttribute("type", "json");
+    this._editConfig.setValue("");
+    this._editConfig.default = "";
+    this._editConfig.hidden = false;
+    this._editConfig.addEventListener("change", this._formChanged.bind(this));
+    this._form.appendChild(this._editConfig);
 
     current.appendChild(this._form);
-
     return current;
-  }
-
-  _showBestGuess() {
-    let hasGcsInfo = (this.data.gcs_key_info !== null);
-    let hasAwsInfo = (this.data.region !== null || this.data.aws_secret_access_key !== null || this.data.aws_access_key_id !== null);
-
-    if (hasGcsInfo && !hasAwsInfo) {
-      this._showBucketFields("gcs");
-    } else if (hasAwsInfo && !hasGcsInfo) {
-      this._showBucketFields("aws");
-    } else  { //if (hasGcsInfo && hasAwsInfo), or for some other reason
-      for (let field of this._allFields) {
-        this._currentBucketType = "none";
-        this.bucketInputs.get(field).hidden = false;
-      }
-    }
-  }
-
-  _setBucketType() {
-    let type = this._editBucketType.getValue();
-    this._showBucketFields(type);
-  }
-
-  _showBucketFields(type) {
-    this._currentBucketType = type;
-    let hideType = type == "aws" ? "gcs" : "aws";
-    
-    // hide non-relevent fields
-    // #todo expand to loops list of hideTypes if we have > 2 (ie. Wasabi)
-    for (let field of this._bucketFieldsByType.get(hideType)) {
-      // console.log(this.bucketInputs.get(field));
-      this.bucketInputs.get(field).hidden = true;
-    }
-    
-    // show relevent fields
-    for (let field of this._bucketFieldsByType.get(type)) {
-      // console.log(this.bucketInputs.get(field));
-      this.bucketInputs.get(field).hidden = false;
-    }
   }
 
   _getFormData(id) {
     let formData = {};
-    let bucketType = this._currentBucketType !== null ? this._currentBucketType : "none";
     const isNew = this.data.id == "New";
 
     if (this._editName.changed() || isNew) {
@@ -251,17 +141,16 @@ export class BucketEdit extends OrganizationTypeForm {
       formData.live_sc = this._editLiveSc.getValue();
     }
 
-    if (["aws", "none"].includes(bucketType)) {
-      formData.store_type = "AWS"
-      formData.config = {
-        "aws_access_key_id": this._editAccessKey.getValue(),
-        "aws_secret_access_key": this._editSecretKey.getValue(),
-        "endpoint_url": this._editEndpointUrl.getValue(),
-        "region_name": this._editRegion.getValue(),
-      }
-    } else if (bucketType === "gcs") {
-      formData.store_type = "GCP"
-      formData.config = this._editGcsKeyInfo.getValue();
+    if (this._editConfig.changed() || isNew) {
+      formData.config = JSON.parse(this._editConfig.getValue());
+    }
+
+    if (this._editBucketType.changed() || isNew) {
+      formData.store_type = this._editBucketType.getValue();
+    }
+
+    if (this._editExternalHost.changed() || isNew) {
+      formData.external_host = this._editExternalHost.getValue();
     }
 
     return formData;
