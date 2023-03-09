a229e1a326d942da6f7d13976529d253f08c5497
Brian Tate <brian.tate@cvisionai.com>
2023-03-09 12:18:11 -0500

    Check for proper image/video loading in unit tests.

--- test/test_annotation.py
+++ test/test_annotation.py
@@ -93,8 +93,7 @@ def common_annotation(page, canvas, bias=0):
         page.wait_for_timeout(5000)
 
 
-#@pytest.mark.flaky(reruns=2)
-@pytest.mark.skip(reason="Toolbar buttons not working in playwright.")
+@pytest.mark.flaky(reruns=2)
 def test_video_annotation(page_factory, project, video):
     print("[Video] Going to annotation view...")
     page = page_factory(f"{os.path.basename(__file__)}__{inspect.stack()[0][3]}")
@@ -104,11 +103,11 @@ def test_video_annotation(page_factory, project, video):
     page.wait_for_selector('video-canvas')
     canvas = page.query_selector('video-canvas')
     page.wait_for_selector('play-button:not(.disabled)')
-    common_annotation(page, canvas)
+    # This is commented out because toolbar buttons don't work in playwright
+    #common_annotation(page, canvas)
     page.close()
     
-#@pytest.mark.flaky(reruns=2)
-@pytest.mark.skip(reason="Toolbar buttons not working in playwright.")
+@pytest.mark.flaky(reruns=2)
 def test_image_annotation(page_factory, project, image):
     print("[Image] Going to annotation view...")
     page = page_factory(f"{os.path.basename(__file__)}__{inspect.stack()[0][3]}")
@@ -117,11 +116,11 @@ def test_image_annotation(page_factory, project, image):
     page.on("pageerror", print_page_error)
     page.wait_for_selector('image-canvas')
     canvas = page.query_selector('image-canvas')
-    common_annotation(page, canvas)
+    # This is commented out because toolbar buttons don't work in playwright
+    #common_annotation(page, canvas)
     page.close()
 
-#@pytest.mark.flaky(reruns=2)
-@pytest.mark.skip(reason="Toolbar buttons not working in playwright.")
+@pytest.mark.flaky(reruns=2)
 def test_multi_annotation(page_factory, project, multi):
     print("[Multi] Going to annotation view...")
     page = page_factory(f"{os.path.basename(__file__)}__{inspect.stack()[0][3]}")
@@ -134,5 +133,6 @@ def test_multi_annotation(page_factory, project, multi):
     page.wait_for_timeout(2000) # Not sure what else to wait for here but sometimes vids load after play button enabled
     assert(len(canvas) == 2)
     common_annotation(page, canvas[0])
-    common_annotation(page, canvas[1], bias=3)
+    # This is commented out because toolbar buttons don't work in playwright
+    #common_annotation(page, canvas[1], bias=3)
     page.close()
