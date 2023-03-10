7155458cc6af227e2d8f54073f03856bcb1dbd6e
Hugh Enxing <hugh.enxing@cvisionai.com>
2023-03-10 16:09:06 -0500

    Updates openapi-core package

--- api/main/schema/_parse.py
+++ api/main/schema/_parse.py
@@ -1,12 +1,43 @@
 import logging
 
-from openapi_core import create_spec
-from openapi_core.validation.request.validators import RequestValidator
+from openapi_core import Spec, validate_request
+from openapi_core.validation.request.protocols import RequestValidator
 from openapi_core.contrib.django import DjangoOpenAPIRequest
 
 from ._generator import CustomGenerator
 
 logger = logging.getLogger(__name__)
+
+# def parse(request):
+#     """ Parses a request using Tator's generated OpenAPI spec.
+#     """
+#     if parse.spec is None:
+#         generator = CustomGenerator(title='Tator REST API')
+#         spec = generator.get_schema(parser=True)
+#         parse.spec = Spec.from_dict(spec)
+#     openapi_request = DjangoOpenAPIRequest(request)
+#     if openapi_request.mimetype.startswith('application/json') \
+#         or (not openapi_request.mimetype):
+#         openapi_request.mimetype = 'application/json'
+#     result = validate_request(request, spec=spec)
+#     result.raise_for_errors()
+#     out = {
+#         **result.parameters.path,
+#         **result.parameters.query,
+#     }
+#     if result.body:
+#         out['body'] = result.body
+#         if not isinstance(result.body, list):
+#             out = {**result.body, **out}
+
+#     # Handle turning off merge if single version is selected
+#     if 'merge' in out:
+#         if len(out.get('version',[])) <= 1:
+#             out['merge'] = 0
+#     return out
+
+# parse.spec = None
+
 
 def parse(request):
     """ Parses a request using Tator's generated OpenAPI spec.
@@ -14,7 +45,7 @@ def parse(request):
     if parse.validator is None:
         generator = CustomGenerator(title='Tator REST API')
         spec = generator.get_schema(parser=True)
-        openapi_spec = create_spec(spec)
+        openapi_spec = Spec.from_dict(spec)
         parse.validator = RequestValidator(openapi_spec)
     openapi_request = DjangoOpenAPIRequest(request)
     if openapi_request.mimetype.startswith('application/json') \
