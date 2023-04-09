7eb664fddce8b01913ade169db28b08526321d41
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2023-04-09 16:34:39 +0000

    Include the issue time in local storage so we can check when a refresh is needed

--- ui/server/static/oidc.js
+++ ui/server/static/oidc.js
@@ -20,13 +20,15 @@ function exchangeAuthToken () {
     return response.json();
   })
   .then((data) => {
+    const issueTime = new Date();
     localStorage.setItem("access_token", data.access_token);
     localStorage.setItem("expires_in", data.expires_in);
     localStorage.setItem("id_token", data.id_token);
     localStorage.setItem("token_type", data.token_type);
+    localStorage.setItem("issue_time", issueTime.toISOString());
   })
   .catch((error) => {
     console.error("Error exchanging token!");
-    //window.location.href = "/accounts/login";
+    window.location.href = "/accounts/login";
   });
 }
