f7f72d834e9477616e3beea40866fc794ff4c6a4
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2023-04-09 18:10:10 +0000

    Add logic to check if login required when keycloak is enabled

--- ui/server/static/require-login.js
+++ ui/server/static/require-login.js
@@ -5,22 +5,35 @@ function getCookie(name) {
     return parts.pop().split(";").shift();
   }
 }
-const backend = window.localStorage.getItem("backend");
+
+function goToLogin() {
+  window.location.href = `/accounts/login?next=${window.location.pathname}`;
+}
+
+const backend = localStorage.getItem("backend");
+const keycloakEnabled = localStorage.getItem("keycloak_enabled");
 if (window.location.pathname != "/accounts/login") {
-  fetch(`${backend}/accounts/account-profile`, {
-    method: "GET",
-    credentials: "include",
-    headers: {
-      "X-CSRFToken": getCookie("csrftoken"),
-      "Accept": "application/json",
-      "Content-Type": "application/json"
+  if (keycloakEnabled) {
+    let accessToken = localStorage.getItem("accessToken");
+    if (accessToken === null) {
+      toToLogin();
     }
-  })
-  .then(response => {
-    const url = new URL(response.url);
-    if (response.status == 301 || url.pathname.startsWith('/accounts/login')) {
-      window.location.href = `/accounts/login?next=${window.location.pathname}`;
-    }
-  });
+  } else {
+    fetch(`${backend}/accounts/account-profile`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "X-CSRFToken": getCookie("csrftoken"),
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+      const url = new URL(response.url);
+      if (response.status == 301 || url.pathname.startsWith('/accounts/login')) {
+        goToLogin();
+      }
+    });
+  }
 }
   
