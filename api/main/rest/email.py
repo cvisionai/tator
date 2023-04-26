89a5ec5a26d02b9a5264d8ff50e9f885eaf3eeb2
Hugh Enxing <hugh.enxing@cvisionai.com>
2023-04-26 13:51:18 -0400

    Updates email rest endpoint

--- api/main/rest/email.py
+++ api/main/rest/email.py
@@ -1,6 +1,7 @@
-
+# pylint: disable=import-error
 import logging
 import os
+import re
 import traceback
 
 from django.conf import settings
@@ -13,79 +14,65 @@ from ..tator_mail import get_email_service
 
 logger = logging.getLogger(__name__)
 
+
 class EmailAPI(BaseListView):
-    """ API to send an email message using Tator
-    """
+    """API to send an email message using Tator"""
+
     schema = EmailSchema()
     permission_classes = [ProjectExecutePermission]
-    http_method_names = ['post']
+    http_method_names = ["post"]
 
     def _post(self, params):
-        """ Sends an email message via boto3
-        """
+        """Sends an email message via the configured mail service"""
 
         if not settings.TATOR_EMAIL_ENABLED:
             raise ValueError("Email not enabled")
 
-        # Wrapped code in try/except incase there are security related issues with exceptions
-        # that are bubbled up
-        error = False
-        try:
+        # Verify recipients exist and are in the same project
+        project = params["project"]
+        recipients = params["recipients"]
+        for entry in recipients:
+            # Check if in RFC 5322 compliant format
+            try:
+                email_addr = re.search(
+                    r"^([^<]*?<)?(?P<email>[^<@\s]+@[^@\s]+[.][^@\s]+?)>?$", entry
+                ).group("email")
+            except AttributeError:
+                raise ValueError(
+                    f"Invalid email address '{entry}' received; addresses must be RFC 5322 compliant"
+                )
 
-            # Verify recipients are in the same project
-            project = params['project']
-            recipients = params['recipients']
-            for entry in recipients:
+            # Correlate with a user object
+            qs = User.objects.filter(email=email_addr)
+            if not qs.exists():
+                raise ValueError(f"Recipient (email: {email_addr}) does not correlate with a user")
 
-                # Get the email address
-                if "<" in entry:
-                    email_addr = entry[entry.find("<")+1:entry.find(">")]
+            # Verify user is part of the project
+            user = qs.first()
+            qs = Membership.objects.filter(user_id=user.id, project_id=project)
+            if not qs.exists():
+                raise ValueError(f"Recipient (user_id: {user.id}) not part of project")
 
-                else:
-                    # It should just be email@email.com
-                    email_addr = entry
+        # Grab the contents of the message
+        subject = params["subject"]
+        if not subject:
+            raise ValueError("Subject must not be empty")
 
-                qs = User.objects.filter(email=email_addr)
-                user = qs[0]
+        text_body = params["text"]
+        if not text_body:
+            raise ValueError("Body must not be empty")
 
-                # Correlate with a user object
-                if len(qs) < 1:
-                    error_msg = f"Recipient (email: {email_addr}) does not correlate with a user"
-                    raise ValueError(error_msg)
+        # Grab the attachments if there are any
+        attachments = params.get("attachments", [])
 
-                # Verify user is part of the project
-                qs = Membership.objects.filter(user_id=user.id, project_id=project)
-                if len(qs) < 1:
-                    error_msg = f"Recipient (user_id: {user.id}) not part of project"
-                    raise ValueError(error_msg)
+        # Send email
+        get_email_service().email(
+            sender=settings.TATOR_EMAIL_SENDER,
+            recipients=recipients,
+            title=subject,
+            text=text_body,
+            attachments=attachments,
+            raise_on_failure="Email response was not 200. Error occured",
+        )
 
-            # Grab the contents of the message
-            subject = params['subject']
-            if not subject:
-                raise ValueError("Subject must not be empty")
-
-            text_body = params['text']
-            if not text_body:
-                raise ValueError("Body must not be empty")
-
-            # Grab the attachments if there are any
-            attachments = params.get('attachments', [])
-
-            # Send email
-            get_email_service().email(
-                sender=settings.TATOR_EMAIL_SENDER,
-                recipients=recipients,
-                title=subject,
-                text=text_body,
-                attachments=attachments,
-                raise_on_failure="Email response was not 200. Error occured"
-            )
-
-        except Exception as exc:
-            logger.error(f"Exception in REST email POST endpoint:\n{traceback.format_exc()}")
-            error = True
-
-        if error:
-            raise ValueError("Error sending email message")
-
-        return {'message': f"Email message sent!"}
+        return {"message": f"Email message sent!"}
