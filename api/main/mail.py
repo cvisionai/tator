28b242b2bcd9347bc1d3457f85191cdd7178714d
Hugh Enxing <hugh.enxing@cvisionai.com>
2023-05-24 14:07:04 -0400

    Adjusts email wording

--- api/main/mail.py
+++ api/main/mail.py
@@ -55,7 +55,7 @@ class TatorMail(ABC):
         """
         if settings.TATOR_EMAIL_NOTIFY_STAFF:
             if add_footer and text:
-                footer = "\n\nThis message has been sent to all deployment staff. No action is required."
+                footer = " This message has been sent to all deployment staff. No action is required."
                 text += footer
 
             # Get all non-empty staff emails
