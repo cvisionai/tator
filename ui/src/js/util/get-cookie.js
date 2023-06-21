f55bb69a5f116e0594a1f9a8518a355af63f8157
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2023-06-21 12:52:11 +0000

    Add `getCookie` utility back into UI code

---
+++ ui/src/js/util/get-cookie.js
@@ -0,0 +1,7 @@
+export function getCookie(name) {
+  let value = "; " + document.cookie;
+  let parts = value.split("; " + name + "=");
+  if (parts.length == 2) {
+    return parts.pop().split(";").shift();
+  }
+}
