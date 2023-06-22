76289e6301520e6058ded366d9cf3bd28af7c21b
Mark Taipan <mark.taipan@cvisionai.com>
2023-06-21 21:23:43 -0400

    update format

--- ui/src/js/components/inputs/bool-input.js
+++ ui/src/js/components/inputs/bool-input.js
@@ -113,7 +113,6 @@ export class BoolInput extends TatorElement {
    * @param {string} val - Style string associated with the attribute type
    */
   setStyle(val) {
-
     if (typeof val != "string") {
       console.warn(`Provided style is not a string`);
       return;
@@ -124,24 +123,19 @@ export class BoolInput extends TatorElement {
       if (token.includes("label-css-add-")) {
         var classAdd = token.split("label-css-add-")[1];
         this._legend.classList.add(classAdd);
-      }
-      else if (token.includes("label-css-rem")) {
+      } else if (token.includes("label-css-rem")) {
         var classRem = token.split("label-css-rem-")[1];
         this._legend.classList.remove(classRem);
-      }
-      else if (token.includes("field-css-add")) {
+      } else if (token.includes("field-css-add")) {
         var classAdd = token.split("field-css-add-")[1];
         this._controls.classList.add(classAdd);
-      }
-      else if (token.includes("field-css-rem")) {
+      } else if (token.includes("field-css-rem")) {
         var classRem = token.split("field-css-rem-")[1];
         this._controls.classList.remove(classRem);
-      }
-      else if (token.includes("css-add-")) {
+      } else if (token.includes("css-add-")) {
         var classAdd = token.split("css-add-")[1];
         this.label.classList.add(classAdd);
-      }
-      else if (token.includes("css-rem-")) {
+      } else if (token.includes("css-rem-")) {
         var classRem = token.split("css-rem-")[1];
         this.label.classList.add(classRem);
       }
