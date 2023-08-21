076a456281ed368f3c60f37e0b1e52e9cd0ae797
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2023-08-21 13:42:24 +0000

    Always include authentication middleware, otherwise cron jobs break if keycloak is enabled

--- api/tator_online/settings.py
+++ api/tator_online/settings.py
@@ -78,6 +78,7 @@ MIDDLEWARE = (
         "django.middleware.security.SecurityMiddleware",
         "django.contrib.sessions.middleware.SessionMiddleware",
         "django.middleware.common.CommonMiddleware",
+        "django.contrib.auth.middleware.AuthenticationMiddleware",
     ]
     + (
         [
@@ -86,7 +87,6 @@ MIDDLEWARE = (
         if KEYCLOAK_ENABLED
         else [
             "django.middleware.csrf.CsrfViewMiddleware",
-            "django.contrib.auth.middleware.AuthenticationMiddleware",
         ]
     )
     + [
