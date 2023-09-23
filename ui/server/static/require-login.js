683fe05ebba1ac3d4e21cc4abed0d0b15374849d
Hugh Enxing <hugh.enxing@cvisionai.com>
2023-09-23 16:55:15 -0400

    Adds redirect page to no login views

--- ui/server/static/require-login.js
+++ ui/server/static/require-login.js
@@ -5,8 +5,11 @@ function getCookie(name) {
     return parts.pop().split(";").shift();
   }
 }
+const NO_LOGIN_VIEWS = [
+  '/accounts/login', '/redirect/login',
+]
 const backend = window.localStorage.getItem("backend");
-if (window.location.pathname != "/accounts/login") {
+if (!NO_LOGIN_VIEWS.includes(window.location.pathname)) {
   fetch(`${backend}/accounts/account-profile`, {
     method: "GET",
     credentials: "include",
