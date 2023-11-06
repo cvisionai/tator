bcc40a26a46215fddd511f274dce7e52b0a3a8a1
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2023-11-06 17:20:22 +0000

    Add download utility using file system access API

---
+++ ui/src/js/util/download-file-list.js
@@ -0,0 +1,34 @@
+async function downloadFile(name, url, index, existing, dirHandle) {
+  let msg;
+  if (existing.has(name)) {
+    msg = `Skipping file ${name}, already exists...`;
+  } else {
+    msg = `Downloading file ${name}...\n`;
+    const response = await fetch(url);
+    const data = await response.blob();
+    const fileHandle = await dirHandle.getFileHandle(name, {create: true});
+    const writableStream = await fileHandle.createWritable();
+    await writableStream.write(data);
+    await writableStream.close();
+  }
+  return msg;
+}
+
+export async function downloadFileList(names, urls, callback) {
+  const dirHandle = await window.showDirectoryPicker({mode: "readwrite", startIn: "downloads"});
+  if (dirHandle) {
+    const existing = new Set();
+    for await (const entry of dirHandle.values()) {
+      if (entry.kind === "file") {
+        existing.add(entry.name);
+      }
+    }
+    for (let i = 0; i < urls.length; i++) {
+      const msg = await downloadFile(names[i], urls[i], i, existing, dirHandle);
+      if (typeof callback !== "undefined") {
+        callback(i, msg);
+      }
+    }
+  }
+}
+
