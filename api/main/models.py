6195aaa13417bc863aec0bb6d05df4089b088573
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2024-01-22 16:02:34 +0000

    Make `latest_mark` nullable for backward compatible migration

--- api/main/models.py
+++ api/main/models.py
@@ -1816,7 +1816,7 @@ class Localization(Model, ModelDiffMixin):
     """ Indicates this is a variant that is deleted """
     mark = PositiveIntegerField(default=0, blank=True)
     """ Mark represents the revision number of the element  """
-    latest_mark = PositiveIntegerField(default=0, blank=True)
+    latest_mark = PositiveIntegerField(default=0, blank=True, null=True)
     """ Mark represents the latest revision number of the element  """
 
 
