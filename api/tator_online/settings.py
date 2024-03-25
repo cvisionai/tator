fe1c71968f79f84c4d5a5cde5b8ba03d351cf15f
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2024-03-25 17:47:29 +0000

    Remove statsd from REST service

--- api/tator_online/settings.py
+++ api/tator_online/settings.py
@@ -91,13 +91,6 @@ MIDDLEWARE = (
             "django.middleware.csrf.CsrfViewMiddleware",
         ]
     )
-    + (
-        [
-            "tator_online.StatsdMiddleware",
-        ]
-        if STATSD_ENABLED
-        else []
-    )
     + [
         "django.contrib.messages.middleware.MessageMiddleware",
         "django.middleware.clickjacking.XFrameOptionsMiddleware",
