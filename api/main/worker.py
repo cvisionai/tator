3dcb4eb58ed8dcf115a3c015aa583a222dfd227e
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2024-03-29 12:34:42 +0000

    Conditionally format logs

--- api/main/worker.py
+++ api/main/worker.py
@@ -7,9 +7,13 @@ import sys
 import argparse
 import logging
 
-FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
-          '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
-          '- %(message)s')
+if os.getenv("DD_AGENT_HOST"):
+    FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
+              '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
+              '- %(message)s')
+else:
+    FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
+              '- %(message)s')
 logging.basicConfig(format=FORMAT)
 
 def push_job(queue, function, *args, **kwargs):
--- api/tator_online/settings.py
+++ api/tator_online/settings.py
@@ -194,11 +194,15 @@ ASGI_APPLICATION = "tator_online.routing.application"
 
 
 # Turn on logging
-FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
-          '[dd.service=%(dd.service)s dd.env=%(dd.env)s '
-          'dd.version=%(dd.version)s '
-          'dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
-          '- %(message)s')
+if os.getenv("DD_AGENT_HOST"):
+    FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
+              '[dd.service=%(dd.service)s dd.env=%(dd.env)s '
+              'dd.version=%(dd.version)s '
+              'dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
+              '- %(message)s')
+else:
+    FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
+              '- %(message)s')
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
