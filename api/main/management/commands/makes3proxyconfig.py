2a2fb52ee885370cb6547528cfd6eca6c892188e
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2024-06-17 19:49:43 +0000

    Add management command for converting scratch buckets to s3proxy configs

---
+++ api/main/management/commands/makes3proxyconfig.py
@@ -0,0 +1,62 @@
+from django.core.management.base import BaseCommand
+import os
+
+from main.models import Project
+
+def _write_conf(path, project):
+    bucket = project.scratch_bucket
+    store_type = bucket.store_type.lower()
+    config = bucket.config
+    outpath = os.path.join(path, f"project-{project.id}.conf")
+    with open(outpath, 'w') as f:
+        if store_type in ["aws", "minio", "vast"]:
+            outpath = _write_s3_config(f, project, config)
+        elif store_type == "oci":
+            outpath = _write_s3_config(f, project, config)
+        elif store_type == "gcp":
+            outpath = _write_gcp_config(f, project, config)
+        else:
+            logger.warning(f"Failed to write s3proxy config for project {project.id}, unrecognized store type {bucket.store_type}")
+    return outpath
+
+def _write_s3_config(f, project, config):
+    f.write("s3proxy.endpoint=http://0.0.0.0:80\n")
+    f.write("s3proxy.authorization=none\n")
+    f.write("jclouds.provider=s3\n")
+    f.write(f"jclouds.endpoint={config['endpoint_url']}\n")
+    f.write(f"jclouds.identity={config['aws_access_key_id']}\n")
+    f.write(f"jclouds.credential={config['aws_secret_access_key']}\n")
+    f.write(f"jclouds.region={config['region_name']}\n")
+
+def _write_oci_config(f, project, config):
+    f.write("s3proxy.endpoint=http://0.0.0.0:80\n")
+    f.write("s3proxy.authorization=none\n")
+    f.write("jclouds.provider=s3\n")
+    f.write(f"jclouds.endpoint={config['boto3_config']['endpoint_url']}\n")
+    f.write(f"jclouds.identity={config['boto3_config']['aws_access_key_id']}\n")
+    f.write(f"jclouds.credential={config['boto3_config']['aws_secret_access_key']}\n")
+    f.write(f"jclouds.region={config['boto3_config']['region_name']}\n")
+
+def _write_gcp_config(path, project, config):
+    f.write("s3proxy.endpoint=http://0.0.0.0:80\n")
+    f.write("s3proxy.authorization=none\n")
+    f.write("jclouds.provider=google-cloud-storage\n")
+    f.write(f"jclouds.identity={config['private_key_id']}\n")
+    f.write(f"jclouds.credential={config['private_key']}\n")
+
+def _write_script(path, confs):
+    outpath = os.path.join(path, "s3proxy.sh")
+    cmd = "s3proxy "
+    for conf in confs:
+        cmd += f"--properties {conf} "
+    with open(outpath, "w") as f:
+        f.write(cmd)
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("--outdir", type=str)
+
+    def handle(self, **options):
+        projects = Project.objects.filter(scratch_bucket__isnull=False)
+        confs = [_write_config(options.outdir, project) for project in projects.iterator()]
+        _write_script(options.outdir, confs)
