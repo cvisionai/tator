4773d93403c3aebd19de4ed3e5b1608c6f6b09e1
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2024-08-16 23:48:28 -0400

    Use top level css in tator element, remove scss for converted css

--- ui/src/js/components/tator-element.js
+++ ui/src/js/components/tator-element.js
@@ -1,25 +1,15 @@
 export const svgNamespace = "http://www.w3.org/2000/svg";
 
-class StyleHolder {
-  constructor() {
-    this.css = new CSSStyleSheet();
-    this.css.insertRule("* { visibility: hidden }");
-    this.ready = false;
-    this.elements = [];
-    fetch("/static/index.css")
-      .then((body) => body.text())
-      .then((text) => {
-        this.css.replaceSync(text);
-      });
-  }
-}
-
-let global_style = new StyleHolder();
+import variables from "/static/css/variables.css" with { type: "css" };
+import base from "/static/css/base.css" with { type: "css" };
+import text from "/static/css/text.css" with { type: "css" };
+import reset from "/static/css/reset.css" with { type: "css" };
+import utilities from "/static/css/utilities.css" with { type: "css" };
 
 export class TatorElement extends HTMLElement {
   constructor() {
     super();
     this._shadow = this.attachShadow({ mode: "open" });
-    this._shadow.adoptedStyleSheets = [global_style.css];
+    this._shadow.adoptedStyleSheets.push(variables, base, text, reset, utilities);
   }
 }
