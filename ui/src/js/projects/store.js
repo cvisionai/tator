6b39ea8d00aa27e5b8a52060a59848f7da4d2e0d
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2024-08-23 15:11:27 -0400

    Get projects page working

--- ui/src/js/projects/store.js
+++ ui/src/js/projects/store.js
@@ -1,113 +1,138 @@
-import create from "zustand/vanilla";
-import { subscribeWithSelector } from "zustand/middleware";
-import { getApi } from "../../../../scripts/packages/tator-js/pkg/src/index.js";
-
-const api = getApi();
+import create from '../../../node_modules/zustand/esm/vanilla.mjs';
+import { subscribeWithSelector } from '../../../node_modules/zustand/esm/middleware.js';
+import { fetchCredentials } from '../../../../scripts/packages/tator-js/src/utils/fetch-credentials.js';
 
 async function configureImageClassification(project) {
-  let response = await api.createMediaType(project.id, {
-    name: "Images",
-    dtype: "image",
-    attribute_types: [
-      {
-        name: "Label",
-        description: "Image classification label.",
-        dtype: "string",
-        order: 0,
-      },
-    ],
-  });
+  let response = await fetchCredentials(`/rest/MediaType/${project.id}`, {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Images",
+      dtype: "image",
+      attribute_types: [
+        {
+          name: "Label",
+          description: "Image classification label.",
+          dtype: "string",
+          order: 0,
+        },
+      ],
+    }),
+  }).then(response => response.json());
   console.log(response.message);
 }
 
 async function configureObjectDetection(project) {
-  let response = await api.createMediaType(project.id, {
-    name: "Images",
-    dtype: "image",
-    attribute_types: [],
-  });
+  let response = await fetchCredentials(`/rest/MediaType/${project.id}`, {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Images",
+      dtype: "image",
+      attribute_types: [],
+    }),
+  }).then(response => response.json());
   console.log(response.message);
   const imageTypeId = response.id;
 
-  response = await api.createMediaType(project.id, {
-    name: "Videos",
-    dtype: "video",
-    attribute_types: [],
+  response = await fetchCredentials(`/rest/MediaType/${project.id}`, {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Videos",
+      dtype: "video",
+      attribute_types: [],
+    }),
   });
   console.log(response.message);
   const videoTypeId = response.id;
 
-  response = await api.createLocalizationType(project.id, {
-    name: "Boxes",
-    dtype: "box",
-    media_types: [imageTypeId, videoTypeId],
-    attribute_types: [
-      {
-        name: "Label",
-        description: "Object detection label.",
-        dtype: "string",
-        order: 0,
-      },
-    ],
+  response = await fetchCredentials(`/rest/LocalizationType/${project.id}`, {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Boxes",
+      dtype: "box",
+      media_types: [imageTypeId, videoTypeId],
+      attribute_types: [
+        {
+          name: "Label",
+          description: "Object detection label.",
+          dtype: "string",
+          order: 0,
+        },
+      ],
+    }),
   });
   console.log(response.message);
 }
 
 async function configureMultiObjectTracking(project) {
-  let response = await api.createMediaType(project.id, {
-    name: "Videos",
-    dtype: "video",
-    attribute_types: [],
+  let response = await fetchCredentials(`/rest/MediaType/${project.id}`, {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Videos",
+      dtype: "video",
+      attribute_types: [],
+    }),
   });
   console.log(response.message);
   const videoTypeId = response.id;
 
-  response = await api.createStateType(project.id, {
-    name: "Tracks",
-    association: "Localization",
-    interpolation: "none",
-    media_types: [videoTypeId],
-    attribute_types: [
-      {
-        name: "Label",
-        description: "Track label.",
-        dtype: "string",
-        order: 0,
-      },
-    ],
+  response = await fetchCredentials(`/rest/StateType/${project.id}`, {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Tracks",
+      association: "Localization",
+      interpolation: "none",
+      media_types: [videoTypeId],
+      attribute_types: [
+        {
+          name: "Label",
+          description: "Track label.",
+          dtype: "string",
+          order: 0,
+        },
+      ],
+    }),
   });
   console.log(response.message);
 
-  response = await api.createLocalizationType(project.id, {
-    name: "Boxes",
-    dtype: "box",
-    media_types: [videoTypeId],
-    attribute_types: [],
+  response = await fetchCredentials(`/rest/LocalizationType/${project.id}`, {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Boxes",
+      dtype: "box",
+      media_types: [videoTypeId],
+      attribute_types: [],
+    }),
   });
 }
 
 async function configureActivityRecognition(project) {
-  let response = await api.createMediaType(project.id, {
-    name: "Videos",
-    dtype: "video",
-    attribute_types: [],
+  let response = await fetchCredentials(`/rest/MediaType/${project.id}`, {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Videos",
+      dtype: "video",
+      attribute_types: [],
+    }),
   });
   console.log(response.message);
   const videoTypeId = response.id;
 
-  response = await api.createStateType(project.id, {
-    name: "Activities",
-    association: "Frame",
-    interpolation: "latest",
-    media_types: [videoTypeId],
-    attribute_types: [
-      {
-        name: "Something in view",
-        description: "Whether something is happening in the video.",
-        dtype: "bool",
-        order: 0,
-      },
-    ],
+  response = await fetchCredentials(`/rest/StateType/${project.id}`, {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Activities",
+      association: "Frame",
+      interpolation: "latest",
+      media_types: [videoTypeId],
+      attribute_types: [
+        {
+          name: "Something in view",
+          description: "Whether something is happening in the video.",
+          dtype: "bool",
+          order: 0,
+        },
+      ],
+    }),
   });
   console.log(response.message);
 }
@@ -120,10 +145,14 @@ const store = create(
     organizations: [],
     init: async () => {
       Promise.all([
-        api.whoami(),
-        api.getAnnouncementList(),
-        api.getProjectList(),
-        api.getOrganizationList(),
+        fetchCredentials(`/rest/User/GetCurrent`, {}, true)
+          .then((response) => response.json()),
+        fetchCredentials('/rest/Announcements', {}, true)
+          .then((response) => response.json()),
+        fetchCredentials('/rest/Projects', {}, true)
+          .then((response) => response.json()),
+        fetchCredentials('/rest/Organizations', {}, true)
+          .then((response) => response.json()),
       ]).then((values) => {
         set({
           user: values[0],
@@ -134,7 +163,10 @@ const store = create(
       });
     },
     addProject: async (projectSpec, preset) => {
-      let response = await api.createProject(projectSpec);
+      let response = await fetchCredentials(`/rest/Projects`, {
+        method: "POST",
+        body: JSON.stringify(projectSpec),
+      }).then(response => response.json());
       const project = response.object;
       console.log(response.message);
       switch (preset) {
@@ -159,7 +191,9 @@ const store = create(
       return project;
     },
     removeProject: async (id) => {
-      const response = await api.deleteProject(id);
+      const response = await fetchCredentials(`/rest/Project/${id}`, {
+        method: "DELETE"
+      }).then(response => response.json());
       console.log(response.message);
       set({ projects: get().projects.filter((project) => project.id != id) });
     },
