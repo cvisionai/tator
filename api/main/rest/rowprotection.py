e5519115b28d7bc3116333ed2f39c975e8460266
Brian Tate <brian.tate@cvisionai.com>
2024-08-22 20:37:12 -0400

    Start fleshing out REST implementation for RowProtection post

--- api/main/rest/rowprotection.py
+++ api/main/rest/rowprotection.py
@@ -13,12 +13,7 @@ from django.http import Http404
 from rest_framework.exceptions import PermissionDenied
 import yaml
 
-from ..models import Project
-from ..models import Affiliation
-from ..models import Algorithm
-from ..models import Organization
-from ..models import RowProtection, User
-from ..models import database_qs
+from ..models import *
 from ..schema import RowProtectionListSchema, RowProtectionDetailSchema
 from ._base_views import BaseDetailView
 from ._base_views import BaseListView
@@ -53,7 +48,36 @@ class RowProtectionListAPI(BaseListView):
         """
         Registers a new job cluster using the provided parameters
         """
-        pass
+        target_objects = {
+            "project": Project,
+            "media": Media,
+            "file": File,
+            "section": Section,
+            "algorithm": Algorithm,
+            "version": Version,
+            "target_organization": Organization,
+            "target_group": Group,
+            "job_cluster": JobCluster,
+            "bucket": Bucket,
+            "hosted_template": HostedTemplate,
+        }
+
+        originators = ["user", "organization", "group"]
+
+        destination_type = []
+        for target in target_objects.keys():
+            if params.get(target, None):
+                destination_type.append(target)
+
+        assert len(destination_type) == 1, f"Only one destination is allowed {self.params}"
+        destination_type = destination_type[0]
+
+        originator_type = []
+        for originator in originators:
+            if params.get(originator, None):
+                originator_type.append(originator)
+        assert len(originator_type) == 1, f"Only one originator is allowed {self.params}"
+        originator_type = originator_type[0]
 
 
 class RowProtectionDetailAPI(BaseDetailView):
@@ -78,7 +102,6 @@ class RowProtectionDetailAPI(BaseDetailView):
 
     def _get(self, params):
         """Retrieve the requested Group by ID"""
-        logger.info(f"PARAMS={params}")
         row_protection = self.get_queryset().first()
         return model_to_dict(row_protection)
 
