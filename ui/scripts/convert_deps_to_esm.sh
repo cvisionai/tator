c393db8fa4c8a10349dd4e8aae73bf7645489c71
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2024-08-23 11:24:00 -0400

    Use esbuild to convert commonjs deps to es modules, set import map for tator-js deps

---
+++ ui/scripts/convert_deps_to_esm.sh
@@ -0,0 +1,4 @@
+./node_modules/esbuild/bin/esbuild node_modules/libtess/libtess.min.js --format=esm --outfile=node_modules/libtess/libtess.mjs
+./node_modules/esbuild/bin/esbuild node_modules/underwater-image-color-correction/index.js --format=esm --outfile=node_modules/underwater-image-color-correction/underwater-image-color-correction.mjs
+./node_modules/esbuild/bin/esbuild node_modules/earcut/dist/earcut.min.js --format=esm --outfile=node_modules/earcut/earcut.mjs
+./node_modules/esbuild/bin/esbuild node_modules/spark-md5/spark-md5.min.js --format=esm --outfile=node_modules/spark-md5/spark-md5.mjs
