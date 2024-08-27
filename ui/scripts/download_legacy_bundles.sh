d0a5ea92ff54c5b593595f075d25abf75b9440fa
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2024-08-27 17:10:47 -0400

    Download a zip file instead of individual files

--- ui/scripts/download_legacy_bundles.sh
+++ ui/scripts/download_legacy_bundles.sh
@@ -1,22 +1,3 @@
-basenames=(
-  "tator-ui"
-  "project-settings"
-  "project-detail"
-  "organization-settings"
-  "password-reset"
-  "annotation"
-  "organizations"
-  "account-profile"
-  "token"
-  "projects"
-  "util"
-  "registration"
-  "components"
-  "portal"
-  "third-party"
-)
 mkdir -p legacy
-for basename in "${basenames[@]}"; do
-  wget -O "legacy/${basename}.js" "https://tator-ci.s3.us-east-1.amazonaws.com/bundles_1-3-12/${basename}.js"
-  wget -O "legacy/${basename}.css" "https://tator-ci.s3.us-east-1.amazonaws.com/bundles_1-3-12/${basename}.css"
-done
+wget -O "/tmp/bundles_1-3-12.zip" "https://tator-ci.s3.us-east-1.amazonaws.com/bundles_1-3-12.zip"
+unzip -o "/tmp/bundles_1-3-12.zip" -d "legacy"
