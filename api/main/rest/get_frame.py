9317569f365ce8c4989863e012210669ff090199
Brian Tate <brian.tate@cvisionai.com>
2024-08-27 11:11:24 -0400

    Fix GetFrame endpoint behavior

--- api/main/rest/get_frame.py
+++ api/main/rest/get_frame.py
@@ -32,8 +32,8 @@ class GetFrameAPI(BaseDetailView):
     permission_classes = [ProjectViewOnlyPermission]
     http_method_names = ["get"]
 
-    def get_queryset(self):
-        return Media.objects.filter(pk=self.params["id"])
+    def get_queryset(self, **kwargs):
+        return self.filter_only_viewables(Media.objects.filter(pk=self.params["id"]))
 
     def handle_exception(self, exc):
         logger.error(f"Exception in request: {traceback.format_exc()}")
