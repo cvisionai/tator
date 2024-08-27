123fab7ecffe3888fcfb742893a166c061f5ae78
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2024-08-27 17:00:18 -0400

    Remove static files from NGINX

--- scripts/collect-static.sh
+++ scripts/collect-static.sh
@@ -4,11 +4,3 @@ echo "Copying ui"
 docker cp ui ui:/tator_online/ui
 echo "Copying tator-js"
 docker cp scripts/packages/tator-js ui:/tator_online/scripts/packages/tator-js
-
-# Copy static files using an nginx container
-echo "Copying static files"
-docker cp ui/server/static nginx:/
-docker cp ui/dist nginx:/
-docker cp scripts/packages/tator-js/src/annotator/vid_downloader.js nginx:/static
-docker exec nginx sh -c "cp /dist/* /static/."
-
