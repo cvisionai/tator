6079c659067f9cba217f085f83c8a1ad0ab64337
Qianwei Yin <qianweiyin22@gmail.com>
2024-11-03 16:10:02 -0500

    group/RP calculator handle no data, not permitted, I am admin and check other users permission, etc. situations

--- ui/src/js/permission-settings/components/permission-settings-button.js
+++ ui/src/js/permission-settings/components/permission-settings-button.js
@@ -204,6 +204,16 @@ class FilterAddButton extends PermissionSettingsButton {
     );
   }
 }
+class QuestionMarkButton extends PermissionSettingsButton {
+  constructor() {
+    super();
+    this.init(
+      "",
+      "M144 416c-17.67 0-32 14.33-32 32s14.33 32.01 32 32.01s32-14.34 32-32.01S161.7 416 144 416zM211.2 32H104C46.66 32 0 78.66 0 136v16C0 165.3 10.75 176 24 176S48 165.3 48 152v-16c0-30.88 25.12-56 56-56h107.2C244.7 80 272 107.3 272 140.8c0 22.66-12.44 43.27-32.5 53.81L167 232.8C137.1 248 120 277.9 120 310.6V328c0 13.25 10.75 24.01 24 24.01S168 341.3 168 328V310.6c0-14.89 8.188-28.47 21.38-35.41l72.47-38.14C297.7 218.2 320 181.3 320 140.8C320 80.81 271.2 32 211.2 32z",
+      "0 0 320 512"
+    );
+  }
+}
 
 customElements.define("permission-settings-button", PermissionSettingsButton);
 customElements.define("edit-line-button", EditLineButton);
@@ -217,3 +227,4 @@ customElements.define("sort-ascending-button", SortAscendingButton);
 customElements.define("sort-descending-button", SortDescendingButton);
 customElements.define("x-mark-button", XMarkButton);
 customElements.define("filter-add-button", FilterAddButton);
+customElements.define("question-mark-button", QuestionMarkButton);
