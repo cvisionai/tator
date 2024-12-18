592d55bf51f78b516573392a704c0921d201508e
Brian Tate <brian.tate@cvisionai.com>
2024-12-18 09:54:09 -0500

    Remove mouse handler for resize logic

--- ui/src/js/annotation/annotation-multi-resizer.js
+++ ui/src/js/annotation/annotation-multi-resizer.js
@@ -1,44 +1,8 @@
-class ResizeMouseHandler
-{
-    constructor(resizerBar)
-    {
-        this._resizerBar = resizerBar;
-        this._resizerBar.addEventListener('mousedown', this._onMouseDown.bind(this));
-        this._resizerBar.addEventListener('mouseup', this._onMouseUp.bind(this));
-        this._resizerBar.addEventListener('mousemove', this._onMouseMove.bind(this));
-        this._resizerBar.addEventListener('mouseout', this._onMouseOut.bind(this));
-    }
-
-    _onMouseDown(e)
-    {
-        this._mouseDown = true;
-        this._resizerBar.style.backgroundColor = 'white';
-    }
-
-    _onMouseUp(e)
-    {
-        this._mouseDown = false;
-        this._resizerBar.style.backgroundColor = null;
-    }
-
-    _onMouseOut(e)
-    {
-        this._resizerBar.style.backgroundColor = null
-    }
-
-    _onMouseMove(e)
-    {
-        if (this._mouseDown)
-        {
-            // Resize the multi
-        }
-    }
-}
 export class AnnotationMultiResizer
 {
     constructor(multi, resizerBar)
     {
         this._multi = multi;
-        this._mouseHandler = new ResizeMouseHandler(resizerBar);
+        this._resizerBar = resizerBar;
     }
 }
