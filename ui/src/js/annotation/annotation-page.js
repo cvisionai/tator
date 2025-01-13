5172455d711139adaf8678ccabe69fd7e0e2419a
Brian Tate <brian.tate@cvisionai.com>
2025-01-13 14:36:35 -0500

    Fix seek of localization-less tracks

--- ui/src/js/annotation/annotation-page.js
+++ ui/src/js/annotation/annotation-page.js
@@ -1340,7 +1340,7 @@ export class AnnotationPage extends TatorPage {
                   // select track takes care of frame jump
                   canvas.selectTrack(
                     evt.detail.data,
-                    undefined,
+                    evt.detail.data.frame,
                     !evt.detail.goToEntityFrame
                   );
                 } else if ("frame" in evt.detail.data) {
