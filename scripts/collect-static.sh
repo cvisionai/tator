1ccc12042516ecb881c2c208b1e1da28ff591e08
Brian Tate <brian.tate@cvisionai.com>
2025-01-18 13:31:45 -0500

    Fix collect-static

--- scripts/collect-static.sh
+++ scripts/collect-static.sh
@@ -3,4 +3,4 @@
 echo "Copying ui"
 docker cp ui ui:/tator_online
 echo "Copying tator-js"
-docker cp scripts/packages/tator-js ui:/tator_online/scripts/packages/tator-js
+docker cp scripts/packages/tator-js ui:/tator_online/scripts/packages
