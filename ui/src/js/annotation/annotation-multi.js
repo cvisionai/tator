268005c73b2acfd51a16f5a5dd11c6f42a7ca0dd
Brian Tate <brian.tate@cvisionai.com>
2025-01-27 16:05:51 -0500

    Fix race condition when pause button gets disabled

--- ui/src/js/annotation/annotation-multi.js
+++ ui/src/js/annotation/annotation-multi.js
@@ -2745,6 +2745,11 @@ export class AnnotationMulti extends TatorElement {
     var pausePromises = [];
     let failSafeFunction = () => {
       clearTimeout(this._failSafeTimer);
+      if (this._videoStatus != "paused") {
+        // Timer didn't get cancelled by playing, so the fail safe
+        // will be bad to execute
+        return;
+      }
       this._videoStatus = "paused";
       if (afterPause) {
         afterPause();
