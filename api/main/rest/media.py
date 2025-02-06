5bfdda6f8d3536f71dba409f6e02669ce2147e2f
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2025-02-06 14:25:15 -0500

    Formatting

--- api/main/rest/media.py
+++ api/main/rest/media.py
@@ -596,7 +596,9 @@ class MediaListAPI(BaseListView):
                         project=params["project"], pk=params["primary_section"]
                     )
                     if not section.exists():
-                        raise ValueError(f"Folder with ID {params['primary_section']} does not exist")
+                        raise ValueError(
+                            f"Folder with ID {params['primary_section']} does not exist"
+                        )
                     update_kwargs["primary_section"] = section[0]
             if new_attrs is not None or update_kwargs != {}:
                 attr_count = len(ids_to_update)
@@ -805,7 +807,9 @@ class MediaDetailAPI(BaseDetailView):
                         project=media.project, pk=params["primary_section"]
                     )
                     if not section.exists():
-                        raise ValueError(f"Folder with ID {params['primary_section']} does not exist")
+                        raise ValueError(
+                            f"Folder with ID {params['primary_section']} does not exist"
+                        )
                     qs.update(primary_section=section[0])
 
             if "multi" in params:
