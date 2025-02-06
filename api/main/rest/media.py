4fc58d555a9664b913e13c826dd40c402532c629
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2025-02-06 13:33:42 -0500

    Allow patch of only primary section in bulk media patch

--- api/main/rest/media.py
+++ api/main/rest/media.py
@@ -533,9 +533,10 @@ class MediaListAPI(BaseListView):
             and params.get("user_elemental_id") == None
             and params.get("reset_attributes") is None
             and params.get("null_attributes") is None
+            and params.get("primary_section") is None
         ):
             raise ValueError(
-                "Must specify 'attributes', 'reset_attributes', 'null_attributes', 'user_elemental_id',"
+                "Must specify 'attributes', 'reset_attributes', 'null_attributes', 'user_elemental_id', 'primary_section'"
                 " and/or property to patch, but none found"
             )
         qs = get_media_queryset(params["project"], params)
