b171c7de165cdfa6b341d4724da97ed9a63ae99c
Brian Tate <brian.tate@cvisionai.com>
2025-02-25 17:04:06 -0500

    Disable automatic index creation

--- api/main/tests.py
+++ api/main/tests.py
@@ -60,32 +60,8 @@ class TatorTransactionTest(APITransactionTestCase):
 
 
 def wait_for_indices(entity_type):
-    entity_type.refresh_from_db()
-    attr_indices = [x for x in entity_type.attribute_types if x["dtype"] != "blob"]
-    built_ins = BUILT_IN_INDICES.get(type(entity_type), [])
-    types_to_scan = [*attr_indices, *built_ins]
-    # Wait for btree indices too
-    for t in types_to_scan:
-        if t["dtype"] == "string":
-            new_obj = {**t}
-            new_obj["dtype"] = "string_btree"
-            types_to_scan.append(new_obj)
-        if t["dtype"] == "native_string":
-            new_obj = {**t}
-            new_obj["dtype"] = "native_string_btree"
-            types_to_scan.append(new_obj)
-        if t["dtype"] == "upper_string":
-            new_obj = {**t}
-            new_obj["dtype"] = "upper_string_btree"
-            types_to_scan.append(new_obj)
-    for attribute in types_to_scan:
-        found_it = False
-        for i in range(1, 600):
-            if TatorSearch().is_index_present(entity_type, attribute) == True:
-                found_it = True
-                break
-            time.sleep(0.1 * min(i, 30))
-        assert found_it
+    """ No longer wait for indices"""
+    return
 
 
 def assertResponse(self, response, expected_code):
