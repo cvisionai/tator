286e67cad5d5dce44432603174696d3b3fc0c7dd
Brian Tate <brian.tate@cvisionai.com>
2025-02-26 11:14:47 -0500

    Disable preview seek when playing backwards

--- ui/src/js/annotation/annotation-multi.js
+++ ui/src/js/annotation/annotation-multi.js
@@ -1,7 +1,7 @@
 import { TatorElement } from "../components/tator-element.js";
 import { Utilities } from "../util/utilities.js";
 import { MultiRenderer } from "../../../../scripts/packages/tator-js/src/annotator/multi-renderer.js";
-import { RATE_CUTOFF_FOR_ON_DEMAND } from "../../../../scripts/packages/tator-js/src/annotator/video.js";
+import { RATE_CUTOFF_FOR_ON_DEMAND, Direction } from "../../../../scripts/packages/tator-js/src/annotator/video.js";
 import {
   handle_video_error,
   handle_decoder_error,
@@ -989,6 +989,7 @@ export class AnnotationMulti extends TatorElement {
       let multiImage = false;
       let bias = 50;
       let playingOutOfScrub = 0;
+      let playingBackwards = 0;
       if (this._focusIds.length > 0) {
         video = [];
         for (let focusId of this._focusIds) {
@@ -1011,10 +1012,15 @@ export class AnnotationMulti extends TatorElement {
         ) {
           playingOutOfScrub++;
         }
+        if (playingBackwards |=
+          video[idx]._direction == Direction.BACKWARDS
+        ) {
+          playingBackwards++;
+        }
       }
 
       // Disable the preview if we are playing out of scrub
-      if (this._videoStatus == "playing" && playingOutOfScrub) {
+      if (this._videoStatus == "playing" && (playingOutOfScrub || playingBackwards)) {
         multiImage = false;
       }
 
