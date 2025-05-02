9d9fd2db62e5540a6f9b77ff299b002be6e1ef40
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2025-05-02 21:00:14 +0000

    DD Log Updates.
    Nested getattr doesn't work
    
    (cherry picked from commit b9bca694c70bc0c865b4fbe5e9e3030c46bab457)
    
    Log record missing dd attribute most of the time, try a filter
    
    (cherry picked from commit 710dcb9a2db4cf8121d746ebeaf43c0dc97d6720)
    
    Use a custom logrecord instead of a filter
    
    (cherry picked from commit 5b3beb407b30f5224c95df6dc59077d4d5c4258f)
    
    Revert dockerfile to 22.04, remove venv. Not sure which is the problem but it works
    
    (cherry picked from commit 9f9559d40ac647fe34255e541c52f0d99a1557b3)

--- api/main/apps.py
+++ api/main/apps.py
@@ -1,5 +1,23 @@
 from django.apps import AppConfig
+import logging
+import os
 
+
+def format_multiline_log_message(message):
+    allow_multiline = os.getenv("TATOR_LOG_MULTI_LINE", "false").lower() == "true"
+    if allow_multiline:
+        return message
+    else:
+        return str(message).replace("\n", " \\n ").replace("\t", "    ")
+
+class MultilineLogRecord(logging.LogRecord):
+    def getMessage(self):
+        message = super().getMessage()
+        formatted_message = format_multiline_log_message(message)
+        self.message = formatted_message
+        return self.message
 
 class MainConfig(AppConfig):
     name = "main"
+    def ready(self):
+        logging.setLogRecordFactory(MultilineLogRecord)
